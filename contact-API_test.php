ba0fb812005a995b2cd859a29d897e97307d686d
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-03-19 15:13:38 +0100

    api2

---
+++ contact-API_test.php
@@ -0,0 +1,24 @@
+use Google\Cloud\ResourceManager\V3\ContactsServiceClient;
+
+/**
+ * Example:
+ * ```
+ * list_contacts($projectId);
+ * ```
+ *
+ * @param string $projectId Your Cloud Project ID
+ */
+function list_contacts(string $projectId): void
+{
+    // Instantiate a client.
+    $client = new ContactsServiceClient();
+
+    $formattedParent = $client->projectName($projectId);
+    $response = $client->listContacts($formattedParent);
+
+    // Print the contacts.
+    $contacts = $response->iterateAllElements();
+    foreach ($contacts as $contact) {
+        printf('Contact: %s' . PHP_EOL, $contact->getName());
+    }
+}  
