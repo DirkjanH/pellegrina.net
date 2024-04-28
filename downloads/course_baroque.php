6e45f265fb41cc13c44c2060feaef0f2c79d05c7
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-04-28 13:13:36 +0200

    tutti

--- downloads/course_baroque.php
+++ downloads/course_baroque.php
@@ -178,7 +178,7 @@ $cursusnr = 2 + $cursus_offset;
         <li><a href="https://pellegrina.net/pdf/2024-2/Nisi Dominus RV 608/VIVALDI_Cum_dederit_from_Nisi_Dominus_RV_608_(violons_1_&_2).pdf">Vivaldi - Cum dederit from Nisi Dominus - Violins 1 & 2</a></li>
         <li><a href="https://pellegrina.net/pdf/2024-2/Nisi Dominus RV 608/VIVALDI_Cum_dederit_from_Nisi_Dominus_RV_608_(vlc_&_b.c.).pdf">Vivaldi - Cum dederit from Nisi Dominus - Celli & BC</a></li>
       </ul>
-      <h3>Vivaldi - Sinfonie avanti la Senna Festeggiante RV 693</h3>
+      <h3>Vivaldi - Sinfonie avanti la Senna Festeggiante RV 693 (instruments only)</h3>
       <ul>
         <li><a href="https://pellegrina.net/pdf/2024-2/Vivaldi_Senna_RV693/vivaldi_la_senna_festeggiante_parte_prima_violino1.pdf" target="_blank">Flauto/oboe/violini 1</a></li>
         <li><a href="https://pellegrina.net/pdf/2024-2/Vivaldi_Senna_RV693/vivaldi_la_senna_festeggiante_parte_prima_violino2.pdf" target="_blank">Flauto/oboe/violini 2</a></li>
