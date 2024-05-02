acf33a8b5694a2f175182c25ca3238bbb04f5669
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-05-02 14:56:21 +0200

    2.2

--- EN/index.php
+++ EN/index.php
@@ -1,9 +1,10 @@
 <?php
-require_once( $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/includes2024.php' );
+require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/includes2024.php');
 ?>
 
 <!DOCTYPE HTML>
 <html>
+
 <head>
 	<title>La Pellegrina&nbsp;
 		<?php echo $jaar ?>
@@ -11,13 +12,13 @@ require_once( $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/includes2024.php' );
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="/css/w3.css">
-	<?php require_once $_SERVER['DOCUMENT_ROOT'].'/includes/metatags+javascript.NL.php'; ?>
-	<?php require_once $_SERVER['DOCUMENT_ROOT'].'/includes/GA_code.php'; ?>
+	<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/metatags+javascript.NL.php'; ?>
+	<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/GA_code.php'; ?>
 	<script type="text/javascript">
-		function klapdiensten( id ) {
-			if ( document.getElementById( id ) ) {
-				var cont = document.getElementById( id ).style;
-				if ( cont.display == "block" ) {
+		function klapdiensten(id) {
+			if (document.getElementById(id)) {
+				var cont = document.getElementById(id).style;
+				if (cont.display == "block") {
 					cont.display = "none";
 				} else {
 					cont.display = "block";
@@ -40,44 +41,42 @@ require_once( $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/includes2024.php' );
 <![endif]-->
 	<!-- Facebook Pixel Code -->
 	<script>
-		! function ( f, b, e, v, n, t, s ) {
-			if ( f.fbq ) return;
-			n = f.fbq = function () {
+		! function(f, b, e, v, n, t, s) {
+			if (f.fbq) return;
+			n = f.fbq = function() {
 				n.callMethod ?
-					n.callMethod.apply( n, arguments ) : n.queue.push( arguments )
+					n.callMethod.apply(n, arguments) : n.queue.push(arguments)
 			};
-			if ( !f._fbq ) f._fbq = n;
+			if (!f._fbq) f._fbq = n;
 			n.push = n;
 			n.loaded = !0;
 			n.version = '2.0';
 			n.queue = [];
-			t = b.createElement( e );
+			t = b.createElement(e);
 			t.async = !0;
 			t.src = v;
-			s = b.getElementsByTagName( e )[ 0 ];
-			s.parentNode.insertBefore( t, s )
-		}( window, document, 'script',
-			'https://connect.facebook.net/en_US/fbevents.js' );
-		fbq( 'init', '537749209897328' );
-		fbq( 'track', 'PageView' );
+			s = b.getElementsByTagName(e)[0];
+			s.parentNode.insertBefore(t, s)
+		}(window, document, 'script',
+			'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '537749209897328');
+		fbq('track', 'PageView');
 	</script>
-	<noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=537749209897328&ev=PageView&noscript=1"
-/></noscript>
+	<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=537749209897328&ev=PageView&noscript=1" /></noscript>
 	<!-- End Facebook Pixel Code -->
 </head>
 
 <body>
 	<div id="fb-root"></div>
 	<script>
-		( function ( d, s, id ) {
-			var js, fjs = d.getElementsByTagName( s )[ 0 ];
-			if ( d.getElementById( id ) ) return;
-			js = d.createElement( s );
+		(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s);
 			js.id = id;
 			js.src = "//connect.facebook.net/nl_NL/all.js#xfbml=1";
-			fjs.parentNode.insertBefore( js, fjs );
-		}( document, 'script', 'facebook-jssdk' ) );
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
 	</script>
 
 	<div id="inhoud">
@@ -98,7 +97,7 @@ require_once( $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/includes2024.php' );
 				<div class="bannerrood">The 2024 courses are (nearly) full</div>
 			</div>
 		</div>
-		<?php require_once $_SERVER['DOCUMENT_ROOT'].'/includes/header.EN.php'; ?>
+		<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.EN.php'; ?>
 
 		<div id="main" class="w3-row-padding">
 			<div class="w3-card-2 w3-padding w3-margin-bottom w3-half">
@@ -111,7 +110,7 @@ require_once( $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/includes2024.php' );
 				<p>Click on the course titles for more information. From 1 December, all information will be complete and registration will be open again.</p>
 				<p>Please don't hesitate to contact Dirkjan Horringa in case you have any questions, by mail (<a href="mailto:info@pellegrina.net">info@pellegrina.net</a>) or by phone (+31 619 224 758).</p>
 				<p class="w3-center w3-panel">In cooperation with<br>
-					<a href="http://www.acmp.net" target="_blank"><img src="../Images/Logos/ACMP_CMYK.jpg" width="200" height="104" alt=""/></a>
+					<a href="http://www.acmp.net" target="_blank"><img src="../Images/Logos/ACMP_CMYK.jpg" width="200" height="104" alt="" /></a>
 				</p>
 				<div class="w3-content w3-center">
 					<p><a href="/NL/index.php"><img src="/Images/Logos/Vlag_NL.jpg" alt="Ga naar de Nederlandstalige site" width="34" height="24" border="0" class="w3-image" /></a>
@@ -129,9 +128,9 @@ require_once( $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/includes2024.php' );
 						<li>České Budějovice (Czechia), 25 July - 4 August <?php echo $jaar ?>
 						</li>
 					</ul>
-					<p class="plaatsvoor">Places for horn, some strings in all groups (in particular violins!) and piano available<br>
-					The choir has a few places in all voice groups, in particular for sopranos and basses</p>
-					<p class="volvoor">The course is full for flute, oboe, clarinet and bassoon</p>
+					<p class="plaatsvoor">Places for a few more violins available<br>
+						The choir has a few places in all voice groups, in particular for sopranos and basses</p>
+					<p class="volvoor">The course is full for all other instruments</p>
 					<p class="onzichtbaar">*: Speciální výhodná cena pro české houslisty a violisty: kurs 2 nebo 3 včetně (dvoulůžkové) ubytování a stravování za Kč 4.000. Jenom napište &quot;ANKST&quot; na přihlášce v poli &quot;Remarks and additional information</p>
 				</div>
 				<div class="w3-card-2 w3-padding w3-margin-bottom">
@@ -141,7 +140,7 @@ require_once( $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/includes2024.php' );
 						<li>Priory Nieuw Sion, Diepenveen (Netherlands), 11 - 17 August <?php echo $jaar ?>
 						</li>
 					</ul>
-					<p class="plaatsvoor">Places available for two baroque oboes, some more baroque violins/violas and continuo players (organ/harpsichord/theorbo)</p>
+					<p class="plaatsvoor">Places available for a two more baroque violins/violas and continuo players (organ/harpsichord/theorbo)</p>
 					<p class="volvoor">This course is full for other instruments and for singers</p>
 				</div>
 				<div class="w3-card-2 w3-padding w3-margin-bottom">
@@ -160,107 +159,108 @@ require_once( $_SERVER[ 'DOCUMENT_ROOT' ] . '/includes/includes2024.php' );
 					<p><a href="contact.php">Contact</a></p>
 				</div>
 			</div>
-				<!-- begin olark code -->
-				<script data-cfasync="false" type='text/javascript'>
-					/*<![CDATA[*/
-					window.olark || ( function ( c ) {
-						var f = window,
-							d = document,
-							l = f.location.protocol == "https:" ? "https:" : "http:",
-							z = c.name,
-							r = "load";
-						var nt = function () {
-							f[ z ] = function () {
-								( a.s = a.s || [] ).push( arguments )
-							};
-							var a = f[ z ]._ = {},
-								q = c.methods.length;
-							while ( q-- ) {
-								( function ( n ) {
-									f[ z ][ n ] = function () {
-										f[ z ]( "call", n, arguments )
-									}
-								} )( c.methods[ q ] )
-							}
-							a.l = c.loader;
-							a.i = nt;
-							a.p = {
-								0: +new Date
-							};
-							a.P = function ( u ) {
-								a.p[ u ] = new Date - a.p[ 0 ]
-							};
-
-							function s() {
-								a.P( r );
-								f[ z ]( r )
-							}
-							f.addEventListener ? f.addEventListener( r, s, false ) : f.attachEvent( "on" + r, s );
-							var ld = function () {
-								function p( hd ) {
-									hd = "head";
-									return [ "<", hd, "></", hd, "><", i, ' onl' + 'oad="var d=', g, ";d.getElementsByTagName('head')[0].", j, "(d.", h, "('script')).", k, "='", l, "//", a.l, "'", '"', "></", i, ">" ].join( "" )
-								}
-								var i = "body",
-									m = d[ i ];
-								if ( !m ) {
-									return setTimeout( ld, 100 )
-								}
-								a.P( 1 );
-								var j = "appendChild",
-									h = "createElement",
-									k = "src",
-									n = d[ h ]( "div" ),
-									v = n[ j ]( d[ h ]( z ) ),
-									b = d[ h ]( "iframe" ),
-									g = "document",
-									e = "domain",
-									o;
-								n.style.display = "none";
-								m.insertBefore( n, m.firstChild ).id = z;
-								b.frameBorder = "0";
-								b.id = z + "-loader";
-								if ( /MSIE[ ]+6/.test( navigator.userAgent ) ) {
-									b.src = "javascript:false"
-								}
-								b.allowTransparency = "true";
-								v[ j ]( b );
-								try {
-									b.contentWindow[ g ].open()
-								} catch ( w ) {
-									c[ e ] = d[ e ];
-									o = "javascript:var d=" + g + ".open();d.domain='" + d.domain + "';";
-									b[ k ] = o + "void(0);"
-								}
-								try {
-									var t = b.contentWindow[ g ];
-									t.write( p() );
-									t.close()
-								} catch ( x ) {
-									b[ k ] = o + 'd.write("' + p().replace( /"/g, String.fromCharCode( 92 ) + '"' ) + '");d.close();'
-								}
-								a.P( 2 )
-							};
-							ld()
+			<!-- begin olark code -->
+			<script data-cfasync="false" type='text/javascript'>
+				/*<![CDATA[*/
+				window.olark || (function(c) {
+					var f = window,
+						d = document,
+						l = f.location.protocol == "https:" ? "https:" : "http:",
+						z = c.name,
+						r = "load";
+					var nt = function() {
+						f[z] = function() {
+							(a.s = a.s || []).push(arguments)
 						};
-						nt()
-					} )( {
-						loader: "static.olark.com/jsclient/loader0.js",
-						name: "olark",
-						methods: [ "configure", "extend", "declare", "identify" ]
-					} );
-					/* custom configuration goes here (www.olark.com/documentation) */
-					olark.identify( '5575-684-10-1480' ); /*]]>*/
-				</script>
-				<noscript>
+						var a = f[z]._ = {},
+							q = c.methods.length;
+						while (q--) {
+							(function(n) {
+								f[z][n] = function() {
+									f[z]("call", n, arguments)
+								}
+							})(c.methods[q])
+						}
+						a.l = c.loader;
+						a.i = nt;
+						a.p = {
+							0: +new Date
+						};
+						a.P = function(u) {
+							a.p[u] = new Date - a.p[0]
+						};
+
+						function s() {
+							a.P(r);
+							f[z](r)
+						}
+						f.addEventListener ? f.addEventListener(r, s, false) : f.attachEvent("on" + r, s);
+						var ld = function() {
+							function p(hd) {
+								hd = "head";
+								return ["<", hd, "></", hd, "><", i, ' onl' + 'oad="var d=', g, ";d.getElementsByTagName('head')[0].", j, "(d.", h, "('script')).", k, "='", l, "//", a.l, "'", '"', "></", i, ">"].join("")
+							}
+							var i = "body",
+								m = d[i];
+							if (!m) {
+								return setTimeout(ld, 100)
+							}
+							a.P(1);
+							var j = "appendChild",
+								h = "createElement",
+								k = "src",
+								n = d[h]("div"),
+								v = n[j](d[h](z)),
+								b = d[h]("iframe"),
+								g = "document",
+								e = "domain",
+								o;
+							n.style.display = "none";
+							m.insertBefore(n, m.firstChild).id = z;
+							b.frameBorder = "0";
+							b.id = z + "-loader";
+							if (/MSIE[ ]+6/.test(navigator.userAgent)) {
+								b.src = "javascript:false"
+							}
+							b.allowTransparency = "true";
+							v[j](b);
+							try {
+								b.contentWindow[g].open()
+							} catch (w) {
+								c[e] = d[e];
+								o = "javascript:var d=" + g + ".open();d.domain='" + d.domain + "';";
+								b[k] = o + "void(0);"
+							}
+							try {
+								var t = b.contentWindow[g];
+								t.write(p());
+								t.close()
+							} catch (x) {
+								b[k] = o + 'd.write("' + p().replace(/"/g, String.fromCharCode(92) + '"') + '");d.close();'
+							}
+							a.P(2)
+						};
+						ld()
+					};
+					nt()
+				})({
+					loader: "static.olark.com/jsclient/loader0.js",
+					name: "olark",
+					methods: ["configure", "extend", "declare", "identify"]
+				});
+				/* custom configuration goes here (www.olark.com/documentation) */
+				olark.identify('5575-684-10-1480'); /*]]>*/
+			</script>
+			<noscript>
 				<p><a href="https://www.olark.com/site/5575-684-10-1480/contact" title="Contact us" target="_blank">Questions? Feedback? Use this chat app. Please send in your registration only via the form on the website</a></p>
-				</noscript>
-			
-				<!-- end olark code -->
-			</div>
+			</noscript>
+
+			<!-- end olark code -->
 		</div>
-		<?php 
-require_once ($_SERVER['DOCUMENT_ROOT'].'/includes/footer.php');
-?>
+	</div>
+	<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
+	?>
 </body>
+
 </html>
