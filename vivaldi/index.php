ed51be4ce0e00e80f9fdcade237f607c17d62026
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-06-04 12:50:31 +0200

    vivaldi

---
+++ vivaldi/index.php
@@ -0,0 +1,77 @@
+<?php
+// PLANNER© versie: 1.0 van 25-05-2023 betreft: index.php op locatie: _incb/ door: Jurgen v/d Ent (ThreeDees.eu/InGoedeHanden.Management)
+// Disclaimer: Niets uit dit script mag worden gekopieerd en/of openbaar gemaakt worden zonder schriftelijke toestemming.
+?>
+<!DOCTYPE HTML>
+<html lang=NL>
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf8mb4_ubicode_ci">
+  <title>La Pellegrina - Concert Vivaldi's Venetiaanse Vespers</title>
+  <link rel="apple-touch-icon" sizes="180x180" href="https://pellegrina.net/Images/Logos/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="https://pellegrina.net/Images/Logos/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="https://pellegrina.net/Images/Logos/favicon-16x16.png">
+  <link rel="shortcut icon" href="https://pellegrina.net/Images/Logos/favicon.ico" type="image/png">
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <meta name='keywords' content='Kaartreservering Vivaldi&#39s Venetiaanse Vespers,La Pellegrina,barokcursus'>
+  <meta name='description' content='Kaartreserveringspagian voor concert Vivaldi&#39s Venetiaanse Vespers'>
+  <meta property="og:url" content='https://pellegrina.net/vivaldi'>
+  <meta property="og:type" content='website'>
+  <meta property="og:title" content='La Pellegrina - Concert Vivaldi&#39s Venetiaanse Vespers'>
+  <meta property="og:description" content='Kaartreserveringspagian voor concert Vivaldi&#39s Venetiaanse Vespers'>
+  <meta property="og:image" content='https://pellegrina.net/Images/Logos/favicon-32x32.png'>
+  <!--[if lt IE 9]>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.js"></script>
+<![endif]-->
+  <!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js">
+</script>
+<![endif]-->
+  <!-- Facebook Pixel Code -->
+  <script>
+    ! function(f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function() {
+        n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s)
+    }(window, document, 'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '2292426324112331');
+    fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2292426324112331&ev=PageView&noscript=1" /></noscript>
+  <!-- End Facebook Pixel Code -->
+</head>
+
+<body>
+  <style>
+    body {
+      margin: 0;
+      /* Reset default margin */
+    }
+
+    iframe {
+      display: block;
+      /* iframes are inline by default */
+      border: none;
+      /* Reset default border */
+      height: 100vh;
+      /* Viewport-relative units */
+      width: 100vw;
+    }
+  </style>
+  <iframe src="https://www.ingoedehanden.management/index.php?gotopageid=70139"></iframe>
+</body>
+
+</html>
