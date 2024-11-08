dc87fa324e442330ce7e84338312768dfaf3036b
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-08 14:47:08 +0100

    lp5

--- includes/cursusdata.php
+++ includes/cursusdata.php
@@ -1,5 +1,5 @@
-<?php 
-require_once ($_SERVER['DOCUMENT_ROOT'].'/includes/includes2024.php');
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/includes2025.php');
 
 Kint::$enabled_mode = false;
 
@@ -12,10 +12,10 @@ if ($_GET['test'] == 'geentest') unset($_SESSION['test']);
 $cursusdata = select_query("SELECT * FROM cursus WHERE cursusId = {$cursus}", 1);
 
 d($cursusdata, $_GET, $_SESSION);
-	
+
 if (!is_array($cursusdata)) {
-    echo "No rows found, nothing to print so am exiting";
-    exit;
+	echo "No rows found, nothing to print so am exiting";
+	exit;
 }
 // einde Recordset cursusgegevens
 
@@ -26,38 +26,32 @@ if ($taal == 'NL') {
 	if (strftime('%B', strtotime($cursusdata['datum_begin'])) == strftime('%B', strtotime($cursusdata['datum_eind']))) {
 		$cursusdata['datum'] = strftime('%A %e', strtotime($cursusdata['datum_begin'])) . ' - ' . $cursusdata['einddatum'];
 		$cursusdata['datumkort'] = $cursusdata['cursusplaats_nl'] . ' - ' . strftime('%e', strtotime($cursusdata['datum_begin'])) . ' - ' . strftime('%e %B', strtotime($cursusdata['datum_eind']));
-	}	
-		else {
+	} else {
 		$cursusdata['datum'] = $cursusdata['begindatum'] . ' - ' . $cursusdata['einddatum'];
 		$cursusdata['datumkort'] = $cursusdata['cursusplaats_nl'] . ' - ' . strftime('%e %B', strtotime($cursusdata['datum_begin'])) . ' - ' . strftime('%e %B', strtotime($cursusdata['datum_eind']));
-	}	
+	}
 	$cursusdata['beslisdatum'] = strftime('%e %B', strtotime($cursusdata['datum_beslissing']));
 	$cursusdata['betaaldatum'] = strftime('%e %B', strtotime($cursusdata['datum_betaling']));
-	$cursusdata['cursusnaam'] = $cursusdata['cursusnaam_nl']; 
+	$cursusdata['cursusnaam'] = $cursusdata['cursusnaam_nl'];
 	$cursusdata['ondertitel'] =	$cursusdata['cursusplaats_nl'] . ', ' . $cursusdata['datum'];
-	$cursusdata['nr'] = $cursusdata['CursusId'] - $cursus_offset; 
-}
-
-elseif ($taal == 'EN') {
+	$cursusdata['nr'] = $cursusdata['CursusId'] - $cursus_offset;
+} elseif ($taal == 'EN') {
 	setlocale(LC_ALL, 'en_GB.UTF-8'); // data e.d. in het Engels
 	$cursusdata['begindatum'] = strftime('%A %e %B', strtotime($cursusdata['datum_begin']));
 	$cursusdata['einddatum'] = strftime('%A %e %B %Y', strtotime($cursusdata['datum_eind']));
 	if (strftime('%B', strtotime($cursusdata['datum_begin'])) == strftime('%B', strtotime($cursusdata['datum_eind']))) {
 		$cursusdata['datum'] = strftime('%A %e', strtotime($cursusdata['datum_begin'])) . ' - ' . $cursusdata['einddatum'];
 		$cursusdata['datumkort'] = $cursusdata['cursusplaats_en'] . ' - ' . strftime('%e', strtotime($cursusdata['datum_begin'])) . ' - ' . strftime('%e %B', strtotime($cursusdata['datum_eind']));
-	}	
-		else {
+	} else {
 		$cursusdata['datum'] = $cursusdata['begindatum'] . ' - ' . $cursusdata['einddatum'];
 		$cursusdata['datumkort'] = $cursusdata['cursusplaats_en'] . ' - ' . strftime('%e %B', strtotime($cursusdata['datum_begin'])) . ' - ' . strftime('%e %B', strtotime($cursusdata['datum_eind']));
-	}	
+	}
 	$cursusdata['beslisdatum'] = strftime('%e %B', strtotime($cursusdata['datum_beslissing']));
 	$cursusdata['betaaldatum'] = strftime('%e %B', strtotime($cursusdata['datum_betaling']));
-	$cursusdata['cursusnaam'] = $cursusdata['cursusnaam_en']; 
+	$cursusdata['cursusnaam'] = $cursusdata['cursusnaam_en'];
 	$cursusdata['ondertitel'] =	$cursusdata['cursusplaats_en'] . ', ' . $cursusdata['datum'];
-	$cursusdata['nr'] = $cursusdata['CursusId'] - $cursus_offset; 
-}
-else exit('Deze taal bestaat niet!');
+	$cursusdata['nr'] = $cursusdata['CursusId'] - $cursus_offset;
+} else exit('Deze taal bestaat niet!');
 
-if (isset($_SESSION['test']) AND $_SESSION['test'] == 'test') require_once ($_SERVER['DOCUMENT_ROOT'].'/includes/navigatie_test.php');
-	else require_once ($_SERVER['DOCUMENT_ROOT'].'/includes/navigatie.php');
-?>
+if (isset($_SESSION['test']) and $_SESSION['test'] == 'test') require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/navigatie_test.php');
+else require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/navigatie.php');
--- includes/cursussen.php
+++ includes/cursussen.php
@@ -4,24 +4,24 @@
 
 error_reporting(E_ALL);
 
-require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
 Kint::$enabled_mode = false;
 
-require_once ($_SERVER['DOCUMENT_ROOT'].'/includes/includes2024.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/includes2025.php');
 
 $query_cursussen = "SELECT * FROM cursus WHERE cursusId BETWEEN $eerstecursus AND $laatstecursus";
-	
+
 $result = select_query($query_cursussen);
 
 if (!is_array($result)) {
-    echo "Could not successfully run query ($query_cursussen)";
-    exit;
+	echo "Could not successfully run query ($query_cursussen)";
+	exit;
 }
 
 if (count($result) == 0) {
-    echo "No rows found, nothing to print so am exiting";
-    exit;
+	echo "No rows found, nothing to print so am exiting";
+	exit;
 }
 
 setlocale(LC_ALL, 'en_GB'); // data e.d. in het Engels
@@ -30,21 +30,19 @@ foreach ($result as $cursusdata) {
 	$cursussen[$cursusdata['CursusId']]['begindatum'] = strftime('%A %e %B', strtotime($cursusdata['datum_begin']));
 	$cursussen[$cursusdata['CursusId']]['einddatum'] = strftime('%A %e %B %Y', strtotime($cursusdata['datum_eind']));
 	if (strftime('%B', strtotime($cursusdata['datum_begin'])) == strftime('%B', strtotime($cursusdata['datum_eind']))) {
-		$cursussen[$cursusdata['CursusId']]['datum'] = 
-		strftime('%A %e', strtotime($cursusdata['datum_begin'])) . ' - ' . $cursusdata['datum_eind'];
-		$cursussen[$cursusdata['CursusId']]['datumkort'] = 
-		strftime('%e', strtotime($cursusdata['datum_begin'])) . ' - ' . strftime('%e %B', strtotime($cursusdata['datum_eind']));
-	}	
-		else {
+		$cursussen[$cursusdata['CursusId']]['datum'] =
+			strftime('%A %e', strtotime($cursusdata['datum_begin'])) . ' - ' . $cursusdata['datum_eind'];
+		$cursussen[$cursusdata['CursusId']]['datumkort'] =
+			strftime('%e', strtotime($cursusdata['datum_begin'])) . ' - ' . strftime('%e %B', strtotime($cursusdata['datum_eind']));
+	} else {
 		$cursussen[$cursusdata['CursusId']]['datum'] = $cursusdata['datum_begin'] . ' - ' . $cursusdata['datum_eind'];
-		$cursussen[$cursusdata['CursusId']]['datumkort'] = 
-		strftime('%e %B', strtotime($cursusdata['datum_begin'])) . ' - ' . strftime('%e %B', strtotime($cursusdata['datum_eind']));
-	}	
+		$cursussen[$cursusdata['CursusId']]['datumkort'] =
+			strftime('%e %B', strtotime($cursusdata['datum_begin'])) . ' - ' . strftime('%e %B', strtotime($cursusdata['datum_eind']));
+	}
 	$cursussen[$cursusdata['CursusId']]['beslisdatum'] = strftime('%A %e %B %Y', strtotime($cursusdata['datum_beslissing']));
 	$cursussen[$cursusdata['CursusId']]['betaaldatum'] = strftime('%A %e %B %Y', strtotime($cursusdata['datum_betaling']));
-	$cursussen[$cursusdata['CursusId']]['cursusnaam'] = $cursusdata['cursusnaam_en'] 
+	$cursussen[$cursusdata['CursusId']]['cursusnaam'] = $cursusdata['cursusnaam_en']
 		. '<BR>(' . $cursusdata['cursusplaats_en'] . ', ' . $cursussen[$cursusdata['CursusId']]['datumkort'] . ')';
 }
 
 d($cursusdata);
-?>
