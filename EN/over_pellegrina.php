ec0d09ce90dcb2bce60f1f7aa1808530222fffa6
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-08 14:26:09 +0100

    lp3

--- EN/over_pellegrina.php
+++ EN/over_pellegrina.php
@@ -1,84 +1,87 @@
 <!doctype html>
 <html><!-- InstanceBegin template="/Templates/leeg_EN.dwt.php" codeOutsideHTMLIsLocked="false" -->
+
 <head>
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
 
-   <!-- CSS: -->
-   <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
+  <!-- CSS: -->
+  <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
 
-<!-- InstanceBeginEditable name="doctitle" -->
-<title>Naamloos document</title>
-<!-- InstanceEndEditable -->
-<!-- InstanceBeginEditable name="head" -->
-<!-- InstanceEndEditable -->
+  <!-- InstanceBeginEditable name="doctitle" -->
+  <title>Naamloos document</title>
+  <!-- InstanceEndEditable -->
+  <!-- InstanceBeginEditable name="head" -->
+  <!-- InstanceEndEditable -->
 </head>
 
 <body>
-<!-- InstanceBeginEditable name="mainpage" -->
-<h2 class="begin"><em>La Pellegrina</em></h2>
-  <p>La Pellegrina was founded in 1988 by dancer Doroth&eacute;e Wortelboer and musician Dirkjan Horringa for the organization of summer schools abroad. In the first years of <em>La Pellegrina </em>the courses were held in Italy, with historical dance and -music as subject. Since 1991 the courses have acquired a much wider scope and take place in Czechia and the Netherlands - this summer for the 34<sup>th</sup> time.</p>
+  <!-- InstanceBeginEditable name="mainpage" -->
+  <h2 class="begin"><em>La Pellegrina</em></h2>
+  <p>La Pellegrina was founded in 1988 by dancer Doroth&eacute;e Wortelboer and musician Dirkjan Horringa for the organization of summer schools abroad. In the first years of <em>La Pellegrina </em>the courses were held in Italy, with historical dance and -music as subject. Since 1991 the courses have acquired a much wider scope and take place in Czechia and the Netherlands - this summer for the 35<sup>th</sup> time.</p>
   <h2>What does <em>La Pellegrina</em> mean? </h2>
-  <p><em>La Pellegrina</em> (the Pilgrim Woman) is the name of a Italian play from the 16th century. When this play was to be performed at the marriage of Ferdinand I de' Medici, grand-duke of Tuscany, with Christine de Lorraine, granddaughter of the former queen-mother of France, they decided to make the play a little more lively. Between the play's five acts they added <em>intermedii</em>, musical interludes which were sung, played and danced. They also used spectacular machines which could make gods fly down from the sky and make boats float on stage. These <em>intermedii </em>made a huge impression, which eventually led to the birth of  opera as an art form. The <em>Intermedii per La Pellegrina</em> from 1589 was in fact one of the first opera-like productions. Without it, <em>Orfeo</em> from Monteverdi would not have been imaginable.</p>
+  <p><em>La Pellegrina</em> (the Pilgrim Woman) is the name of a Italian play from the 16th century. When this play was to be performed at the marriage of Ferdinand I de' Medici, grand-duke of Tuscany, with Christine de Lorraine, granddaughter of the former queen-mother of France, they decided to make the play a little more lively. Between the play's five acts they added <em>intermedii</em>, musical interludes which were sung, played and danced. They also used spectacular machines which could make gods fly down from the sky and make boats float on stage. These <em>intermedii </em>made a huge impression, which eventually led to the birth of opera as an art form. The <em>Intermedii per La Pellegrina</em> from 1589 was in fact one of the first opera-like productions. Without it, <em>Orfeo</em> from Monteverdi would not have been imaginable.</p>
   <p>In 1988 Doroth&eacute;e Wortelboer and Dirkjan Horringa were invited to do a master class in Italy with the <em>Intermedii per La Pellegrina </em>as main subject. The inviting organization then went bankrupt. Doroth&eacute;e and Dirkjan decided overnight to organize the summer course themselves. To facilitate this they had to found an company and to choose a name for it; <em>La Pellegrina</em> was the obvious choice. Although in the beginning the focus was on the historical dance and opera, <em>La Pellegrina</em> nowadays doesn't focus on these subjects exclusively anymore, but covers a much wider scope of vocal and instrumental music from the renaissance until now.</p>
   <h2>All periods</h2>
-  <p><em>La Pellegrina</em> focuses on music from the Baroque, the Classical period, the Romantic era and the 20th/21st century. The courses in Czechia focus on the rich Czech music culture. But there will also be attention for the German, Austrian, Hungarian and Slovakian composers, who belong after all to the same cultural area.  For all the courses the century-old historic buildings in which will be rehearsed and where the concerts will take place create a unique atmosphere and lend extra inspiration to the musicians.</p>
+  <p><em>La Pellegrina</em> focuses on music from the Baroque, the Classical period, the Romantic era and the 20th/21st century. The courses in Czechia focus on the rich Czech music culture. But there will also be attention for the German, Austrian, Hungarian and Slovakian composers, who belong after all to the same cultural area. For all the courses the century-old historic buildings in which will be rehearsed and where the concerts will take place create a unique atmosphere and lend extra inspiration to the musicians.</p>
   <p><a href="vorigeprojecten.php">Previous projects</a></p>
   <p class="citaat">Marinus de Mooij: &quot;I've learned so much. There
-   are so many tutors that I often had the impression of having private lessons.&quot;</p>
+    are so many tutors that I often had the impression of having private lessons.&quot;</p>
   <h2>Intensive coaching by specialist tutors</h2>
   <div class="w3-container w3-right"><img src="/Images/blij_kwartet.jpg" alt="Quartet relaxes" width="400" height="251" class="w3-image" />
     <div class="w3-container w3-center fotobijschrift">String quartet at relaxed moment</div>
   </div>
   <p>Conductor Dirkjan Horringa is the driving force
-   behind the summer courses, cooperating closely with an international team of
-   specialised musicians. 'Each project consists of ten days of rehearsing for
-   six hours a day,' he explains. 'Half of that time is spent in close contact
-   with specialist tutors. The great thing about the course is that all the time
-   you are surrounded by tutors, instruments and voices, so you can imagine there
-   is a lot of playing outside official rehearsals, even in ensembles of unusual
-   composition.'</p>
+    behind the summer courses, cooperating closely with an international team of
+    specialised musicians. 'Each project consists of ten days of rehearsing for
+    six hours a day,' he explains. 'Half of that time is spent in close contact
+    with specialist tutors. The great thing about the course is that all the time
+    you are surrounded by tutors, instruments and voices, so you can imagine there
+    is a lot of playing outside official rehearsals, even in ensembles of unusual
+    composition.'</p>
   <p class="citaat">Erika Ploeger: &quot;After ten days of rehearsing everybody
-   is ready for the big concert which really gives you a kick. Afterwards, we
-   play salon music on the goodbye party.&quot;</p>
+    is ready for the big concert which really gives you a kick. Afterwards, we
+    play salon music on the goodbye party.&quot;</p>
   <h2>At a rather high level</h2>
   <p> The Czech summers in Bechyn&#283; consist of three projects, for chamber choir and orchestra, symphonic orchestra
-   and chamber music and for early music specialists. Needless to say
-   that there is both serious work
-   and good fun
-   on a good musical level for passionate singers and instrumentalists.</p>
-<div class="w3-content w3-center "><img src="/Images/orkest.jpg" alt="Orchestral concert" class="w3-image"/>
+    and chamber music and for early music specialists. Needless to say
+    that there is both serious work
+    and good fun
+    on a good musical level for passionate singers and instrumentalists.</p>
+  <div class="w3-content w3-center "><img src="/Images/orkest.jpg" alt="Orchestral concert" class="w3-image" />
     <div class="w3-container fotobijschrift">Concert at the Riding School, Bechyn&#283;</div>
-</div>
-<p class="citaat">Evelien Tollenaar: &quot;Everything has been arranged
-   when you get there. All you need to do is pack your instrument and go. Once
-   in Bechyne, there is ten days of worrying about nothing, except playing the
-   right notes&quot;</p>
-<div class="w3-content w3-center"><img src="/Images/trio.jpg" alt="Trio" class="w3-image"/>
-  <div class="w3-container w3-center fotobijschrift">Trio during chamber music rehearsal</div>
-</div>
-<p class="citaat">Jonelle Soelling:	&quot;Preparing to take part in this music festival really motivated me to achieve a higher level of playing. Being part of an international musical community in Czechia was a wonderful experience&quot;</p>
-<div class="w3-content w3-center"><img src="/Images/gesprek.jpg" alt="Monastery garden" class="w3-image"/>
-  <div class="w3-container w3-center fotobijschrift">Discussion in the monastery cloister, Bechyn&#283;</div>
-</div>
+  </div>
+  <p class="citaat">Evelien Tollenaar: &quot;Everything has been arranged
+    when you get there. All you need to do is pack your instrument and go. Once
+    in Bechyne, there is ten days of worrying about nothing, except playing the
+    right notes&quot;</p>
+  <div class="w3-content w3-center"><img src="/Images/trio.jpg" alt="Trio" class="w3-image" />
+    <div class="w3-container w3-center fotobijschrift">Trio during chamber music rehearsal</div>
+  </div>
+  <p class="citaat">Jonelle Soelling: &quot;Preparing to take part in this music festival really motivated me to achieve a higher level of playing. Being part of an international musical community in Czechia was a wonderful experience&quot;</p>
+  <div class="w3-content w3-center"><img src="/Images/gesprek.jpg" alt="Monastery garden" class="w3-image" />
+    <div class="w3-container w3-center fotobijschrift">Discussion in the monastery cloister, Bechyn&#283;</div>
+  </div>
   <p class="citaat">Wilma Luining: &quot;Since everybody in the course
-   loves music, it never takes long to get to know the others, even if you have
-   come on your own.&quot;</p>
+    loves music, it never takes long to get to know the others, even if you have
+    come on your own.&quot;</p>
   <p class="citaat">Fransje
-   Eiselin: &quot;For me, travelling is difficult as I am blind. However this is
-   my third time in Bechyne. It is easy to hitch rides with other particpants,
-   and travelling together adds to the fun.&quot;</p>
+    Eiselin: &quot;For me, travelling is difficult as I am blind. However this is
+    my third time in Bechyne. It is easy to hitch rides with other particpants,
+    and travelling together adds to the fun.&quot;</p>
   <p class="citaat">Martin Ashton: &quot;After every rehearsal we brass
-   players feel an enormous need to taste the Czech beer in the little pub on
-   the corner.&quot;</p>
-<div class="w3-content w3-center"><img src="/Images/orkest2.jpg" alt="The brass section" class="w3-image"/>
-  <div class="w3-container w3-center fotobijschrift">Orchestra in action in the Riding School, Bechyn&#283;</div>
-</div>
+    players feel an enormous need to taste the Czech beer in the little pub on
+    the corner.&quot;</p>
+  <div class="w3-content w3-center"><img src="/Images/orkest2.jpg" alt="The brass section" class="w3-image" />
+    <div class="w3-container w3-center fotobijschrift">Orchestra in action in the Riding School, Bechyn&#283;</div>
+  </div>
   <p class="citaat">Martha Petringmeier: &quot;From Germany, where I live,
-   Czechia is not far at all, but it is exotic enough to have that
-   feeling of being on a holiday somewhere far away.&quot;</p>
+    Czechia is not far at all, but it is exotic enough to have that
+    feeling of being on a holiday somewhere far away.&quot;</p>
   <h2>Course programme</h2>
   <p><i>La Pellegrina </i> organizes two projects in the upcoming summer.</p>
-<!-- InstanceEndEditable -->
+  <!-- InstanceEndEditable -->
 </body>
-<!-- InstanceEnd --></html>
+<!-- InstanceEnd -->
+
+</html>
--- NL/over_pellegrina.php
+++ NL/over_pellegrina.php
@@ -1,88 +1,91 @@
 ﻿<!doctype html>
 <html><!-- InstanceBegin template="/Templates/leeg_NL.dwt.php" codeOutsideHTMLIsLocked="false" -->
+
 <head>
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
 
-   <!-- CSS: -->
-   <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
+  <!-- CSS: -->
+  <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
 
-<!-- InstanceBeginEditable name="doctitle" -->
-<title>Naamloos document</title>
-<!-- InstanceEndEditable -->
-<!-- InstanceBeginEditable name="head" -->
-<!-- InstanceEndEditable -->
+  <!-- InstanceBeginEditable name="doctitle" -->
+  <title>Naamloos document</title>
+  <!-- InstanceEndEditable -->
+  <!-- InstanceBeginEditable name="head" -->
+  <!-- InstanceEndEditable -->
 </head>
 
 <body>
-<!-- InstanceBeginEditable name="mainpage" -->
-<h2 class="begin"><em>La Pellegrina</em></h2>
-<p><em>La Pellegrina</em> werd in 1988 door danseres Dorothée Wortelboer
-  en musicus Dirkjan Horringa opgericht om zomercursussen in het buitenland
-  mogelijk te maken. De eerste jaren vonden deze cursussen in Italië plaats, met historische dans en -muziek als onderwerp. Vanaf 1991 vinden er jaarlijks cursussen plaats met een veel wijdere keus van muziek, in Tsjechië en Nederland - komende zomer dus voor de 34e keer.</p>
-<h2>Wat betekent <em>La Pellegrina</em>?</h2>
-<p><em>La Pellegrina </em>(de pelgrimse, vrouwelijke pelgrim dus) is de naam van een Italiaans toneelstuk uit de 16e eeuw. Toen dit toneelstuk bij een huwelijk in de Medici familie in 1589 in Florence zou worden opgevoerd, besloot men het te verlevendigen door tussen de vijf aktes van het toneelstuk bij wijze van intermezzi spektakels te spelen. Het was een serie spektakels waarin gezongen, gespeeld en gedanst werd en vernuftige toneelmachines voor in die tijd ongekende effecten zorgden, zoals goden die afdaalden uit de hemel op een wolk, grotten die zich openden en bootjes die rondvoeren op het toneel. Ze maakten zoveel indruk op tijdgenoten dat ze een trend zetten die tot de geboorte van de opera als kunstvorm leidde. De <em>Intermedii per La Pellegrina</em> uit 1589 waren in feite één van de eerste opera-achtige producties, zonder welke de <em>Orfeo</em> van Monteverdi ondenkbaar was geweest. </p>
-<p>In 1989 waren Dorothée Wortelboer en Dirkjan Horringa uitgenodigd om een zomercursus in Italië te geven met de <em>Intermedii per La Pellegrina</em> als onderwerp. De organisatie die hen had uitgenodigd en dit project liet plaatsvinden ging failliet. Daarop besloten Dorothée Wortelboer en Dirkjan Horringa het dan maar zelf te organiseren. Ze moesten daarvoor een organisatie oprichten; de naam <em>La Pellegrina</em> lag voor de hand... Hoewel het begin lag bij historische dans en opera, is dit allang niet meer het enige onderwerp waarmee <em>La Pellegrina</em> zich bezig houdt. </p>
-<h2>Van Barok tot nu</h2>
-<p> <i>La Pellegrina</i> richt zich op muziek uit de Barok, de Klassieke
-  tijd, de Romantiek en de twintigste eeuw tot en met nu. In de cursussen in Tsjechië staat de rijke Tsjechische muziekcultuur centraal, met uitstapjes naar Duitse, Oostenrijkse, Slowaakse en Hongaarse 
-  componisten, die immers uit het zelfde cultuurgebied komen. Voor alle cursussen geldt, dat de eeuwenoude gebouwen waar repetities en concerten plaatsvinden voor een unieke sfeer en extra inspiratie zorgen.	</p>
-<p><a href="vorigeprojecten.php">Projecten uit voorgaande jaren</a></p>
-<p class="citaat">Marinus de Mooij: &quot;Ik heb enorm veel geleerd.
-  Door het grote aantal docenten is het soms net of je privé-les
-  krijgt.&quot;</p>
-<h2> Intensieve begeleiding door vakmusici</h2>
+  <!-- InstanceBeginEditable name="mainpage" -->
+  <h2 class="begin"><em>La Pellegrina</em></h2>
+  <p><em>La Pellegrina</em> werd in 1988 door danseres Dorothée Wortelboer
+    en musicus Dirkjan Horringa opgericht om zomercursussen in het buitenland
+    mogelijk te maken. De eerste jaren vonden deze cursussen in Italië plaats, met historische dans en -muziek als onderwerp. Vanaf 1991 vinden er jaarlijks cursussen plaats met een veel wijdere keus van muziek, in Tsjechië en Nederland - komende zomer dus voor de 35e keer.</p>
+  <h2>Wat betekent <em>La Pellegrina</em>?</h2>
+  <p><em>La Pellegrina </em>(de pelgrimse, vrouwelijke pelgrim dus) is de naam van een Italiaans toneelstuk uit de 16e eeuw. Toen dit toneelstuk bij een huwelijk in de Medici familie in 1589 in Florence zou worden opgevoerd, besloot men het te verlevendigen door tussen de vijf aktes van het toneelstuk bij wijze van intermezzi spektakels te spelen. Het was een serie spektakels waarin gezongen, gespeeld en gedanst werd en vernuftige toneelmachines voor in die tijd ongekende effecten zorgden, zoals goden die afdaalden uit de hemel op een wolk, grotten die zich openden en bootjes die rondvoeren op het toneel. Ze maakten zoveel indruk op tijdgenoten dat ze een trend zetten die tot de geboorte van de opera als kunstvorm leidde. De <em>Intermedii per La Pellegrina</em> uit 1589 waren in feite één van de eerste opera-achtige producties, zonder welke de <em>Orfeo</em> van Monteverdi ondenkbaar was geweest. </p>
+  <p>In 1989 waren Dorothée Wortelboer en Dirkjan Horringa uitgenodigd om een zomercursus in Italië te geven met de <em>Intermedii per La Pellegrina</em> als onderwerp. De organisatie die hen had uitgenodigd en dit project liet plaatsvinden ging failliet. Daarop besloten Dorothée Wortelboer en Dirkjan Horringa het dan maar zelf te organiseren. Ze moesten daarvoor een organisatie oprichten; de naam <em>La Pellegrina</em> lag voor de hand... Hoewel het begin lag bij historische dans en opera, is dit allang niet meer het enige onderwerp waarmee <em>La Pellegrina</em> zich bezig houdt. </p>
+  <h2>Van Barok tot nu</h2>
+  <p> <i>La Pellegrina</i> richt zich op muziek uit de Barok, de Klassieke
+    tijd, de Romantiek en de twintigste eeuw tot en met nu. In de cursussen in Tsjechië staat de rijke Tsjechische muziekcultuur centraal, met uitstapjes naar Duitse, Oostenrijkse, Slowaakse en Hongaarse
+    componisten, die immers uit het zelfde cultuurgebied komen. Voor alle cursussen geldt, dat de eeuwenoude gebouwen waar repetities en concerten plaatsvinden voor een unieke sfeer en extra inspiratie zorgen. </p>
+  <p><a href="vorigeprojecten.php">Projecten uit voorgaande jaren</a></p>
+  <p class="citaat">Marinus de Mooij: &quot;Ik heb enorm veel geleerd.
+    Door het grote aantal docenten is het soms net of je privé-les
+    krijgt.&quot;</p>
+  <h2> Intensieve begeleiding door vakmusici</h2>
   <div class="w3-container w3-right"><img src="/Images/blij_kwartet.jpg" class="w3-image" alt="Quartet relaxes" />
     <div class="w3-container w3-center fotobijschrift">Strijkkwartet op ontspannen moment</div>
   </div>
-<p>De motor achter de projecten
-  is dirigent Dirkjan Horringa. Hij werkt samen met een team van gespecialiseerde
-  musici uit binnen- en buitenland. 'Tien
-  dagen lang repeteer je zo'n zes uur per dag,' legt hij uit. 'De helft van
-  de tijd speel je onder begeleiding van vakmusici. Je hebt continu de beschikking
-  over docenten, instrumenten en stemmen. Zo kun je dus ook buiten de repetities
-  om musiceren, zelfs in bezettingen waar je thuis niet zo snel mensen voor
-  vindt.'</p>
-<p class="citaat">Erika Ploeger: &quot;Het is een echte kick. In tien
-  dagen werk je naar een concert toe, dat is zo'n geweldig hoogtepunt! En erna
-  salonmuziek spelen op het slotfeest.&quot;</p>
-<h2>Op een goed niveau</h2>
-<p> De projecten van <i>La Pellegrina</i> zijn voor kamerkoor en -orkest, voor symfonie-orkest
-  en kamermuziek en voor oude muziek voor zangers en oude instrumenten.
-  Voor enthousiaste zangers en instrumentalisten valt er dus veel
-  te doen op een goed muzikaal niveau.</p>
-<div class="w3-content w3-center "><img src="/Images/orkest.jpg" alt="Orchestral concert" class="w3-image"/>
+  <p>De motor achter de projecten
+    is dirigent Dirkjan Horringa. Hij werkt samen met een team van gespecialiseerde
+    musici uit binnen- en buitenland. 'Tien
+    dagen lang repeteer je zo'n zes uur per dag,' legt hij uit. 'De helft van
+    de tijd speel je onder begeleiding van vakmusici. Je hebt continu de beschikking
+    over docenten, instrumenten en stemmen. Zo kun je dus ook buiten de repetities
+    om musiceren, zelfs in bezettingen waar je thuis niet zo snel mensen voor
+    vindt.'</p>
+  <p class="citaat">Erika Ploeger: &quot;Het is een echte kick. In tien
+    dagen werk je naar een concert toe, dat is zo'n geweldig hoogtepunt! En erna
+    salonmuziek spelen op het slotfeest.&quot;</p>
+  <h2>Op een goed niveau</h2>
+  <p> De projecten van <i>La Pellegrina</i> zijn voor kamerkoor en -orkest, voor symfonie-orkest
+    en kamermuziek en voor oude muziek voor zangers en oude instrumenten.
+    Voor enthousiaste zangers en instrumentalisten valt er dus veel
+    te doen op een goed muzikaal niveau.</p>
+  <div class="w3-content w3-center "><img src="/Images/orkest.jpg" alt="Orchestral concert" class="w3-image" />
     <div class="w3-container fotobijschrift">Concert in de voormalige manege, Bechyn&#283;</div>
-</div>
-<p class="citaat">Evelien Tollenaar: &quot;Alles wordt voor je geregeld.
-  Je pakt je instrument in en vertrekt. Na aankomst in Bechyně is
-  het tien dagen feest, onbekommerd muziekmaken, met een leger van
-  goede muzikanten binnen handbereik.&quot; </p>
-<div class="w3-content w3-center"><img src="/Images/trio.jpg" alt="Trio" class="w3-image"/>
-  <div class="w3-container w3-center fotobijschrift">Trio tijdens kamermuziekrepetitie</div>
-</div>
-<p class="citaat">Jonelle Soelling: &quot;De voorbereiding om deel in dit muziekfestival heeft me echt gemotiveerd om een hoger niveau van spelen te bereiken. Het was een geweldige ervaring onderdeel van een internationale muzikale gemeenschap in Tsjechië te zijn&quot;</p>
-<div class="w3-content w3-center"><img src="/Images/gesprek.jpg" alt="Monastery garden" class="w3-image"/>
-  <div class="w3-container w3-center fotobijschrift">Gesprek in de kloostertuin, Bechyn&#283;</div>
-</div>
-<p class="citaat">Wilma Luining: &quot;Iedereen op het project houdt van
-  muziek. Je hebt daardoor snel contact met elkaar, ook als je in je eentje komt.&quot;</p>
-<p class="citaat">Fransje
-  Eiselin: &quot;Het reizen was voor mij een grote drempel, omdat ik blind ben.
-  Maar ik ga nu al voor de derde keer mee
-  en er
-  is altijd wel iemand met
-  wie ik mee kan rijden. Dat is nog gezellig ook.&quot;</p>
-<p class="citaat">Martin Ashton: &quot;Na een dag hoorn spelen de kroeg
-  in en genieten van het Tsjechische bier.&quot;</p>
-<div class="w3-content w3-center"><img src="/Images/orkest2.jpg" alt="The brass section" class="w3-image"/>
-  <div class="w3-container w3-center fotobijschrift">Orkest in actie in de voormalige manege van het kasteel, Bechyn&#283;</div>
-</div>
-<p class="citaat">Martha Petringmeier: &quot;Vanuit Duitsland is Tsjechië
-  niet ver, maar je hebt het gevoel dat je echt in het buitenland
-  zit.&quot;</p>
-<h2>Cursusaanbod komende zomer </h2>
-<p>In de komende zomer organiseert <i>La Pellegrina</i> twee zomercursussen, één in Nederland, één in Tsjechië.</p>
-<!-- InstanceEndEditable -->
+  </div>
+  <p class="citaat">Evelien Tollenaar: &quot;Alles wordt voor je geregeld.
+    Je pakt je instrument in en vertrekt. Na aankomst in Bechyně is
+    het tien dagen feest, onbekommerd muziekmaken, met een leger van
+    goede muzikanten binnen handbereik.&quot; </p>
+  <div class="w3-content w3-center"><img src="/Images/trio.jpg" alt="Trio" class="w3-image" />
+    <div class="w3-container w3-center fotobijschrift">Trio tijdens kamermuziekrepetitie</div>
+  </div>
+  <p class="citaat">Jonelle Soelling: &quot;De voorbereiding om deel in dit muziekfestival heeft me echt gemotiveerd om een hoger niveau van spelen te bereiken. Het was een geweldige ervaring onderdeel van een internationale muzikale gemeenschap in Tsjechië te zijn&quot;</p>
+  <div class="w3-content w3-center"><img src="/Images/gesprek.jpg" alt="Monastery garden" class="w3-image" />
+    <div class="w3-container w3-center fotobijschrift">Gesprek in de kloostertuin, Bechyn&#283;</div>
+  </div>
+  <p class="citaat">Wilma Luining: &quot;Iedereen op het project houdt van
+    muziek. Je hebt daardoor snel contact met elkaar, ook als je in je eentje komt.&quot;</p>
+  <p class="citaat">Fransje
+    Eiselin: &quot;Het reizen was voor mij een grote drempel, omdat ik blind ben.
+    Maar ik ga nu al voor de derde keer mee
+    en er
+    is altijd wel iemand met
+    wie ik mee kan rijden. Dat is nog gezellig ook.&quot;</p>
+  <p class="citaat">Martin Ashton: &quot;Na een dag hoorn spelen de kroeg
+    in en genieten van het Tsjechische bier.&quot;</p>
+  <div class="w3-content w3-center"><img src="/Images/orkest2.jpg" alt="The brass section" class="w3-image" />
+    <div class="w3-container w3-center fotobijschrift">Orkest in actie in de voormalige manege van het kasteel, Bechyn&#283;</div>
+  </div>
+  <p class="citaat">Martha Petringmeier: &quot;Vanuit Duitsland is Tsjechië
+    niet ver, maar je hebt het gevoel dat je echt in het buitenland
+    zit.&quot;</p>
+  <h2>Cursusaanbod komende zomer </h2>
+  <p>In de komende zomer organiseert <i>La Pellegrina</i> twee zomercursussen, één in Nederland, één in Tsjechië.</p>
+  <!-- InstanceEndEditable -->
 </body>
-<!-- InstanceEnd --></html>
+<!-- InstanceEnd -->
+
+</html>
