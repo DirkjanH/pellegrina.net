e6ea0a19ace6ed6c705e1e82c797c1d01a3d6036
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-13 20:41:46 +0100

    txt

--- EN/baroque/programma.php
+++ EN/baroque/programma.php
@@ -126,10 +126,10 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/cursusdata.php'; ?>
           </li>
         <p>
         <div class="fotolinks"><img src="\Images\purcell.jpg"
-            alt="Henry Purcell"><br>Henry Purcell</div>Purcell
-        contributed two Odes to St. Cecilia: <i>Welcome to all the
-          pleasures</i> (Z.339) was written by Purcell to a text by
-        Christopher Fishburn in 1683. The second, <i>Hail! Bright
+            alt="Henry Purcell" width="250"><br>Henry Purcell</div>
+        Purcell contributed two Odes to St. Cecilia: <i>Welcome to all
+          the pleasures</i> (Z.339) was written by Purcell to a text
+        by Christopher Fishburn in 1683. The second, <i>Hail! Bright
           Cecilia </i>(Z.328), also known as The Ode to St. Cecilia,
         was composed to a text by the Irishman Nicholas Brady in 1692.
         </li>
