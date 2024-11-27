80270d125485c8d179d81ce3d3a66302f5b04cb4
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-27 23:43:19 +0100

    dr

--- includes/navigatie.tijdelijk.EN.php
+++ includes/navigatie.tijdelijk.EN.php
@@ -14,7 +14,8 @@
     <a class="grijs w3-bar-item w3-button" <!--href="aanmelding.php"
         target="_parent">Registration</a>
     <div class="w3-dropdown-click">
-        <button class="w3-button" onclick="myFunction()"><em>La Pellegrina</em>
+        <button class="w3-bar-item w3-button" onclick="myFunction()"><em>La
+                Pellegrina</em>
             <i class="fa fa-caret-down"></i></button>
         <div id="demo" class="w3-dropdown-content w3-bar-block w3-card">
             <a class="w3-bar-item w3-button" href="over_pellegrina.php">About
