2f4b8ab024ee6f1dfe47ba57ffea50e89e0ec32a
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-27 19:48:00 +0100

    nav

--- includes/navigatie.tijdelijk.NL.php
+++ includes/navigatie.tijdelijk.NL.php
@@ -9,49 +9,49 @@
     <a href="index.php" target="_parent"
         class="w3-bar-item w3-button">{cursus}</a>
     <a href="programma.php" class="w3-bar-item w3-button">Programma</a>
-    <li class="grijs"><a <!--href="cursus.php"
-            class="w3-bar-item w3-button">Cursusdetails</a>
-        <a class="grijs w3-bar-item w3-button" <!--href="aanmelding.php"
-            target="_parent" -->Aanmelding</a>
-        <div class="w3-accordion">
-            <a onclick="myAccFunc('LP')" href="#"><em>La Pellegrina</em> <i
-                    class="fa fa-caret-down"></i></a>
-            <div id="LP" class="w3-accordion-content w3-white w3-card-4">
-                <a href="over_pellegrina.php" class="w3-bar-item w3-button">Over
-                    <em>La Pellegrina</em></a>
-                <a href="vorigeprojecten.php"
-                    class="w3-bar-item w3-button">Eerdere projecten</a>
-                <a href="/algemeen/werken.php"
-                    class="w3-bar-item w3-button">Eerder uitgevoerde werken</a>
-                <a href="contact.php" target="_parent"
-                    class="w3-bar-item w3-button">Contact & links</a>
-            </div>
+    <a <!--href="cursus.php"
+        class="grijs w3-bar-item w3-button">Cursusdetails</a>
+    <a class="grijs w3-bar-item w3-button" <!--href="aanmelding.php"
+        target="_parent" -->Aanmelding</a>
+    <div class="w3-accordion">
+        <a onclick="myAccFunc('LP')" href="#"><em>La Pellegrina</em> <i
+                class="fa fa-caret-down"></i></a>
+        <div id="LP" class="w3-accordion-content w3-white w3-card-4">
+            <a href="over_pellegrina.php" class="w3-bar-item w3-button">Over
+                <em>La Pellegrina</em></a>
+            <a href="vorigeprojecten.php" class="w3-bar-item w3-button">Eerdere
+                projecten</a>
+            <a href="/algemeen/werken.php" class="w3-bar-item w3-button">Eerder
+                uitgevoerde werken</a>
+            <a href="contact.php" target="_parent"
+                class="w3-bar-item w3-button">Contact & links</a>
         </div>
-        <a href="plaats.php" target="_parent"
-            class="w3-bar-item w3-button">{locatie}, de locatie</a>
-        <a href="docenten.php" target="_parent"
-            class="w3-bar-item w3-button">Docenten</a>
-        <a class="grijs w3-bar-item w3-button" <!--href="praktisch.php"
-            target="_parent" -->Prijzen & praktische zaken</a>
-        <a href="faq.php" target="_parent" class="w3-bar-item w3-button">Vaak
-            gestelde vragen</a>
-        <a href="route_plek.php" target="_parent"
-            class="w3-bar-item w3-button">Reisinformatie</a>
-        <a href="adres_toevoegen.php" target="_parent"
-            class="w3-bar-item w3-button w3-text-red">Informatie ontvangen? Geef
-            E-mail op!</a>
-        <a class="grijs w3-bar-item w3-button" <!--href="login.php"
-            target="_parent" -->Speciaal voor deelnemers</a>
-        <div class="w3-content w3-center">
-            <a href="/EN/index.php" target="_parent"> <img
-                    src="/Images/Logos/Vlag_UK.jpg"
-                    alt="Ga naar de Engelstalige site" width="34" height="24"
-                    class="geenlijn" /></a> <a
-                href="http://www.facebook.com/pellegrina.net"
-                title="Vind La Pellegrina op Facebook" target="_blank"><img
-                    src="/Images/Logos/facebook_logo.png" alt="Facebook"
-                    width="25" height="25" class="geenlijn" /></a>
-        </div>
+    </div>
+    <a href="plaats.php" target="_parent"
+        class="w3-bar-item w3-button">{locatie}, de locatie</a>
+    <a href="docenten.php" target="_parent"
+        class="w3-bar-item w3-button">Docenten</a>
+    <a class="grijs w3-bar-item w3-button" <!--href="praktisch.php"
+        target="_parent" -->Prijzen & praktische zaken</a>
+    <a href="faq.php" target="_parent" class="w3-bar-item w3-button">Vaak
+        gestelde vragen</a>
+    <a href="route_plek.php" target="_parent"
+        class="w3-bar-item w3-button">Reisinformatie</a>
+    <a href="adres_toevoegen.php" target="_parent"
+        class="w3-bar-item w3-button w3-text-red">Informatie ontvangen? Geef
+        E-mail op!</a>
+    <a class="grijs w3-bar-item w3-button" <!--href="login.php" target="_parent"
+        -->Speciaal voor deelnemers</a>
+    <div class="w3-content w3-center">
+        <a href="/EN/index.php" target="_parent"> <img
+                src="/Images/Logos/Vlag_UK.jpg"
+                alt="Ga naar de Engelstalige site" width="34" height="24"
+                class="geenlijn" /></a> <a
+            href="http://www.facebook.com/pellegrina.net"
+            title="Vind La Pellegrina op Facebook" target="_blank"><img
+                src="/Images/Logos/facebook_logo.png" alt="Facebook" width="25"
+                height="25" class="geenlijn" /></a>
+    </div>
 </nav>
 <script>
 function w3_open() {
