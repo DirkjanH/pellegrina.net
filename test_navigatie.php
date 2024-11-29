488b93c91bf39a684e642f2bd1ef8e72a4937535
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-29 16:44:58 +0100

    test

---
+++ test_navigatie.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$_SESSION['test'] = 'test';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Session test</title>
+</head>
+
+<body> <?php session_status(); ?> </body>
+
+</html>
