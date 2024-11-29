8ec8cf275cd335c45d5ae120a2d6b3402282d670
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-29 12:13:38 +0100

    bud

--- EN/budejovice.php
+++ EN/budejovice.php
@@ -1,110 +1,109 @@
 <!doctype html>
 <html>
 <!-- InstanceBegin template="/Templates/leeg_EN.dwt.php" codeOutsideHTMLIsLocked="false" -->
-
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta charset="utf-8">
-  <!-- CSS: -->
-  <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
-  <!-- InstanceBeginEditable name="doctitle" -->
-  <title>Naamloos document</title>
-  <!-- InstanceEndEditable -->
-  <!-- InstanceBeginEditable name="head" -->
-  <!-- InstanceEndEditable -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <!-- CSS: -->
+    <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
+    <!-- InstanceBeginEditable name="doctitle" -->
+    <title>Naamloos document</title>
+    <!-- InstanceEndEditable -->
+    <!-- InstanceBeginEditable name="head" -->
+    <!-- InstanceEndEditable -->
 </head>
-
 <body>
-  <!-- InstanceBeginEditable name="mainpage" -->
-  <h2 class="begin">České Budějovice, Czechia</h2>
-  <div class="fotorechts"><img src="/Images/Logos/mapacz.gif"
-      alt="Kaart van Tsjechië" width="320" height="185"
-      class="w3-image geenlijn" /></div>
-  <p>České Budějovice (also known by its German name Budweis) was founded at
-    the site of the confluence of the river Vltava (Moldau) and Mal&scaron;e
-    in 1265. The historic town is still mostly in medieval state. The
-    Dominican monastery stems from the 13th century. The Gothic-Renaissance
-    Cern&aacute; ve&#382; (Black Tower) is with its height of 72 meter a
-    striking dominant on the city skyline and offers a delightful view over
-    the city and its surroundings, including the nearby Šumava mountains. On
-    the diamond square one of the largest fountains of Czechia is situated -
-    the Samson Fountain. The city has also become world famous thanks to the
-    Budvar beer, imitated in America under the name Budweiser. Today it is
-    the seat of the Province of South Bohemia. </p>
-  <div class="fotocenter"><img
-      src="/Images/Locaties/Budejovice/plein_vanaf_toren.jpg"
-      alt="The central square, seen from the Black Tower" width="600"
-      height="400" class="w3-image" border="1" /><br /> The central
-    square, seen from the Black Tower</div>
-  <div class="container">
-    <h4>The conservatoire</h4>
+    <!-- InstanceBeginEditable name="mainpage" -->
+    <h2 class="begin">České Budějovice, Czechia</h2>
+    <div class="fotorechts"><img src="/Images/Logos/mapacz.gif"
+            alt="Kaart van Tsjechië" width="320" height="185"
+            class="w3-image geenlijn" /></div>
+    <p>České Budějovice (also known by its German name Budweis) was founded at
+        the site of the confluence of the river Vltava (Moldau) and Mal&scaron;e
+        in 1265. The historic town is still mostly in medieval state. The
+        Dominican monastery stems from the 13th century. The Gothic-Renaissance
+        Cern&aacute; ve&#382; (Black Tower) is with its height of 72 meter a
+        striking dominant on the city skyline and offers a delightful view over
+        the city and its surroundings, including the nearby Šumava mountains. On
+        the diamond square one of the largest fountains of Czechia is situated -
+        the Samson Fountain. The city has also become world famous thanks to the
+        Budvar beer, imitated in America under the name Budweiser. Today it is
+        the seat of the Province of South Bohemia. </p>
     <div class="fotocenter"><img
-        src="/Images/Locaties/Budejovice/konzervator3.jpg"
-        alt="The conservatoire, hostel and venue for rehearsals"
-        width="600" height="400" border="1" class="w3-image" /> <br />
-      The conservatoire, hostel and venue for rehearsals</div>
-    <div class="fotorechts"><img
-        src="/Images/Locaties/Budejovice/domov_mladeze.jpg"
-        alt="Room at the hostel" class="w3-image" width="500"
-        height="298" /><br /> A room in the hostel of the conservatoire
+            src="/Images/Locaties/Budejovice/plein_vanaf_toren.jpg"
+            alt="The central square, seen from the Black Tower" width="600"
+            height="400" class="w3-image" border="1" /><br /> The central
+        square, seen from the Black Tower</div>
+    <div class="container">
+        <h4>The conservatoire</h4>
+        <div class="fotocenter"><img
+                src="/Images/Locaties/Budejovice/konzervator3.jpg"
+                alt="The conservatoire, hostel and venue for rehearsals"
+                width="600" height="400" border="1" class="w3-image" /> <br />
+            The conservatoire, hostel and venue for rehearsals</div>
+        <div class="fotorechts"><img
+                src="/Images/Locaties/Budejovice/domov_mladeze.jpg"
+                alt="Room at the hostel" class="w3-image" width="500"
+                height="298" /><br /> A room in the hostel of the conservatoire
+        </div>
+        <p>The participants can opt for accommodation in the hostel of the
+            conservatoire. This building offers accommodation in quiet single
+            and double rooms, all with private bathrooms.</p>
+        <p class="citaat">Lia van den Heuvel: "La Pellegrina is 10 days of great
+            music, friendly people and beautiful surroundings."</p>
     </div>
-    <p>The participants can opt for accommodation in the hostel of the
-      conservatoire. This building offers accommodation in quiet single
-      and double rooms, all with private bathrooms.</p>
-    <p class="citaat">Lia van den Heuvel: "La Pellegrina is 10 days of great
-      music, friendly people and beautiful surroundings."</p>
-  </div>
-  <div class="container">
-    <div class="fotolinks"><img
-        src="/Images/Locaties/Budejovice/Room 27.jpg" width="500"
-        height="375" alt="classroom" /><br /> Classroom with two grand
-      pianos at the conservatoire</div>
-    <p>The conservatoire is equipped with spacious, well-sounding classrooms
-      with well-maintained grand pianos and upright pianos. A leading
-      piano tuner comes from Prague to tune all instruments before the
-      course. The building has recently been renovated and now has a light
-      and pleasant appearance.</p>
-  </div>
-  <div class="container">
-    <h4>The concert hall</h4>
-    <div class="fotorechts"><img
-        src="/Images/Locaties/Budejovice/konzervator_zaal.JPG"
-        alt="The concert hall" class="w3-image" width="500"
-        height="284" /><br /> The concert hall</div>
-    <p>The concert hall in the conservatoire, venue for the tutors' concert
-      and rehearsals of orchestra and choir. Since the reconstruction of
-      the building this hall has air conditioning.</p>
-    <p class="citaat">Robert Beverly: "La Pellegrina brings out the best in
-      a rather diverse and eclectic group of individuals - dedication,
-      motivation, cooperation, and the unquenchable joy of making music."
-    </p>
-  </div>
-  <div class="container">
-    <h4>Meals </h4>
-    <div class="fotolinks"><img
-        src="/Images/Locaties/Budejovice/masne_kramy.jpg" width="500"
-        height="378" class="w3-image" alt="Masné krámy" /><br /> The old
-      town of České Budějovice has many ancient buildings, such as this
-      market hall from 1336, now in use as a restaurant</div>
-    <p>For those staying in the conservatory, there is a breakfast buffet in
-      the entrance hall and at a nearby restaurant, where lunch is also
-      served in the form of a buffet. Dinner is not included in the course
-      price, except on the days of arrival and of the final concert. In
-      the boarding school of the conservatory there is a kitchen in each
-      corridor where you can prepare something yourself. In the nearby
-      historic centre you find many good restaurants for every budget.</p>
-    <p class="citaat">Laurie Boltjes: 'I always go on a long cycling tour
-      after the course. Sometimes some of the other participants join me
-      for a short holiday.'</p>
-    <p class="citaat">Katy en Jan de Jongh: 'Before the course, we stay in
-      Prague for a few days, and afterwards, we have a quiet holiday near
-      the town. Bohemia is so cheap that we can afford to stay a little
-      longer.' </p>
-  </div>
-  <h2>Photos</h2>
-  <p>See also in <a href="https://delveintoeurope.com/ceske-budejovice/"
-      target="_blank">this photo album</a> for more photos of České
-    Budějovice</p>
+    <div class="container">
+        <div class="fotolinks"><img
+                src="/Images/Locaties/Budejovice/Room 27.jpg" width="500"
+                height="375" alt="classroom" /><br /> Classroom with two grand
+            pianos at the conservatoire</div>
+        <p>The conservatoire is equipped with spacious, well-sounding classrooms
+            with well-maintained grand pianos and upright pianos. A leading
+            piano tuner comes from Prague to tune all instruments before the
+            course. The building has recently been renovated and now has a light
+            and pleasant appearance.</p>
+    </div>
+    <div class="container">
+        <h4>The concert hall</h4>
+        <div class="fotorechts"><img
+                src="/Images/Locaties/Budejovice/konzervator_zaal.JPG"
+                alt="The concert hall" class="w3-image" width="500"
+                height="284" /><br /> The concert hall</div>
+        <p>The concert hall in the conservatoire, venue for the tutors' concert
+            and rehearsals of orchestra and choir. Since the reconstruction of
+            the building this hall has air conditioning.</p>
+        <p class="citaat">Robert Beverly: "La Pellegrina brings out the best in
+            a rather diverse and eclectic group of individuals - dedication,
+            motivation, cooperation, and the unquenchable joy of making music."
+        </p>
+    </div>
+    <div class="container">
+        <h4>Meals </h4>
+        <div class="fotolinks"><img
+                src="/Images/Locaties/Budejovice/masne_kramy.jpg" width="500"
+                height="378" class="w3-image" alt="Masné krámy" /><br /> The old
+            town of České Budějovice has many ancient buildings, such as this
+            market hall from 1336, now in use as a restaurant</div>
+        <p>For those staying in the conservatory, there is a breakfast buffet in
+            the entrance hall and at a nearby restaurant, where lunch is also
+            served in the form of a buffet. Dinner is not included in the course
+            price, except on the days of arrival and of the final concert. In
+            the boarding school of the conservatory there is a kitchen in each
+            corridor where you can prepare something yourself. In the nearby
+            historic centre you find many good restaurants for every budget.</p>
+    </div>
+    <div class="container">
+        <p class="citaat">Laurie Boltjes: 'I always go on a long cycling tour
+            after the course. Sometimes some of the other participants join me
+            for a short holiday.'</p>
+        <p class="citaat">Katy en Jan de Jongh: 'Before the course, we stay in
+            Prague for a few days, and afterwards, we have a quiet holiday near
+            the town. Bohemia is so cheap that we can afford to stay a little
+            longer.' </p>
+    </div>
+    <h2>Photos</h2>
+    <p>See also in <a href="https://delveintoeurope.com/ceske-budejovice/"
+            target="_blank">this photo album</a> for more photos of České
+        Budějovice</p>
 </body>
-
 </html>
