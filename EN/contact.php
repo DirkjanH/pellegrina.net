bd984ff79b2cf4d522ecc807d61f83827e410edf
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-29 17:00:13 +0100

    s

--- EN/contact.php
+++ EN/contact.php
@@ -1,66 +1,64 @@
 <!doctype html>
-<html><!-- InstanceBegin template="/Templates/leeg_EN.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<html>
+
 <head>
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta charset="utf-8">
-
-   <!-- CSS: -->
-   <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
-
-<!-- InstanceBeginEditable name="doctitle" -->
-<title>Contact</title>
-<!-- InstanceEndEditable -->
-<!-- InstanceBeginEditable name="head" -->
-<!-- InstanceEndEditable -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
+  <title>Contact</title>
 </head>
 
 <body>
-<!-- InstanceBeginEditable name="mainpage" -->
-    <h2 class="begin">Contact</h2>
-    <p>If you have any questions or suggestions, please feel free to contact <i>La
-      Pellegrina</i> at:</p>
-    <p><em><strong>La Pellegrina</strong></em><br />
-      Dirkjan Horringa <br />
-      Merwedeplantsoen 73,<br />
-      3522 JZ  Utrecht, The Netherlands</p>
-    <p>Tel. +31 619 224 758<br />
-      E-mail <a href="mailto:info@pellegrina.net">info@pellegrina.net</a></p>
-    <p>Chamber of Commerce Utrecht nr. 30090647 </p>
-    <p>VAT NL001344674B18</p>
-	<h4>Bank details</h4>
-    <p>NL33 ASNB 0707 2500 72 (BIC ASNB NL21) in name of <em>La Pellegrina</em>, Utrecht</p>
-    <p>For payments from Czechia: CZ44 0100 0000 0005 3894 0301 (BIC KOMBCZPPXXX) in name of Dirk Johannes Horringa, Utrecht</p>
-    <p>For payments from the USA: Wise Account number 8310003482 (ACH and Wire routing number  026073150) in name of La Pellegrina, Utrecht NL</p>
-<h2>Imprint</h2>
-    <h4>Text</h4>
-    <p>Dirkjan Horringa</p>
-    <h4>Video</h4>
-    <p>Martin Mrkva, Bram Heemskerk, Nicky de Bie, Ank de Wit</p>
-    <h4>Design &amp; realisation</h4>
-    <p>Thomas Op de Coul, Dirkjan Horringa </p>
-    <h4>Photo's</h4>
-    <p>For illustrating this site grateful use has been made of the many
-      beautiful photographs by participants like Briana Gillet, Matthia Cox, Lea Schuiling, Wout&nbsp;van&nbsp;den&nbsp;Heuvel,
-  Cassandra Luckhardt, Désirée Staverman, Vivian Stemerdink, Patrick&nbsp;Vercammen, Petra&nbsp;Wals,
-  Joyce Vermeer, Fred Willems and Annemieke&nbsp;Zwanenburg.</p>
-    <h2>Useful musical links</h2>
-    <h3 class="begin">Music libraries and digital collections of editions</h3>
-<table border="0" cellpadding="5">
-  <tr>
-    <td><a href="http://www.Wranitzky.com" target="_blank"><img src="/Images/Logos/wranitzky-banner.gif" alt="www.Wranitzky.com" width="234" height="60" class="w3-image" /></a></td>
-    <td><p><a href="http://www.Wranitzky.com">www.Wranitzky.com</a></p>
-    <p>a wonderful website dedicated to the music of Pavel Vranický, with a great number of chamber music and orchestral scores in excellent modern edition</p></td>
+  <h2 class="begin">Contact</h2>
+  <p>If you have any questions or suggestions, please feel free to contact
+    <i>La Pellegrina</i> at:
+  </p>
+  <p><em><strong>La Pellegrina</strong></em><br /> Dirkjan Horringa <br />
+    Merwedeplantsoen 73,<br /> 3522 JZ Utrecht, The Netherlands</p>
+  <p>Tel. +31 619 224 758<br /> E-mail <a
+      href="mailto:info@pellegrina.net">info@pellegrina.net</a></p>
+  <p>Chamber of Commerce Utrecht nr. 30090647 </p>
+  <p>VAT NL001344674B18</p>
+  <h4>Bank details</h4>
+  <p>NL33 ASNB 0707 2500 72 (BIC ASNB NL21) in name of <em>La Pellegrina</em>,
+    Utrecht</p>
+  <p>For payments from Czechia: CZ44 0100 0000 0005 3894 0301 (BIC
+    KOMBCZPPXXX) in name of Dirk Johannes Horringa, Utrecht</p>
+  <p>For payments from the USA: Wise Account number 8310003482 (ACH and Wire
+    routing number 026073150) in name of La Pellegrina, Utrecht NL</p>
+  <h2>Imprint</h2>
+  <h4>Text</h4>
+  <p>Dirkjan Horringa</p>
+  <h4>Video</h4>
+  <p>Martin Mrkva, Bram Heemskerk, Nicky de Bie, Ank de Wit</p>
+  <h4>Design &amp; realisation</h4>
+  <p>Thomas Op de Coul, Dirkjan Horringa </p>
+  <h4>Photo's</h4>
+  <p>For illustrating this site grateful use has been made of the many
+    beautiful photographs by participants like Briana Gillet, Matthia Cox,
+    Lea Schuiling, Wout&nbsp;van&nbsp;den&nbsp;Heuvel, Cassandra Luckhardt,
+    Désirée Staverman, Vivian Stemerdink, Patrick&nbsp;Vercammen,
+    Petra&nbsp;Wals, Joyce Vermeer, Fred Willems and
+    Annemieke&nbsp;Zwanenburg.</p>
+  <h2>Useful musical links</h2>
+  <h3 class="begin">Music libraries and digital collections of editions</h3>
+  <a href="http://www.Wranitzky.com" target="_blank"><img
+      src="/Images/Logos/wranitzky-banner.gif" width="234" height="60"
+      alt="www.Wranitzky.com" class="geenlijn"
+      style="float: left; margin-right: 20px;" /></a>
+  <p><a href="http://www.Wranitzky.com">www.Wranitzky.com</a>, a wonderful
+    website dedicated to the music of Pavel Vranický, with a great number of
+    chamber music and orchestral scores in excellent modern edition</p>
+  </td>
   </tr>
-</table>
-<h3>Links to comparable organisers of summer schools</h3>
-<p><a href="http://www.huismuziek.nl" target="_blank">Huismuziek</a> (in
-  Dutch)</p>
-<p><a href="http://www.iam-ev.de" target="_blank">Internationale Arbeitsgemeinschaft
-  f&uuml;r Musik</a> (in
-  German)</p>
-<p><a href="http://www.esp.ele.tue.nl/faso/index.htm" target="_blank">FASO - Federatie
-  van Amateur Symfonie- en Strijkorkesten</a> (in
-Dutch)</p>
-    <!-- InstanceEndEditable -->
+  </table>
+  <h3>Links to comparable organisers of summer schools</h3>
+  <p><a href="http://www.huismuziek.nl" target="_blank">Huismuziek</a> (in
+    Dutch)</p>
+  <p><a href="http://www.iam-ev.de" target="_blank">Internationale
+      Arbeitsgemeinschaft f&uuml;r Musik</a> (in German)</p>
+  <p><a href="http://www.esp.ele.tue.nl/faso/index.htm" target="_blank">FASO -
+      Federatie van Amateur Symfonie- en Strijkorkesten</a> (in Dutch)</p>
 </body>
-<!-- InstanceEnd --></html>
+
+</html>
