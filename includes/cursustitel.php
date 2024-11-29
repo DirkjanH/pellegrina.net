5ee0827ea53b1c8b49a242215124d5d52af27f41
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-29 12:47:45 +0100

    cur

--- includes/cursustitel.php
+++ includes/cursustitel.php
@@ -1,13 +1,13 @@
 <?php Kint::$enabled_mode = false; ?> <div class="<?php echo $filenaam[6] ?>"
-	id="titel">
-	<div id="teksten">
-		<div class="cijfer"> <?php echo $cursusdata['nr']; ?> </div>
-		<div class="tekst">
-			<h3> <?php echo $cursusdata['cursusnaam']; ?> </h3>
-			<p> <?php echo $cursusdata['ondertitel']; ?> </p>
-		</div>
-	</div>
-	<div id="cursusplaatje">&nbsp;</div>
+    id="titel">
+    <div id="teksten">
+        <div class="cijfer"> <?php echo $cursusdata['nr']; ?> </div>
+        <div class="tekst">
+            <h3> <?php echo $cursusdata['cursusnaam']; ?> </h3>
+            <p> <?php echo $cursusdata['ondertitel']; ?> </p>
+        </div>
+    </div>
+    <div id="cursusplaatje">&nbsp;</div>
 </div> <?php
 
 		d($filenaam, $opening_inschrijving, date('c'), ($opening_inschrijving > date('c')));
@@ -17,80 +17,80 @@
 		if (isset($filenaam[7]) and $filenaam[7] == 'cursus') {
 			if (isset($filenaam[5]) and $filenaam[5] == 'NL') {
 				$menu = <<<EOD
-	<div id="cursusmenu" class="w3-bar w3-border w3-card-4 {$filenaam[6]}">
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#programma" target="_self">De
-    muziek</a> 
+	<div id="cursusmenu" class="w3-bar w3-border w3-card-4">
+	<a class="w3-bar-item w3-button w3-mobile" href="#programma" target="_self"><span class="{$filenaam[6]}">De
+    muziek</span></a> 
 EOD;
 
 				if (isset($filenaam[6]) and $filenaam[6] == 'romantic')
 					$menu .= <<<EOD
-<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#kennismaking"
-    target="_self">Kennismakingsrepetitie</a>
+<a class="w3-bar-item w3-button w3-mobile" href="#kennismaking"
+    target="_self"><span class="{$filenaam[6]}">Kennismakingsrepetitie</span></a>
 EOD;
 
 				if ($opening_inschrijving > date('c')) {
 					$menu .= <<<EOD
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}"
-    href="#voorwie" target="_self">Voor wie</a>
-    <a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#dagindeling"
-        target="_self">Week- en dagindeling</a>
-    <a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#waar"
-        target="_self">Waar</a>
-    <a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#metwie" target="_self">Met
-        wie</a>
+	<a class="w3-bar-item w3-button w3-mobile"
+    href="#voorwie" target="_self"><span class="{$filenaam[6]}">Voor wie</span></a>
+    <a class="w3-bar-item w3-button w3-mobile" href="#dagindeling"
+        target="_self"><span class="{$filenaam[6]}">Week- en dagindeling</span></a>
+    <a class="w3-bar-item w3-button w3-mobile" href="#waar"
+        target="_self"><span class="{$filenaam[6]}">Waar</span></a>
+    <a class="w3-bar-item w3-button w3-mobile" href="#metwie" target="_self"><span class="{$filenaam[6]}">Met
+        wie</span></a>
     </div> 
 EOD;
 				} else {
 					$menu .= <<<EOD
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#voorwie" 
-		target="_self">Voor wie</a>
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#dagindeling"
-		target="_self">Week- en dagindeling</a>
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#waar"
-		target="_self">Waar</a>
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#metwie"
-		target="_self">Met wie</a>
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="aanmelding.php"
-		target="_self">Aanmelding</a>
+	<a class="w3-bar-item w3-button w3-mobile" href="#voorwie" 
+		target="_self"><span class="{$filenaam[6]}">Voor wie</span></a>
+	<a class="w3-bar-item w3-button w3-mobile" href="#dagindeling"
+		target="_self"><span class="{$filenaam[6]}">Week- en dagindeling</span></a>
+	<a class="w3-bar-item w3-button w3-mobile" href="#waar"
+		target="_self"><span class="{$filenaam[6]}">Waar</span></a>
+	<a class="w3-bar-item w3-button w3-mobile" href="#metwie"
+		target="_self"><span class="{$filenaam[6]}">Met wie</span></a>
+	<a class="w3-bar-item w3-button w3-mobile" href="aanmelding.php"
+		target="_self"><span class="{$filenaam[6]}">Aanmelding</span></a>
 	</div> 
 EOD;
 				}
 			} else {
 				$menu = <<<EOD
-	<div id="cursusmenu" class="w3-bar w3-border w3-card-4 {$filenaam[6]}">
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#programma"
-	target="_self">The music</a> 
+	<div id="cursusmenu" class="w3-bar w3-border w3-card-4">
+	<a class="w3-bar-item w3-button w3-mobile" href="#programma"
+	target="_self"><span class="{$filenaam[6]}">The music</span></a> 
 EOD;
 
 				if (isset($filenaam[6]) and $filenaam[6] == 'romantic') $menu .= <<<EOD
-				<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#kennismaking"
-	target="_self">Introductory rehearsal</a>'
+				<a class="w3-bar-item w3-button w3-mobile" href="#kennismaking"
+	target="_self"><span class="{$filenaam[6]}">Introductory rehearsal</span></a>'
 	EOD;
 				if ($opening_inschrijving > date('c')) {
 					$menu .= <<<EOD
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#voorwie"
-	target="_self">For whom</a>
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#dagindeling"
-		target="_self">Week and day programme</a>
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#waar"
-		target="_self">Where</a>
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#metwie"
-		target="_self">With whom</a>
+	<a class="w3-bar-item w3-button w3-mobile" href="#voorwie"
+	target="_self"><span class="{$filenaam[6]}">For whom</span></a>
+	<a class="w3-bar-item w3-button w3-mobile" href="#dagindeling"
+		target="_self"><span class="{$filenaam[6]}">Week and day programme</span></a>
+	<a class="w3-bar-item w3-button w3-mobile" href="#waar"
+		target="_self"><span class="{$filenaam[6]}">Where</span></a>
+	<a class="w3-bar-item w3-button w3-mobile" href="#metwie"
+		target="_self"><span class="{$filenaam[6]}">With whom</span></a>
 	</div> 
 EOD;
 				} else {
 					$menu .= <<<EOD
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#voorwie"
-	target="_self">For whom</a>
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}"
-		href="#dagindeling" target="_self">Week and day
-		programme</a>
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#waar"
-		target="_self">Where</a>
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}" href="#metwie"
-		target="_self">With whom</a>
-	<a class="w3-bar-item w3-button w3-mobile {$filenaam[6]}"
-		href="aanmelding.php" target="_self">Registration</a>
+	<a class="w3-bar-item w3-button w3-mobile" href="#voorwie"
+	target="_self"><span class="{$filenaam[6]}">For whom</span></a>
+	<a class="w3-bar-item w3-button w3-mobile"
+		href="#dagindeling" target="_self"><span class="{$filenaam[6]}">Week and day
+		programme</span></a>
+	<a class="w3-bar-item w3-button w3-mobile" href="#waar"
+		target="_self"><span class="{$filenaam[6]}">Where</span></a>
+	<a class="w3-bar-item w3-button w3-mobile" href="#metwie"
+		target="_self"><span class="{$filenaam[6]}">With whom</span></a>
+	<a class="w3-bar-item w3-button w3-mobile"
+		href="aanmelding.php" target="_self"><span class="{$filenaam[6]}">Registration</span></a>
 	</div> 
 EOD;
 				}
