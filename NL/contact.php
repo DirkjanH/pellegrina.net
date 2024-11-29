f4af8aad607b80c8c8acbbbf8d560e6398f3eefc
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-29 16:06:28 +0100

    c

--- NL/contact.php
+++ NL/contact.php
@@ -1,83 +1,80 @@
 <!doctype html>
 <html>
 <!-- InstanceBegin template="/Templates/leeg_NL.dwt.php" codeOutsideHTMLIsLocked="false" -->
-
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta charset="utf-8">
-  <!-- CSS: -->
-  <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
-  <!-- InstanceBeginEditable name="doctitle" -->
-  <title>Contact</title>
-  <!-- InstanceEndEditable -->
-  <!-- InstanceBeginEditable name="head" -->
-  <!-- InstanceEndEditable -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <!-- CSS: -->
+    <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
+    <!-- InstanceBeginEditable name="doctitle" -->
+    <title>Contact</title>
+    <!-- InstanceEndEditable -->
+    <!-- InstanceBeginEditable name="head" -->
+    <!-- InstanceEndEditable -->
 </head>
-
 <body>
-  <!-- InstanceBeginEditable name="mainpage" -->
-  <h2 class="begin">Contact</h2>
-  <p>Bij vragen of opmerkingen kun je <i>La Pellegrina</i> als volgt bereiken:
-  </p>
-  <p><em><strong>La Pellegrina</strong></em><br /> Dirkjan Horringa <br />
-    Merwedeplantsoen 73,<br /> 3522 JZ Utrecht, Nederland</p>
-  <p>Tel. 0619 224 758 (buiten Nederland +31 619 224 758)<br /> E-mail <a
-      href="mailto:info@pellegrina.net">info@pellegrina.net</a></p>
-  <p>Kamer van Koophandel Utrecht nr. 30090647 </p>
-  <p>BTW NL001344674B18</p>
-  <h4>Bankrekening</h4>
-  <p>NL33 ASNB 0707 2500 72 (BIC ASNB NL21) t.n.v. La Pellegrina, Utrecht</p>
-  <h2>Colofon</h2>
-  <h4>Tekst</h4>
-  <p>Dirkjan Horringa</p>
-  <h4>Video</h4>
-  <p>Martin Mrkva, Bram Heemskerk, Nicky de Bie, Ank de Wit</p>
-  <h4>Design &amp; uitvoering</h4>
-  <p>Thomas Op de Coul, Dirkjan Horringa </p>
-  <h4>Foto's</h4>
-  <p>Voor het illustreren van deze site is dankbaar gebruik gemaakt van de
-    vele mooie foto's van deelnemers, waaronder Briana Gillet, Matthia Cox,
-    Lea Schuiling, Wout&nbsp;van&nbsp;den&nbsp;Heuvel, Cassandra Luckhardt,
-    Désirée Staverman, Vivian Stemerdink, Patrick&nbsp;Vercammen,
-    Petra&nbsp;Wals, Joyce Vermeer, Fred Willems en
-    Annemieke&nbsp;Zwanenburg.</p>
-  <h2 class="begin">Interessante links over muziek</h2>
-  <h3>Muziekbibliotheken en&nbsp;digitale collecties van&nbsp;edities</h3>
-  <a href="http://www.Wranitzky.com" target="_blank"><img
-      src="/Images/Logos/wranitzky-banner.gif" width="234" height="60"
-      alt="www.Wranitzky.com" class="geenlijn"
-      style="float: left; margin-right: 20px;" /></a>
-  <p><a href="http://www.Wranitzky.com">www.Wranitzky.com</a>, een prachtige
-    website gewijd aan de muziek van Pavel Vranický, met een grote
-    verzameling bladmuziek voor kamermuziek en orkest in uitstekende moderne
-    edities</p>
-  <h3>Links naar andere aanbieders van zomercursussen</h3>
-  <ul>
-    <li><a href="http://www.huismuziek.nl" target="_blank">Huismuziek</a>
-    </li>
-    <li><a href="http://www.iam-ev.de" target="_blank">Internationale
-        Arbeitsgemeinschaft f&uuml;r Musik</a></li>
-    <li><a href="http://www.esp.ele.tue.nl/faso/index.htm"
-        target="_blank">FASO - Federatie van Amateur Symfonie- en
-        Strijkorkesten</a></li>
-  </ul>
-  <h3>Algemene muziekinformatie</h3>
-  <ul>
-    <li><a href="http://blokfluit.beginthier.nl/"
-        target="_blank">Blokfluitpagina 'Beginthier'</a></li>
-    <li> <a href="http://www.gehoorprotectie.nl/" target="_blank">Specialist
-        Gehoorprotectie</a></li>
-  </ul>
-  <h3>Websites met betrekking tot zang</h3>
-  <ul>
-    <li><a href="http://zingen.startpagina.nl"
-        target="_blank">zingen.startpagina.nl</a> </li>
-    <li><a href="http://www.zingmagazine.nl/" target="_blank"><img
-          src="https://www.zingmagazine.nl/wp-content/uploads/2018/10/Zing-header.jpg"
-          alt="Zing Magazine" width="350" class="geenlijn" /></a></li>
-  </ul>
-  <!-- InstanceEndEditable -->
+    <!-- InstanceBeginEditable name="mainpage" -->
+    <h2 class="begin">Contact</h2>
+    <p>Bij vragen of opmerkingen kun je <i>La Pellegrina</i> als volgt bereiken:
+    </p>
+    <p><em><strong>La Pellegrina</strong></em><br /> Dirkjan Horringa <br />
+        Merwedeplantsoen 73,<br /> 3522 JZ Utrecht, Nederland</p>
+    <p>Tel. 0619 224 758 (buiten Nederland +31 619 224 758)<br /> E-mail <a
+            href="mailto:info@pellegrina.net">info@pellegrina.net</a></p>
+    <p>Kamer van Koophandel Utrecht nr. 30090647 </p>
+    <p>BTW NL001344674B18</p>
+    <h4>Bankrekening</h4>
+    <p>NL33 ASNB 0707 2500 72 (BIC ASNB NL21) t.n.v. La Pellegrina, Utrecht</p>
+    <h2>Colofon</h2>
+    <h4>Tekst</h4>
+    <p>Dirkjan Horringa</p>
+    <h4>Video</h4>
+    <p>Martin Mrkva, Bram Heemskerk, Nicky de Bie, Ank de Wit</p>
+    <h4>Design &amp; uitvoering</h4>
+    <p>Thomas Op de Coul, Dirkjan Horringa </p>
+    <h4>Foto's</h4>
+    <p>Voor het illustreren van deze site is dankbaar gebruik gemaakt van de
+        vele mooie foto's van deelnemers, waaronder Briana Gillet, Matthia Cox,
+        Lea Schuiling, Wout&nbsp;van&nbsp;den&nbsp;Heuvel, Cassandra Luckhardt,
+        Anthonie Meijers, Désirée Staverman, Vivian Stemerdink,
+        Patrick&nbsp;Vercammen, Petra&nbsp;Wals, Joyce Vermeer, Fred Willems en
+        Annemieke&nbsp;Zwanenburg.</p>
+    <h2 class="begin">Interessante links over muziek</h2>
+    <h3>Muziekbibliotheken en&nbsp;digitale collecties van&nbsp;edities</h3>
+    <a href="http://www.Wranitzky.com" target="_blank"><img
+            src="/Images/Logos/wranitzky-banner.gif" width="234" height="60"
+            alt="www.Wranitzky.com" class="geenlijn"
+            style="float: left; margin-right: 20px;" /></a>
+    <p><a href="http://www.Wranitzky.com">www.Wranitzky.com</a>, een prachtige
+        website gewijd aan de muziek van Pavel Vranický, met een grote
+        verzameling bladmuziek voor kamermuziek en orkest in uitstekende moderne
+        edities</p>
+    <h3>Links naar andere aanbieders van zomercursussen</h3>
+    <ul>
+        <li><a href="http://www.huismuziek.nl" target="_blank">Huismuziek</a>
+        </li>
+        <li><a href="http://www.iam-ev.de" target="_blank">Internationale
+                Arbeitsgemeinschaft f&uuml;r Musik</a></li>
+        <li><a href="http://www.esp.ele.tue.nl/faso/index.htm"
+                target="_blank">FASO - Federatie van Amateur Symfonie- en
+                Strijkorkesten</a></li>
+    </ul>
+    <h3>Algemene muziekinformatie</h3>
+    <ul>
+        <li><a href="http://blokfluit.beginthier.nl/"
+                target="_blank">Blokfluitpagina 'Beginthier'</a></li>
+        <li> <a href="http://www.gehoorprotectie.nl/" target="_blank">Specialist
+                Gehoorprotectie</a></li>
+    </ul>
+    <h3>Websites met betrekking tot zang</h3>
+    <ul>
+        <li><a href="http://zingen.startpagina.nl"
+                target="_blank">zingen.startpagina.nl</a> </li>
+        <li><a href="http://www.zingmagazine.nl/" target="_blank"><img
+                    src="https://www.zingmagazine.nl/wp-content/uploads/2018/10/Zing-header.jpg"
+                    alt="Zing Magazine" width="350" class="geenlijn" /></a></li>
+    </ul>
+    <!-- InstanceEndEditable -->
 </body>
 <!-- InstanceEnd -->
-
 </html>
