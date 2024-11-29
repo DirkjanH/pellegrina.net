d1cf3463967680fbce221f7579c912814af002cc
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-29 17:03:48 +0100

    b

--- EN/contact.php
+++ EN/contact.php
@@ -20,45 +20,51 @@
   <p>Chamber of Commerce Utrecht nr. 30090647 </p>
   <p>VAT NL001344674B18</p>
   <h4>Bank details</h4>
-  <p>NL33 ASNB 0707 2500 72 (BIC ASNB NL21) in name of <em>La Pellegrina</em>,
-    Utrecht</p>
-  <p>For payments from Czechia: CZ44 0100 0000 0005 3894 0301 (BIC
-    KOMBCZPPXXX) in name of Dirk Johannes Horringa, Utrecht</p>
-  <p>For payments from the USA: Wise Account number 8310003482 (ACH and Wire
-    routing number 026073150) in name of La Pellegrina, Utrecht NL</p>
+  <ul>
+    <li>NL33 ASNB 0707 2500 72 (BIC ASNB NL21) in name of <em>La
+        Pellegrina</em>, Utrecht</li>
+    <li>For payments from Czechia: CZ44 0100 0000 0005 3894 0301 (BIC
+      KOMBCZPPXXX) in name of Dirk Johannes Horringa, Utrecht</li>
+    <li>For payments from the USA: Wise Account number 8310003482 (ACH and
+      Wire routing number 026073150) in name of La Pellegrina, Utrecht NL
+    </li>
+  </ul>
   <h2>Imprint</h2>
   <h4>Text</h4>
-  <p>Dirkjan Horringa</p>
-  <h4>Video</h4>
-  <p>Martin Mrkva, Bram Heemskerk, Nicky de Bie, Ank de Wit</p>
-  <h4>Design &amp; realisation</h4>
-  <p>Thomas Op de Coul, Dirkjan Horringa </p>
-  <h4>Photo's</h4>
-  <p>For illustrating this site grateful use has been made of the many
-    beautiful photographs by participants like Briana Gillet, Matthia Cox,
-    Lea Schuiling, Wout&nbsp;van&nbsp;den&nbsp;Heuvel, Cassandra Luckhardt,
-    Désirée Staverman, Vivian Stemerdink, Patrick&nbsp;Vercammen,
-    Petra&nbsp;Wals, Joyce Vermeer, Fred Willems and
-    Annemieke&nbsp;Zwanenburg.</p>
-  <h2>Useful musical links</h2>
-  <h3 class="begin">Music libraries and digital collections of editions</h3>
-  <a href="http://www.Wranitzky.com" target="_blank"><img
-      src="/Images/Logos/wranitzky-banner.gif" width="234" height="60"
-      alt="www.Wranitzky.com" class="geenlijn"
-      style="float: left; margin-right: 20px;" /></a>
-  <p><a href="http://www.Wranitzky.com">www.Wranitzky.com</a>, a wonderful
-    website dedicated to the music of Pavel Vranický, with a great number of
-    chamber music and orchestral scores in excellent modern edition</p>
-  </td>
-  </tr>
-  </table>
-  <h3>Links to comparable organisers of summer schools</h3>
-  <p><a href="http://www.huismuziek.nl" target="_blank">Huismuziek</a> (in
-    Dutch)</p>
-  <p><a href="http://www.iam-ev.de" target="_blank">Internationale
-      Arbeitsgemeinschaft f&uuml;r Musik</a> (in German)</p>
-  <p><a href="http://www.esp.ele.tue.nl/faso/index.htm" target="_blank">FASO -
-      Federatie van Amateur Symfonie- en Strijkorkesten</a> (in Dutch)</p>
+  <li>Dirkjan Horringa</p>
+    <h4>Video</h4>
+    <p>Martin Mrkva, Bram Heemskerk, Nicky de Bie, Ank de Wit</p>
+    <h4>Design &amp; realisation</h4>
+    <p>Thomas Op de Coul, Dirkjan Horringa </p>
+    <h4>Photo's</h4>
+    <p>For illustrating this site grateful use has been made of the many
+      beautiful photographs by participants like Briana Gillet, Matthia
+      Cox, Lea Schuiling, Wout&nbsp;van&nbsp;den&nbsp;Heuvel, Cassandra
+      Luckhardt, Désirée Staverman, Vivian Stemerdink,
+      Patrick&nbsp;Vercammen, Petra&nbsp;Wals, Joyce Vermeer, Fred Willems
+      and Annemieke&nbsp;Zwanenburg.</p>
+    <h2>Useful musical links</h2>
+    <h3 class="begin">Music libraries and digital collections of editions
+    </h3>
+    <a href="http://www.Wranitzky.com" target="_blank"><img
+        src="/Images/Logos/wranitzky-banner.gif" width="234" height="60"
+        alt="www.Wranitzky.com" class="geenlijn"
+        style="float: left; margin-right: 20px;" /></a>
+    <p><a href="http://www.Wranitzky.com">www.Wranitzky.com</a>, a wonderful
+      website dedicated to the music of Pavel Vranický, with a great
+      number of chamber music and orchestral scores in excellent modern
+      edition</p>
+    </td>
+    </tr>
+    </table>
+    <h3>Links to comparable organisers of summer schools</h3>
+    <p><a href="http://www.huismuziek.nl" target="_blank">Huismuziek</a> (in
+      Dutch)</p>
+    <p><a href="http://www.iam-ev.de" target="_blank">Internationale
+        Arbeitsgemeinschaft f&uuml;r Musik</a> (in German)</p>
+    <p><a href="http://www.esp.ele.tue.nl/faso/index.htm"
+        target="_blank">FASO - Federatie van Amateur Symfonie- en
+        Strijkorkesten</a> (in Dutch)</p>
 </body>
 
 </html>
