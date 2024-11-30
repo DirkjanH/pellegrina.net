86796d540b4b41b7d5afad357aecd79a335eccfa
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-30 14:57:13 +0100

    d

--- algemeen/statistics_fund.php
+++ algemeen/statistics_fund.php
@@ -1,5 +1,5 @@
 <?php //Connection statement
-require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/includes2025.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/includes2024.php');
 
 // stel php in dat deze fouten weergeeft
 //ini_set('display_errors', 1);
@@ -103,116 +103,140 @@ while ($i <= $laatstecursus) {
 d($cursus, $cursussen, $korting, $cursusnaam);
 ?>
 <!DOCTYPE HTML>
-<html><!-- InstanceBegin template="/Templates/LP algemeen EN.dwt.php" codeOutsideHTMLIsLocked="false" -->
-
+<html>
+<!-- InstanceBegin template="/Templates/LP algemeen EN.dwt.php" codeOutsideHTMLIsLocked="false" -->
 <head>
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <!-- CSS: -->
+    <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
+    <!-- InstanceBeginEditable name="doctitle" -->
+    <title>Statistics Fund <?php echo $jaar ?></title>
+    <!-- InstanceEndEditable -->
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/metatags+javascript.EN.php'; ?>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/GA_code.php'; ?>
+    <link href="/css/pagina_stijlen_algemeen.css" rel="stylesheet"
+        type="text/css">
+    <!-- InstanceBeginEditable name="head" -->
+    <link rel="stylesheet" href="../css/pellegrina_stijlen.css" type="text/css">
+    <style type="text/css">
+    <!--
+    table#stat {
+        width: 100%;
+        left: 11px;
+        top: 89px;
+    }
 
-   <!-- CSS: -->
-   <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
+    p {
+        width: auto;
+    }
 
-   <!-- InstanceBeginEditable name="doctitle" -->
-   <title>Statistics Fund <?php echo $jaar ?></title>
-   <!-- InstanceEndEditable -->
-   <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/metatags+javascript.EN.php'; ?>
-   <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/GA_code.php'; ?>
-   <link href="/css/pagina_stijlen_algemeen.css" rel="stylesheet" type="text/css">
-   <!-- InstanceBeginEditable name="head" -->
+    td {
+        width: 25%;
+    }
 
-
-   <link rel="stylesheet" href="../css/pellegrina_stijlen.css" type="text/css">
-   <style type="text/css">
-      <!--
-      table#stat {
-         width: 100%;
-         left: 11px;
-         top: 89px;
-      }
-
-      p {
-         width: auto;
-      }
-
-      td {
-         width: 25%;
-      }
-
-      .cursusnaam {
-         vertical-align: top;
-         background-color: #BE9495;
-         font-weight: bold;
-         padding-left: 25px;
-      }
-      -->
-   </style>
-   <!-- InstanceEndEditable -->
+    .cursusnaam {
+        vertical-align: top;
+        background-color: #BE9495;
+        font-weight: bold;
+        padding-left: 25px;
+    }
+    -->
+    </style>
+    <!-- InstanceEndEditable -->
 </head>
-
 <body>
-   <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/GA_tagmanager.php'; ?>
-   <div id="inhoud">
-      <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.EN.php'; ?>
-      <div id="main"> <!-- InstanceBeginEditable name="mainpage" -->
-         <h2>Statistics <?php echo $jaar; ?></h2>
-         <p>Fund for Music Students and Eastern European Participants&nbsp;</p>
-         <table id="stat">
-            <?php
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/GA_tagmanager.php'; ?>
+    <div id="inhoud">
+        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.EN.php'; ?>
+        <div id="main">
+            <!-- InstanceBeginEditable name="mainpage" -->
+            <h2>Statistics <?php echo $jaar; ?></h2>
+            <p>Fund for Music Students and Eastern European Participants&nbsp;
+            </p>
+            <table id="stat"> <?php
             $i = $eerstecursus;
             while ($i <= $laatstecursus) {
-            ?>
-               <tr>
-                  <td class="cursusnaam"><?php echo $cursusnaam[$i]; ?>:</td>
-               </tr>
-               <tr>
-                  <td valign="top">
-                     <ul>
-                        <li>Participants: <?php echo $aangenomen[$i]; ?>
-                           <ul>
-                              <li>Of whom students: <?php echo $student[$i]; ?></li>
-                              <li>Of whom Eastern Europeans: <?php echo $oost[$i]; ?></li>
-                              <li>Of whom Eastern European students: <?php echo $ooststudent[$i]; ?></li>
-                              <li>Of whom other participants applying for a reduction: <?php echo $reductor[$i]; ?></li>
-                           </ul>
-                        </li>
-                        <li>Donations by participants:&nbsp;<?php echo euro($cursus[$i]['donatie']); ?></li>
-                        <li>Number of donators:&nbsp;<?php echo $donator[$i] . ' (' . round($donator[$i] / $aangenomen[$i] * 100, 1) . ' %)'; ?></li>
-                        <li>Average donation per donator: <?php echo euro($cursus[$i]['donatie'] / $donator[$i]); ?></li>
-                        <li>Average donation per participant: <?php echo euro($cursus[$i]['donatie'] / $aangenomen[$i]); ?><br><br>
-                           <ul>
-                              <li>Reductions applicants:&nbsp;<?php echo euro($cursus[$i]['korting']); ?></li>
-                              <?php if ($ACMP) { ?>
-                                 <li>Reductions students:&nbsp;<?php echo euro($cursus[$i]['student']); ?></li>
-                                 <li>Reductions Eastern Europeans:&nbsp;<?php echo euro($cursus[$i]['oost']); ?></li>
-                                 <li>Reductions Eastern European students:&nbsp;<?php echo euro($cursus[$i]['ooststudent']); ?></li>
-                              <?php $cursus[$i]['korting'] += $cursus[$i]['student'] + $cursus[$i]['oost'] + $cursus[$i]['ooststudent'];
+            ?> <tr>
+                    <td class="cursusnaam"><?php echo $cursusnaam[$i]; ?>:</td>
+                </tr>
+                <tr>
+                    <td valign="top">
+                        <ul>
+                            <li>Participants: <?php echo $aangenomen[$i]; ?>
+                                <ul>
+                                    <li>Of whom students:
+                                        <?php echo $student[$i]; ?></li>
+                                    <li>Of whom Eastern Europeans:
+                                        <?php echo $oost[$i]; ?></li>
+                                    <li>Of whom Eastern European students:
+                                        <?php echo $ooststudent[$i]; ?></li>
+                                    <li>Of whom other participants applying for
+                                        a reduction:
+                                        <?php echo $reductor[$i]; ?></li>
+                                </ul>
+                            </li>
+                            <li>Donations by
+                                participants:&nbsp;<?php echo euro($cursus[$i]['donatie']); ?>
+                            </li>
+                            <li>Number of
+                                donators:&nbsp;<?php echo $donator[$i] . ' (' . round($donator[$i] / $aangenomen[$i] * 100, 1) . ' %)'; ?>
+                            </li>
+                            <li>Average donation per donator:
+                                <?php echo euro($cursus[$i]['donatie'] / $donator[$i]); ?>
+                            </li>
+                            <li>Average donation per participant:
+                                <?php echo euro($cursus[$i]['donatie'] / $aangenomen[$i]); ?><br><br>
+                                <ul>
+                                    <li>Reductions
+                                        applicants:&nbsp;<?php echo euro($cursus[$i]['korting']); ?>
+                                    </li> <?php if ($ACMP) { ?> <li>Reductions
+                                        students:&nbsp;<?php echo euro($cursus[$i]['student']); ?>
+                                    </li>
+                                    <li>Reductions Eastern
+                                        Europeans:&nbsp;<?php echo euro($cursus[$i]['oost']); ?>
+                                    </li>
+                                    <li>Reductions Eastern European
+                                        students:&nbsp;<?php echo euro($cursus[$i]['ooststudent']); ?>
+                                    </li> <?php $cursus[$i]['korting'] += $cursus[$i]['student'] + $cursus[$i]['oost'] + $cursus[$i]['ooststudent'];
                               } ?>
-                           </ul>
-                        <li>Reductions total:&nbsp;<?php echo euro($cursus[$i]['korting']); ?></li>
-                        <li>People benefitting from a reduction: <?php echo $reductor[$i] + $reductor2[$i] . ' (' . round(($reductor[$i] + $reductor2[$i]) / $aangenomen[$i] * 100, 1) . ' %)'; ?></li>
-                        <li>Average reduction:&nbsp;<?php if ($reductor[$i] + $reductor2[$i] > 0)  echo euro($cursus[$i]['korting'] / ($reductor[$i] + $reductor2[$i]));
+                                </ul>
+                            <li>Reductions
+                                total:&nbsp;<?php echo euro($cursus[$i]['korting']); ?>
+                            </li>
+                            <li>People benefitting from a reduction:
+                                <?php echo $reductor[$i] + $reductor2[$i] . ' (' . round(($reductor[$i] + $reductor2[$i]) / $aangenomen[$i] * 100, 1) . ' %)'; ?>
+                            </li>
+                            <li>Average reduction:&nbsp;<?php if ($reductor[$i] + $reductor2[$i] > 0)  echo euro($cursus[$i]['korting'] / ($reductor[$i] + $reductor2[$i]));
                                                       else echo '-' ?></li>
-                     </ul>
-                  </td>
-               <?php
+                        </ul>
+                    </td> <?php
                $i++;
             }
                ?>
-               </tr>
-               <tr>
-                  <td colspan="<?php echo $aantal_cursussen; ?>" valign="top">
-                     <p>Total participants:&nbsp;<?php echo $aangenomen['totaal']; ?> | donations:&nbsp;<?php echo euro($cursus['totaal']['donatie']); ?> |
-                        donators: <?php echo $donator['totaal'] . ' (' . round($donator['totaal'] / $aangenomen['totaal'] * 100, 1) . ' %)'; ?> | reductions:&nbsp;<?php echo euro($cursus['totaal']['korting']); ?> | people benefitting from a reduction: <?php echo ($reductor['totaal'] + $reductor2['totaal']) . ' (' . round(($reductor['totaal'] + $reductor2['totaal']) / $aangenomen['totaal'] * 100, 1) . ' %)'; ?></p>
-                  </td>
-               </tr>
-         </table>
-         <!-- InstanceEndEditable -->
-         <h2> <a href="javascript: history.go(-1)">Back</a></h2>
-         <p>&nbsp;</p>
-      </div>
-   </div>
-   <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
+                </tr>
+                <tr>
+                    <td colspan="<?php echo $aantal_cursussen; ?>" valign="top">
+                        <p>Total
+                            participants:&nbsp;<?php echo $aangenomen['totaal']; ?>
+                            |
+                            donations:&nbsp;<?php echo euro($cursus['totaal']['donatie']); ?>
+                            | donators:
+                            <?php echo $donator['totaal'] . ' (' . round($donator['totaal'] / $aangenomen['totaal'] * 100, 1) . ' %)'; ?>
+                            |
+                            reductions:&nbsp;<?php echo euro($cursus['totaal']['korting']); ?>
+                            | people benefitting from a reduction:
+                            <?php echo ($reductor['totaal'] + $reductor2['totaal']) . ' (' . round(($reductor['totaal'] + $reductor2['totaal']) / $aangenomen['totaal'] * 100, 1) . ' %)'; ?>
+                        </p>
+                    </td>
+                </tr>
+            </table>
+            <!-- InstanceEndEditable -->
+            <h2> <a href="javascript: history.go(-1)">Back</a></h2>
+            <p>&nbsp;</p>
+        </div>
+    </div>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
 </body>
 <!-- InstanceEnd -->
-
 </html>
