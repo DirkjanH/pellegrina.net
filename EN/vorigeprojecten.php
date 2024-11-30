808889bed6c282af03fc04f322637fb264940603
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-30 14:43:09 +0100

    erv

--- EN/vorigeprojecten.php
+++ EN/vorigeprojecten.php
@@ -1,72 +1,80 @@
 <!doctype html>
-<html><!-- InstanceBegin template="/Templates/leeg_EN.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<html>
 <head>
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta charset="utf-8">
-
-   <!-- CSS: -->
-   <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
-
-<!-- InstanceBeginEditable name="doctitle" -->
-<title>Naamloos document</title>
-<!-- InstanceEndEditable -->
-<!-- InstanceBeginEditable name="head" -->
-<!-- InstanceEndEditable -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
+    <title>Naamloos document</title>
 </head>
-
 <body>
-<!-- InstanceBeginEditable name="mainpage" -->
-<h2 class="begin">Experiences of participants</h2>
-<table class="w3-table w3-striped w3-bordered" border="1">
-  <tr>
-    <td width="20%"><p>Chantal van Bruggen</p></td>
-    <td valign="top"><p>Great to see    such a project grow, a project in which professionals and amateurs together create    a fascinating and beautiful production!</p></td>
-  </tr>
-  <tr>
-    <td><p>Peter de Vrijer</p></td>
-    <td valign="top"><p>Working with    a group towards an ultimate performance is for me very motivating.</p></td>
-  </tr>
-  <tr>
-    <td><p>Atty Tordoir</p></td>
-    <td valign="top"><p>Joining    La Pellegrina is a fantastic experience. You meet people from all corners of    the world that share your passion and are willing to go to the end and a bit    further. And there are always plenty of people with whom you laugh your head off    during breaks - yes, there are break, but not many -  or talk about things that concern you. And    all in a wonderful country in which you feel in many ways like being in the    50's.</p></td>
-  </tr>
-  <tr>
-    <td><p>Yolande Krooshof</p></td>
-    <td valign="top"><p>A nice    compliment during the farewell evening was good nourishment for a few months    to come!</p></td>
-  </tr>
-  <tr>
-    <td><p>Noortje Briët</p></td>
-    <td valign="top"><p>In a beautiful    setting with serious and intense and fun music, that's what you La Pellegrina    offers</p></td>
-  </tr>
-  <tr>
-    <td><p>Rhodan ten Kleij</p></td>
-    <td valign="top"><p>That    volume you can get out of your voice, combined with your family miles away being    at home – they don’t suffer hearing it. A win-win situation.</p></td>
-  </tr>
-  <tr>
-    <td><p>Jaap van Luin</p></td>
-    <td valign="top"><p>I came    home in my study and thought 'how long have I been gone?&quot; I really    thought it had been months, but it was only two weeks ... Totally busy doing    something else that also gave a great sense of satisfaction. A rich    experience, for not so much money</p></td>
-  </tr>
-  <tr>
-    <td><p>Guus Vijftigschild</p></td>
-    <td valign="top"><p>Pellegrina    is what you need!!!!! An inspiring musical summer in Czechia!</p></td>
-  </tr>
-  <tr>
-    <td><p>Margriet van der    Meij</p></td>
-    <td valign="top"><p>Your playing    gets a huge boost from it! I'm looking forward to next year.</p></td>
-  </tr>
-  <tr>
-    <td><p>Ellen Ehrnreich</p></td>
-    <td valign="top"><p>Playing    with a piano trio including Zbynek Padourek has made me grow musically</p></td>
-  </tr>
-  <tr>
-    <td><p>Casper van Dongen</p></td>
-    <td valign="top"><p>If you join from 40+ to 70+ already, you are lightly 'hooked'. Why: 1. the balanced combination of chamber music and orchestra, and 2. the unrivalled Czech-musical atmosphere and teachers.</p></td>
-  </tr>
-  <tr>
-    <td><p>Joke van Raalte en    Wout van der Heuvel</p></td>
-    <td valign="top"><p>Tips and    inspiration of the teachers work for a long while. With good preparation and    good coaching, we brought the music to a higher level. We enjoyed that!</p></td>
-  </tr>
-</table>
-<!-- InstanceEndEditable -->
+    <h2 class="begin">Experiences of participants</h2>
+    <table class="w3-table w3-striped w3-bordered" border="1">
+        <tr>
+            <td>Yolande Krooshof</td>
+            <td valign="top">A nice compliment during the farewell evening was
+                good nourishment for a few months to come!</td>
+        </tr>
+        <tr>
+            <td width="20%">Chantal van Bruggen</td>
+            <td valign="top">Great to see such a project grow, a project in
+                which professionals and amateurs together create a fascinating
+                and beautiful production!</td>
+        </tr>
+        <tr>
+            <td>Casper van Dongen</td>
+            <td valign="top">If you join from 40+ to 70+ already, you are
+                lightly 'hooked'. Why: 1. the balanced combination of chamber
+                music and orchestra, and 2. the unrivalled Czech-musical
+                atmosphere and teachers.</td>
+        </tr>
+        <tr>
+            <td>Joke van Raalte en Wout van der Heuvel</td>
+            <td valign="top">Tips and inspiration of the teachers work for a
+                long while. With good preparation and good coaching, we brought
+                the music to a higher level. We enjoyed that!</td>
+        </tr>
+        <tr>
+            <td>Atty Tordoir</td>
+            <td valign="top">Joining La Pellegrina is a fantastic experience.
+                You meet people from all corners of the world that share your
+                passion and are willing to go to the end and a bit further. And
+                there are always plenty of people with whom you laugh your head
+                off during breaks - yes, there are break, but not many -  or
+                talk about things that concern you. And all in a wonderful
+                country in which you feel in many ways like being in the 50's.
+            </td>
+        </tr>
+        <tr>
+            <td>Noortje Briët</td>
+            <td valign="top">In a beautiful setting with serious and intense and
+                fun music, that's what you La Pellegrina offers</td>
+        </tr>
+        <tr>
+            <td>Rhodan ten Kleij</td>
+            <td valign="top">That volume you can get out of your voice, combined
+                with your family miles away being at home – they don’t suffer
+                hearing it. A win-win situation.</td>
+        </tr>
+        <tr>
+            <td>Jaap van Luin</td>
+            <td valign="top">I came home in my study and thought 'how long have
+                I been gone?&quot; I really thought it had been months, but it
+                was only two weeks ... Totally busy doing something else that
+                also gave a great sense of satisfaction. A rich experience, for
+                not so much money</td>
+        </tr>
+        <tr>
+            <td>Guus Vijftigschild</td>
+            <td valign="top">Pellegrina is what you need!!!!! An inspiring
+                musical summer in Czechia!</td>
+        </tr>
+        <tr>
+            <td>Margriet van der Meij</td>
+            <td valign="top">Your playing gets a huge boost from it! I'm looking
+                forward to next year.</td>
+        </tr>
+    </table>
+    <!-- InstanceEndEditable -->
 </body>
-<!-- InstanceEnd --></html>
+<!-- InstanceEnd -->
+</html>
--- NL/vorigeprojecten.php
+++ NL/vorigeprojecten.php
@@ -1,72 +1,87 @@
 <!doctype html>
-<html><!-- InstanceBegin template="/Templates/leeg_NL.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<html>
+<!-- InstanceBegin template="/Templates/leeg_NL.dwt.php" codeOutsideHTMLIsLocked="false" -->
 <head>
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta charset="utf-8">
-
-   <!-- CSS: -->
-   <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
-
-<!-- InstanceBeginEditable name="doctitle" -->
-<title>Naamloos document</title>
-<!-- InstanceEndEditable -->
-<!-- InstanceBeginEditable name="head" -->
-<!-- InstanceEndEditable -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <!-- CSS: -->
+    <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
+    <!-- InstanceBeginEditable name="doctitle" -->
+    <title>Naamloos document</title>
+    <!-- InstanceEndEditable -->
+    <!-- InstanceBeginEditable name="head" -->
+    <!-- InstanceEndEditable -->
 </head>
-
 <body>
-<!-- InstanceBeginEditable name="mainpage" -->
-<h2 class="begin">Ervaringen van deelnemers</h2>
-<table class="w3-table w3-striped w3-bordered" border="1">
-  <tr>
-    <td width="20%"><p>Chantal van Bruggen</p></td>
-    <td><p>Geweldig om zo'n project te zien groeien    waarin professionals en amateurs samen een boeiend en mooie productie weten    neer te zetten!</p></td>
-  </tr>
-  <tr>
-    <td><p>Peter de Vrijer</p></td>
-    <td><p>Het met een groep naar een ultieme    uitvoering toewerken is voor mij erg motiverend.</p></td>
-  </tr>
-  <tr>
-    <td><p>Atty Tordoir</p></td>
-    <td><p>Meedoen met La Pellegrina is een    fantastische ervaring. Je ontmoet mensen uit allerlei uithoeken die jouw    passie delen en bereid zijn tot het gaatje te gaan, en nog iets verder. En er    zijn altijd genoeg mensen met wie je in de pauzes - jazeker, die zijn er ook-    kunt lachen, op je hoofd staan of praten over zaken die je bezighouden of    opvallen. En dat alles in een heerlijk land waarin je je in meerdere    opzichten regelmatig in de jaren '50 waant.</p></td>
-  </tr>
-  <tr>
-    <td><p>Yolande Krooshof</p></td>
-    <td><p>Een mooi compliment tijdens de    afscheidsavond was goed om nog een paar maanden op te teren!</p></td>
-  </tr>
-  <tr>
-    <td><p>Noortje Bri&euml;t</p></td>
-    <td><p>In een prachtige omgeving serieus en    intensief en met veel plezier muziek maken, dat is wat La Pellegrina je biedt</p></td>
-  </tr>
-  <tr>
-    <td><p>Rhodan ten Kleij</p></td>
-    <td><p>Dat volume dat je uit je stem kunt    halen, gecombineerd met je familie thuis - kilometers verderop - die het niet    hoeft aan te horen. Een win-winsituatie.'</p></td>
-  </tr>
-  <tr>
-    <td><p>Jaap van Luin</p></td>
-    <td><p>Ik kwam thuis in mijn werkkamer en ik    dacht &quot;wat ben ik hier lang weggeweest &quot;. Echt waar ik dacht dat    het maanden waren; &lsquo;t was twee weken&hellip; Helemaal totaal met iets anders bezig    wat ook nog een voldaan gevoel gaf . Een rijke belevenis dus, voor niet te    veel geld</p></td>
-  </tr>
-  <tr>
-    <td><p>Guus Vijftigschild</p></td>
-    <td><p>Van Pellegrina moet je het hebben!!!!!    Een inspirerende muzikale zomervakantie in Tsjechi&euml;!</p></td>
-  </tr>
-  <tr>
-    <td><p>Margriet van der Meij</p></td>
-    <td><p>Je spel krijgt er een enorme oppepper    van! Ik kijk nu al weer uit naar volgend jaar.</p></td>
-  </tr>
-  <tr>
-    <td><p>Ellen Ehrnreich</p></td>
-    <td><p>Het samenspelen met Zbyn&#283;k Pa&#271;ourek in    een pianotrio heeft mij muzikaal doen groeien</p></td>
-  </tr>
-  <tr>
-    <td><p>Casper van Dongen</p></td>
-    <td><p>Als je van 40+ tot 70+ al meedoet, dan ben je licht 'verslaafd'. Waarom? 1. de evenwichtige combinatie van kamermuziek en orkest, en 2. de ongeëvenaarde Tsjechisch-muzikale sfeer en docenten.</p></td>
-  </tr>
-  <tr>
-    <td><p>Joke van Raalte en Wout van der Heuvel</p></td>
-    <td><p>Tips en inspiratie van de docenten    werken lang door. Met goede voorbereiding en prima coaching brengen we de    muziek naar een hoger niveau. Genieten is dat!</p></td>
-  </tr>
-</table>
-<!-- InstanceEndEditable -->
+    <!-- InstanceBeginEditable name="mainpage" -->
+    <h2 class="begin">Ervaringen van deelnemers</h2>
+    <table class="w3-table w3-striped w3-bordered" border="1">
+        <tr>
+            <td>Yolande Krooshof</td>
+            <td>Een mooi compliment tijdens de afscheidsavond was goed om nog
+                een paar maanden op te teren!</td>
+        </tr>
+        <tr>
+            <td>Casper van Dongen</td>
+            <td>Als je van 40+ tot 70+ al meedoet, dan ben je licht 'verslaafd'.
+                Waarom? 1. de evenwichtige combinatie van kamermuziek en orkest,
+                en 2. de ongeëvenaarde Tsjechisch-muzikale sfeer en docenten.
+            </td>
+        </tr>
+        <tr>
+            <td>Joke van Raalte en Wout van der Heuvel</td>
+            <td>Tips en inspiratie van de docenten werken lang door. Met goede
+                voorbereiding en prima coaching brengen we de muziek naar een
+                hoger niveau. Genieten is dat!</td>
+        </tr>
+        <tr>
+            <td width="20%">Chantal van Bruggen</td>
+            <td>Geweldig om zo'n project te zien groeien waarin professionals en
+                amateurs samen een boeiend en mooie productie weten neer te
+                zetten!</td>
+        </tr>
+        <tr>
+            <td>Atty Tordoir</td>
+            <td>Meedoen met La Pellegrina is een fantastische ervaring. Je
+                ontmoet mensen uit allerlei uithoeken die jouw passie delen en
+                bereid zijn tot het gaatje te gaan, en nog iets verder. En er
+                zijn altijd genoeg mensen met wie je in de pauzes - jazeker, die
+                zijn er ook- kunt lachen, op je hoofd staan of praten over zaken
+                die je bezighouden of opvallen. En dat alles in een heerlijk
+                land waarin je je in meerdere opzichten regelmatig in de jaren
+                '50 waant.</td>
+        </tr>
+        <tr>
+            <td>Noortje Bri&euml;t</td>
+            <td>In een prachtige omgeving serieus en intensief en met veel
+                plezier muziek maken, dat is wat La Pellegrina je biedt</td>
+        </tr>
+        <tr>
+            <td>Rhodan ten Kleij</td>
+            <td>Dat volume dat je uit je stem kunt halen, gecombineerd met je
+                familie thuis - kilometers verderop - die het niet hoeft aan te
+                horen. Een win-winsituatie.'</td>
+        </tr>
+        <tr>
+            <td>Jaap van Luin</td>
+            <td>Ik kwam thuis in mijn werkkamer en ik dacht &quot;wat ben ik
+                hier lang weggeweest &quot;. Echt waar ik dacht dat het maanden
+                waren; &lsquo;t was twee weken&hellip; Helemaal totaal met iets
+                anders bezig wat ook nog een voldaan gevoel gaf . Een rijke
+                belevenis dus, voor niet te veel geld</td>
+        </tr>
+        <tr>
+            <td>Guus Vijftigschild</td>
+            <td>Van Pellegrina moet je het hebben!!!!! Een inspirerende muzikale
+                zomervakantie in Tsjechi&euml;!</td>
+        </tr>
+        <tr>
+            <td>Margriet van der Meij</td>
+            <td>Je spel krijgt er een enorme oppepper van! Ik kijk nu al weer
+                uit naar volgend jaar.</td>
+        </tr>
+    </table>
+    <!-- InstanceEndEditable -->
 </body>
-<!-- InstanceEnd --></html>
+<!-- InstanceEnd -->
+</html>
