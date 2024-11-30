8535018de2b74535c86b8ae5b1fda328fb27c4e3
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-30 15:17:17 +0100

    faq

--- NL/faq.php
+++ NL/faq.php
@@ -1,62 +1,48 @@
 <!doctype html>
 <html>
-
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
-  <title>Naamloos document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
+    <title>Naamloos document</title>
 </head>
-
 <body>
-  <h2 class="begin">Vaak gestelde vragen </h2>
-  <ul class="w3-ul w3-card-2">
-    <ul>
-      <ul>
-        <li><b>Hoe worden de kamermuziekensembles samengesteld?</b> Als
-          de bezetting van het project definitief is, meestal in de
-          loop van april, dan worden er twee sets van
-          kamermuziekensembles voor het ochtendprogramma samengesteld.
-          Iedereen speelt namelijk in twee verschillende ensembles.
-          Bij het samenstellen wordt rekening gehouden met wensen van
-          deelnemers. Iedereen mag één ensemble zelf samenstellen;
-          voor het andere ensemble mogen wensen worden doorgegeven.
-          <i>La Pellegrina </i>streeft ernaar die te honoreren, maar
-          kan dat niet garanderen.
-        </li>
-        <li><b>De avonden zijn vrij. Wordt er dan ook muziek
-            gemaakt?</b> De echt fanatieke deelnemers gaan 's avonds
-          terug naar het klooster of het conservatorium om in de vele
-          repetitieruimtes te musiceren. Deze ensembles vormen zich
-          meestal ter plekke. Het is aan te raden om je eigen
-          bladmuziek mee te nemen. </li>
-        <li><b>Hoe kan ik betalen?</b> <i>La Pellegrina</i> accepteert
-          betalingen via rekening NL33 ASNB 0707 2500 72 (voor
-          buitenlandse betalingen: BIC ASNB NL21) t.n.v. La Pellegrina
-          te Utrecht. Als dit niet praktisch is, is betaling in
-          contanten ter plekke in overleg ook mogelijk.</li>
-        <li><strong>Hoe wordt er gereisd naar České Budějovice?</strong>
-          Vanuit Nederland en Duitsland gaan er inmiddels weer
-          internationale treinen, naar Linz (Oostenrijk) of Praag.
-          Vanuit beide plaatsen gaan er frequente treinen naar České
-          Budějovice. Het is ook mogelijk te vliegen naar Praag en
-          vanaf daar de trein te nemen. Vanaf het station van České
-          Budějovice is het nog geen kwartier lopen naar het
-          conservatorium. Het is mogelijk om je bagage op te laten
-          halen bij het station.</li>
-        <li><b>Hoe kan ik een annuleringsverzekering afsluiten?</b> Het
-          is aan te raden een annuleringsverzekering af te sluiten.
-          Dit kan via de eigen verzekeringsagent of bank.</li>
-        <li><b>Is het mogelijk om samen te reizen?</b> <i>La
-            Pellegrina</i> organiseert de reis niet. Maar om mensen
-          die graag samen willen reizen met elkaar in contact te
-          brengen, wordt na het beslismoment over deelname,
-          <strong><?php echo $cursusdata['beslisdatum']; ?></strong>,
-          een deelnemerslijst gepubliceerd.
-        </li>
-      </ul>
-    </ul>
-  </ul>
+    <h2 class="begin">Vaak gestelde vragen </h2>
+    <div class="w3-card-2 w3-padding"><b>Hoe worden de kamermuziekensembles
+            samengesteld?</b> Als de bezetting van het project definitief is,
+        meestal in de loop van april, dan worden er twee sets van
+        kamermuziekensembles voor het ochtendprogramma samengesteld. Iedereen
+        speelt namelijk in twee verschillende ensembles. Bij het samenstellen
+        wordt rekening gehouden met wensen van deelnemers. Iedereen mag één
+        ensemble zelf samenstellen; voor het andere ensemble mogen wensen worden
+        doorgegeven. <i>La Pellegrina </i>streeft ernaar die te honoreren, maar
+        kan dat niet garanderen. </div>
+    <div class="w3-card-2 w3-padding"><b>De avonden zijn vrij. Wordt er dan ook
+            muziek gemaakt?</b> De echt fanatieke deelnemers gaan 's avonds
+        terug naar het klooster of het conservatorium om in de vele
+        repetitieruimtes te musiceren. Deze ensembles vormen zich meestal ter
+        plekke. Het is aan te raden om je eigen bladmuziek mee te nemen. </div>
+    <div class="w3-card-2 w3-padding"><b>Hoe kan ik betalen?</b> <i>La
+            Pellegrina</i> accepteert betalingen via rekening NL33 ASNB 0707
+        2500 72 (voor buitenlandse betalingen: BIC ASNB NL21) t.n.v. La
+        Pellegrina te Utrecht. Als dit niet praktisch is, is betaling in
+        contanten ter plekke in overleg ook mogelijk.</div>
+    <div class="w3-card-2 w3-padding"><strong>Hoe wordt er gereisd naar České
+            Budějovice?</strong> Vanuit Nederland en Duitsland gaan er inmiddels
+        weer internationale treinen, naar Linz (Oostenrijk) of Praag. Vanuit
+        beide plaatsen gaan er frequente treinen naar České Budějovice. Het is
+        ook mogelijk te vliegen naar Praag en vanaf daar de trein te nemen.
+        Vanaf het station van České Budějovice is het nog geen kwartier lopen
+        naar het conservatorium. Het is mogelijk om je bagage op te laten halen
+        bij het station.</div>
+    <div class="w3-card-2 w3-padding"><b>Hoe kan ik een annuleringsverzekering
+            afsluiten?</b> Het is aan te raden een annuleringsverzekering af te
+        sluiten. Dit kan via de eigen verzekeringsagent of bank.</div>
+    <div class="w3-card-2 w3-padding"><b>Is het mogelijk om samen te reizen?</b>
+        <i>La Pellegrina</i> organiseert de reis niet. Maar om mensen die graag
+        samen willen reizen met elkaar in contact te brengen, wordt na het
+        beslismoment over deelname,
+        <strong><?php echo $cursusdata['beslisdatum']; ?></strong>, een
+        deelnemerslijst gepubliceerd. </div>
 </body>
-
 </html>
