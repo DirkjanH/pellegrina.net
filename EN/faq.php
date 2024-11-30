bac1691b06a9249c309b77ee02e7262ee16e8b16
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-11-30 15:14:08 +0100

    faq

--- EN/faq.php
+++ EN/faq.php
@@ -8,40 +8,42 @@
 </head>
 <body>
     <h2 class="begin">Frequently Asked Questions</h2>
-    <div class="w3-card-2"><B>How are chamber music ensembles put together?</B>
-        As soon as we know who is going to take part in the project, which is
-        usually by April, we make two sets of chamber music ensembles for the
-        morning program as every player takes part in two different ensembles.
-        We try to take everyone's wishes into account putting these groups
-        together. Of course, we cannot guarantee complete satisfaction. Every
-        participant may put one of these ensembles themselves. For the other
-        ensemble, let us know if you have any preferences. </div>
-    <div class="w3-card-2"><B>The evenings are free. Is there time to play
-            together? </B>Oh yes. The fanatics return to the monastery or the
-        conservatoire after dinner to play in the numerous study rooms. These
-        'night ensembles' are usually formed on the spot, so don't forget to
-        bring your favorite sheet music. </div>
-    <div class="w3-card-2"><B>How should I pay?</B> <em>La Pellegrina</em>
-        offers two methods of payment. You may either transfer your payment into
-        account NL33 ASNB 0707 2500 72 (BIC ASNB NL21) of <em>La
-            Pellegrina</em>, Utrecht, the Netherlands. If this is not practical,
-        payment in cash on the spot is also possible by mutual agreement. See
-        the <a href="praktisch.php" target="_self">Prices &amp; Practicals
-            page</a> for more information. </div>
-    <div class="w3-card-2"><strong>How to travel to České Budějovice?</strong>
-        International trains now run again from the Netherlands and Germany, to
-        Linz (Austria) or Prague. From both places, there are frequent trains to
-        České Budějovice. It is also possible to fly to Prague and take the
-        train from there. From České Budějovice railway station, it is a
-        15-minute walk to the conservatoire. It is possible to have luggage
-        collected from the station. </div>
-    <div class="w3-card-2"><B>How can I provide for a cancellation
+    <div class="w3-card-2 w3-padding"><B>How are chamber music ensembles put
+            together?</B> As soon as we know who is going to take part in the
+        project, which is usually by April, we make two sets of chamber music
+        ensembles for the morning program as every player takes part in two
+        different ensembles. We try to take everyone's wishes into account
+        putting these groups together. Of course, we cannot guarantee complete
+        satisfaction. Every participant may put one of these ensembles
+        themselves. For the other ensemble, let us know if you have any
+        preferences. </div>
+    <div class="w3-card-2 w3-padding"><B>The evenings are free. Is there time to
+            play together? </B>Oh yes. The fanatics return to the monastery or
+        the conservatoire after dinner to play in the numerous study rooms.
+        These 'night ensembles' are usually formed on the spot, so don't forget
+        to bring your favorite sheet music. </div>
+    <div class="w3-card-2 w3-padding"><B>How should I pay?</B> <em>La
+            Pellegrina</em> offers two methods of payment. You may either
+        transfer your payment into account NL33 ASNB 0707 2500 72 (BIC ASNB
+        NL21) of <em>La Pellegrina</em>, Utrecht, the Netherlands. If this is
+        not practical, payment in cash on the spot is also possible by mutual
+        agreement. See the <a href="praktisch.php" target="_self">Prices &amp;
+            Practicals page</a> for more information. </div>
+    <div class="w3-card-2 w3-padding"><strong>How to travel to České
+            Budějovice?</strong> International trains now run again from the
+        Netherlands and Germany, to Linz (Austria) or Prague. From both places,
+        there are frequent trains to České Budějovice. It is also possible to
+        fly to Prague and take the train from there. From České Budějovice
+        railway station, it is a 15-minute walk to the conservatoire. It is
+        possible to have luggage collected from the station. </div>
+    <div class="w3-card-2 w3-padding"><B>How can I provide for a cancellation
             insurance?</B> We advise you to carry a cancellation insurance
         through your own bank or insurance agent.</div>
-    <div class="w3-card-2"><B>Is there any possibility of travelling together?
-        </B><I>La Pellegrina</I> only organizes the project, not the journey.
-        But you may be able to find your travel partners as a list of
-        participants will be available after the decision about participation,
+    <div class="w3-card-2 w3-padding"><B>Is there any possibility of travelling
+            together? </B><I>La Pellegrina</I> only organizes the project, not
+        the journey. But you may be able to find your travel partners as a list
+        of participants will be available after the decision about
+        participation,
         <strong><?php echo $cursusdata['beslisdatum']; ?></strong>. </div>
 </body>
 </html>
