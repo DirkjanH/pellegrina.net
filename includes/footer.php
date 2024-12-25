8378b34d7ff8d282865c4fc373148b692968f103
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-12-25 13:07:37 +0100

    px

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,4 @@
-<p>&nbsp;</p>
-<a title="Web Analytics" href="https://clicky.com/101101981"><img alt="Web Analytics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+<a title="Web Analytics" href="https://clicky.com/101101981"><img alt="Web Analytics" src="//static.getclicky.com/media/links/badge.gif" border="0" style="margin-top: 20px;"/></a>
 <script src="//static.getclicky.com/js" type="text/javascript"></script>
 <script type="text/javascript">try{ clicky.init(101101981); }catch(e){}</script>
 <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101101981ns.gif" /></p></noscript>
