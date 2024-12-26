15d0707970ff799b8deab380bca829a0999182a9
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-12-26 13:36:43 +0100

    cz

--- CZ_info.php
+++ CZ_info.php
@@ -1,117 +1,108 @@
-<?php require_once $_SERVER['DOCUMENT_ROOT'].'/includes/cursussen.php'; ?>
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/cursussen.php'; ?>
 
 <!DOCTYPE HTML>
 <html><!-- InstanceBegin template="/Templates/LP algemeen CZ.dwt.php" codeOutsideHTMLIsLocked="false" -->
+
 <head>
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
 
-   <!-- CSS: -->
-   <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
-   <link rel="stylesheet" href="/css/pagina_stijlen.css" type="text/css">
+	<!-- CSS: -->
+	<link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
+	<link rel="stylesheet" href="/css/pagina_stijlen.css" type="text/css">
 
-<!-- InstanceBeginEditable name="doctitle" -->
-<title>Letní hudební kurzy společnost La Pellegrina</title>
-<style type="text/css">
-	h2.lijnboven {
-	border-top-width: thin;
-	border-top-style: solid;
-	border-top-color: inherit;
-	}
-	h3.plaats {
-		margin-top: -10px;
-		margin-left: 24px;
-	}
-</style>
-<!-- InstanceEndEditable -->
-<?php require_once $_SERVER['DOCUMENT_ROOT'].'/includes/metatags+javascript.EN.php'; ?>
-<?php require_once $_SERVER['DOCUMENT_ROOT'].'/includes/GA_code.php'; ?>
-<link href="/css/pagina_stijlen_algemeen.css" rel="stylesheet" type="text/css">
-<!-- InstanceBeginEditable name="head" -->
-	
-<!-- InstanceEndEditable -->
+	<!-- InstanceBeginEditable name="doctitle" -->
+	<title>Letní hudební kurzy společnost La Pellegrina</title>
+	<style type="text/css">
+		h2.lijnboven {
+			border-top-width: thin;
+			border-top-style: solid;
+			border-top-color: inherit;
+		}
+
+		h3.plaats {
+			margin-top: -10px;
+			margin-left: 24px;
+		}
+	</style>
+	<!-- InstanceEndEditable -->
+	<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/metatags+javascript.EN.php'; ?>
+	<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/GA_code.php'; ?>
+	<link href="/css/pagina_stijlen_algemeen.css" rel="stylesheet" type="text/css">
+	<!-- InstanceBeginEditable name="head" -->
+
+	<!-- InstanceEndEditable -->
 </head>
-<body>
-<?php require_once $_SERVER['DOCUMENT_ROOT'].'/includes/GA_tagmanager.php'; ?>
-<div id="inhoud">
-  <?php require_once $_SERVER['DOCUMENT_ROOT'].'/includes/header.EN.php'; ?>
-  <div id="main"> <!-- InstanceBeginEditable name="mainpage" -->
-<div class="w3-center" lang="cs-CZ">
-   <h1 class="LP_colour w3-margin-bottom" style="margin-top: 0px;">Tři projekty pro každého hudebníka s profesionálním přístupem</h1>
-</div>
-<div class="w3-row-padding">
-   <div class="w3-twothird w3-panel">
-      <h2 class="LP_colour lijnboven" style="margin-top: 0px;">Letní hudební kurzy společnosti <em>La Pellegrina</em> <?php echo $jaar ?> v Čechách a Nízozemsku:</h2>
-      <p>tři projekty, ve kterých se můžete odevzdat nádherné hudbě se špičkovými lektory a stejně smýšlejícími lidmi. Projekty jsou určeny zkušeným instrumentalistům a zpěvákům. Vítáni jsou amatéři, poloprofesionálové i profesionálové, zkrátka všichni hudební nadšenci, kteří jsou pohotoví, přizpůsobiví, schopní hrát /zpívat v souboru s ostatními hráči a jejichž technická úroveň dovoluje zahrát /zazpívat své party v předem určených skladbách. Předpokladem spokojenosti všech účastníků je jejich předchozí zodpovědná příprava a technické zvládnutí svého partu před začátkem kurzu. Věk účastníků je neomezen.</p>
-		<div class="w3-card-8 w3-panel w3-pale-green">
-			<p class="LP_colour w3-large"><strong>Českým a&nbsp;slovenským účastníkům kursů pořádaných agenturou <i>La
-				Pellegrina</i> nabízíme zvýhodněné podmínky, mimo jiné i&nbsp;<a href="algemeen/CZ_prijzen.php" target="_blank">snížené ceny</a>.      </strong></p>
-		</div>
-      <h2 class="romantic lijnboven">1. REICHOVO REQUIEM pro orchestr, sbor a sólisty</h2>
-		<h3 class="romantic plaats">25.  července - 4. srpna v Českých Budějovicích </h3>
-		<ul>
-			<li>pro sborové a sólové zpěváky, orchestrální a komorní hráče na smyčcové a dechové nástroje, klavíristy. Vítány jsou i existující soubory<br>
-			</li>
-			<li>lektoři: Martina Bernášková (flétna), Petr Bernášek (housle), Václav Bernášek (violoncello), Pavel Hořejší (viola), Dirkjan Horringa (klarinet), Libor Nováček (klavír), Mitchell Sandler (zpěv), Rudolf Sternadel (housle a viola) a další<br>
-			</li>
-			<li>dílo ke společnému nastudování: Missa pro defunctis (Requiem) Antonína Rejchy. Orchestr zahraje také Dvořákovy Symfonické variace op. 78.<br>
-			  Sbor: kromě Rejchova Requiem s orchestrem také skladby komorního sborového repertoáru, a cappella a s klavírem.<br>
-			</li>
-			<li>instrumentalisté budou hrát každý ve dvou předem určených komorních souborech a ve velkém symfonickém orchestru<br>
-			</li>
-			<li>účastníci budou mít možnost zahrát nebo zazpívat si jednu větu z sólového koncertu nebo arii W.A. Mozarta (nebo některého z jeho současníků) za doprovodu “ad hoc” orchestru<br>
-			</li>
-			<li>kurz probíhá v příjemném prostředí Konzervatoře v Českých Budějovicích a vyvrcholí společným koncertem v katedrále</li>
-		</ul>
-		<h2 class="baroque lijnboven">2. BAROKNÍ KURZ: Vivaldiho Benátské nešpory</h2>
-<h3 class="baroque plaats">11. - 17. srpna v klášteře Nieuw Sion, Nízozemsko</h3>
-      <ul>
-         <li>pro zpěváky (sólisty a zkušené sboristy) a instrumentalisty (pouze na dobové nástroje, ladění 415 Hz)<br>
-         	</li>
-         <li>lektoři: Femke Huizinga (orchestr, housle), Hanna Lindeijer (barokní dechové nástroje), Dirkjan Horringa (zpěv a umělecký vedoucí), Ricardo Rodríguez Miranda         	(viola da gamba, barokní tánec), Mitchell Sandler (zpěv) a Edoardo Valorz (basso continuo)<br>
-         	</li>
-         <li>dílo ke společnému nastudování:  Benátské nešpory pro Pannu Marii složené z děl Vivaldiho (včetně jeho Magnificat), Galuppiho a Cavalliho.<br>
-         </li>
-         <li>každé dopoledne hraní barokní hudby v menších instrumentálních, vokálních, nebo smíšených vokálně intrumentálních ansámblech<br>
-         	</li>
-         <li>kurz probíhá v příjemném prostředí bývalého kláštera a vyvrcholí společným koncertem v klášterním kostele </li>
-         <li>S dílnou barokního tance pro všechny<br>
-         </li>
-         </ul>
-      <h2 class="romantic lijnboven"><a href="https://www.kinskytrio.cz/inpage/cs/" target="_blank"><span class="chamber">3. KURZ KOMORNÍ HUDBY s Kinsky triem a jejich přáteli</span></a></h2>
-<h3 class="chamber plaats">13. - 21. července v Českých Budějovicích</h3>
-      <ul>
-      	<li>pro hráče na klavír, smyčcové a dechové nástroje, případně pro existující komorní soubory<br>
-         	</li>
-         <li>lektoři: Lucie Sedláková Hůlová, Pavel Hůla (housle), Martin Sedlák , Jitka Vlašánková (violoncello), Veronika Böhmová (klavír), Jan Nykrýn (viola), Jan Thuri (oboe) a Ludmila Peterková (klarinet), v případě více účastníků i další<br>
-         	</li>
-         <li>každý hráč hraje ve dvou komorních souborech různého složení, tedy dvě různé skladby<br>
-         	</li>
-         <li>repertoár je volen na základě požadavků účastníků a doporučení lektorů. Je znám v dostatečném předstihu, aby se každý mohl připravit</li>
-         <li>intenzivní výuka probíhá pod vedením, nebo s aktivní účastí jednoho lektora v každém souboru<br>
-         	</li>
-         <li>kurz probíhá v profesionálním prostředí Konzervatoře v Českých Budějovicích a je zakončen veřejným vystoupením všech souborů</li>
-         <li class="nadruk">N.B. Tento kurz je organizován českými lektory a na jejich odpovědnost ve spolupráci s<em> La Pellegrina</em></li>
-      </ul>
-   </div>
-   <div class="w3-onethird w3-panel"> 
-		<p><img src="Images/CZ_info-web-resources/image/IMG_1782-2.jpg" alt="Houslistka v kostele"/></p> 
-		<p><img src="Images/CZ_info-web-resources/image/P1020545.png" alt="Orchestr a bas v Bechyňském kostele"/></p>
-		<p><img src="Images/CZ_info-web-resources/image/IMG_2544.jpg" alt="Klarinetistka v komořině"/></p>
-		<p><img src="Images/Locaties/Budejovice/ensemble_in_zaal.jpg" alt="Koncertní sál v Budějovicích" width="325" height="248"/></p> 
-	</div>
-</div>
-  <!-- InstanceEndEditable -->
-    <h2> <a href="javascript: history.go(-1)">Zpět</a></h2>
-    <p>&nbsp;</p>
-  </div>
-</div>
 
-<a title="Web Analytics" href="http://clicky.com/101101981"><img alt="Web Analytics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
-<script src="//static.getclicky.com/js" type="text/javascript"></script>
-<script type="text/javascript">try{ clicky.init(101101981); }catch(e){}</script>
-<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101101981ns.gif" /></p></noscript>
+<body>
+	<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/GA_tagmanager.php'; ?>
+	<div id="inhoud">
+		<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.EN.php'; ?>
+		<div id="main"> <!-- InstanceBeginEditable name="mainpage" -->
+			<div class="w3-center" lang="cs-CZ">
+				<h1 class="LP_colour w3-margin-bottom" style="margin-top: 0px;">Dva projekty pro každého hudebníka s profesionálním přístupem</h1>
+			</div>
+			<div class="w3-row-padding">
+				<div class="w3-twothird w3-panel">
+					<h2 class="LP_colour lijnboven" style="margin-top: 0px;">Letní hudební kurzy společnosti <em>La Pellegrina</em> <?php echo $jaar ?> v Čechách a Nízozemsku:</h2>
+					<p>dva projekty, ve kterých se můžete odevzdat nádherné hudbě se špičkovými lektory a stejně smýšlejícími lidmi. Projekty jsou určeny zkušeným instrumentalistům a zpěvákům. Vítáni jsou amatéři, poloprofesionálové i profesionálové, zkrátka všichni hudební nadšenci, kteří jsou pohotoví, přizpůsobiví, schopní hrát /zpívat v souboru s ostatními hráči a jejichž technická úroveň dovoluje zahrát /zazpívat své party v předem určených skladbách. Předpokladem spokojenosti všech účastníků je jejich předchozí zodpovědná příprava a technické zvládnutí svého partu před začátkem kurzu. Věk účastníků je neomezen.</p>
+					<div class="w3-card-8 w3-panel w3-pale-green">
+						<p class="LP_colour w3-large"><strong>Českým a&nbsp;slovenským účastníkům kursů pořádaných agenturou <i>La
+									Pellegrina</i> nabízíme zvýhodněné podmínky, mimo jiné i&nbsp;<a href="algemeen/CZ_prijzen.php" target="_blank">snížené ceny</a>. </strong></p>
+					</div>
+					<h2 class="romantic lijnboven">1. Dvořákův Zlatý kolovrat a Haydnova <i>Missa in tempore belli</i></h2>
+					<h3 class="romantic plaats">24. července - 3. srpna v Českých Budějovicích</h3>
+					<ul>
+						<li>pro sborové a sólové zpěváky, orchestrální a komorní hráče na smyčcové a dechové nástroje, klavíristy. Vítány jsou i existující soubory
+						</li>
+						<li>lektoři: Martina Bernášková (flétna), Petr Bernášek (housle), Václav Bernášek (violoncello), Pavel Hořejší (viola), Dirkjan Horringa (klarinet), Libor Nováček (klavír), Mitchell Sandler (zpěv), Rudolf Sternadel (housle a viola) a další
+						</li>
+						<li>Ústřední dílo pro všechny: Haydnova Missa in Tempore Belli. Orchestr zahraje také Dvořákovu symfonickou báseň <i>Zlatý kolovrat</i> op. 109</li>
+						<li>Sbor: kromě Haydnova Missa in Tempore Belli s orchestrem také skladby komorního sborového repertoáru, a cappella a s klavírem</li>
+						<li>instrumentalisté budou hrát každý ve dvou předem určených komorních souborech a ve velkém symfonickém orchestru
+						</li>
+						<li>účastníci budou mít možnost zahrát nebo zazpívat si jednu větu z sólového koncertu nebo arii W.A. Mozarta (nebo některého z jeho současníků) za doprovodu “ad hoc” orchestru
+						</li>
+						<li>kurz probíhá v příjemném prostředí Konzervatoře v Českých Budějovicích a vyvrcholí společným koncertem v katedrále</li>
+					</ul>
+					<h2 class="baroque lijnboven">2. BAROKNÍ KURZ: Purcell & Händel</h2>
+					<h3 class="baroque plaats">10. - 16. srpna v klášteře Nieuw Sion, Nízozemsko</h3>
+					<ul>
+						<li>pro zpěváky (sólisty a zkušené sboristy) a instrumentalisty (pouze na dobové nástroje, ladění 415 Hz)</li>
+						<li>lektoři: Femke Huizinga (orchestr, housle), Hanna Lindeijer (barokní dechové nástroje), Dirkjan Horringa (zpěv a umělecký vedoucí), Ricardo Rodríguez Miranda (viola da gamba, barokní tánec), Mitchell Sandler (zpěv) a Edoardo Valorz (basso continuo)</li>
+						<li>Ústřední skladby pro všechny: výběr z obou Purcellových Ód na den svaté Cecílie a z Handelsova Utrechtského Te Deum a Jubilate</li>
+						<li>každé dopoledne hraní barokní hudby v menších instrumentálních, vokálních, nebo smíšených vokálně intrumentálních ansámblech</li>
+						<li>kurz probíhá v příjemném prostředí bývalého kláštera a vyvrcholí společným koncertem v klášterním kostele </li>
+						<li>S dílnou barokního tance pro všechny účastníky, vedenou Ricardem Rodríguezem Mirandou
+						</li>
+					</ul>
+				</div>
+				<div class="w3-onethird w3-panel">
+					<p><img src="Images/CZ_info-web-resources/image/IMG_1782-2.jpg" alt="Houslistka v kostele" /></p>
+					<p><img src="Images/CZ_info-web-resources/image/P1020545.png" alt="Orchestr a bas v Bechyňském kostele" /></p>
+					<p><img src="Images/CZ_info-web-resources/image/IMG_2544.jpg" alt="Klarinetistka v komořině" /></p>
+					<p><img src="Images/Locaties/Budejovice/ensemble_in_zaal.jpg" alt="Koncertní sál v Budějovicích" width="325" height="248" /></p>
+				</div>
+			</div>
+			<!-- InstanceEndEditable -->
+			<h2> <a href="javascript: history.go(-1)">Zpět</a></h2>
+			<p>&nbsp;</p>
+		</div>
+	</div>
+
+	<a title="Web Analytics" href="http://clicky.com/101101981"><img alt="Web Analytics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+	<script src="//static.getclicky.com/js" type="text/javascript"></script>
+	<script type="text/javascript">
+		try {
+			clicky.init(101101981);
+		} catch (e) {}
+	</script>
+	<noscript>
+		<p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101101981ns.gif" /></p>
+	</noscript>
 
 </body>
-<?php require_once $_SERVER['DOCUMENT_ROOT'].'/includes/footer.php'; ?>
-<!-- InstanceEnd --></html>
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
+<!-- InstanceEnd -->
+
+</html>
