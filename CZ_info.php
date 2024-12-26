3e68aba132e0595cb004314a02bb008963be5beb
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-12-26 13:48:45 +0100

    cz

--- CZ_info.php
+++ CZ_info.php
@@ -36,9 +36,9 @@
 
 <body>
 	<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/GA_tagmanager.php'; ?>
-	<div id="inhoud" style="max-width: 1400px; clear: none;">
+	<div id="inhoud" style="max-width: 1400px">
 		<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.EN.php'; ?>
-		<div id="main" style="max-width: 1400px; clear: none;"> <!-- InstanceBeginEditable name="mainpage" -->
+		<div id="main" style="clear: none;"> <!-- InstanceBeginEditable name="mainpage" -->
 			<div class="w3-center" lang="cs-CZ">
 				<h1 class="LP_colour w3-margin-bottom" style="margin-top: 0px;">Dva projekty pro každého hudebníka s profesionálním přístupem</h1>
 			</div>
