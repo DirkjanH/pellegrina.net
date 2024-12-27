296039350e56840f4f0290afd9a84d6b7093b034
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-12-27 15:03:16 +0100

    lp

--- onderhoud/LP_navigatie.php
+++ onderhoud/LP_navigatie.php
@@ -26,31 +26,31 @@ $editFormhref = $_SERVER[ 'PHP_SELF' ] . ( isset( $_SERVER[ 'QUERY_STRING' ] ) ?
 	<form name="form1" method="post" href="<?php echo $editFormhref; ?>">
 		<div class="w3-container" style="width: 80%;">
 			<div class="w3-cell-row">
-				<div class="w3-container w3-cell w3-mobile"><a class="button" href="LP_statistiek.php">Statistieken</a>
+				<div class="w3-cell w3-mobile"><a class="button" href="LP_statistiek.php">Statistieken</a>
 				</div>
-				<div class="w3-container w3-cell w3-mobile"><a class="button" href="LP_reminder.php">Aanmaning inschr.geld</a>
+				<div class="w3-cell w3-mobile"><a class="button" href="LP_reminder.php">Aanmaning inschr.geld</a>
 				</div>
-				<div class="w3-container w3-cell w3-mobile"><a class="button" href="LP_bezettingen.php">Bezettingen</a>
+				<div class="w3-cell w3-mobile"><a class="button" href="LP_bezettingen.php">Bezettingen</a>
 				</div>
-				<div class="w3-container w3-cell w3-mobile"><a class="button" href="LP_herhaling.php">Herhaling</a>
+				<div class="w3-cell w3-mobile"><a class="button" href="LP_herhaling.php">Herhaling</a>
 				</div>
-				<div class="w3-container w3-cell w3-mobile"><a class="button" href="LP_wanbetalers.php">Aanm. cursusgeld</a>
+				<div class="w3-cell w3-mobile"><a class="button" href="LP_wanbetalers.php">Aanm. cursusgeld</a>
 				</div>
-				<div class="w3-container w3-cell w3-mobile"><a class="button" href="LP_zwartelijst.php">Zwarte lijst</a>
+				<div class="w3-cell w3-mobile"><a class="button" href="LP_zwartelijst.php">Zwarte lijst</a>
 				</div>
 			</div>
 			<div class="w3-cell-row">
-				<div class="w3-container w3-cell w3-mobile"><a class="button" href="LP_persoonlijk.php">Persoonlijk</a>
+				<div class="w3-cell w3-mobile"><a class="button" href="LP_persoonlijk.php">Persoonlijk</a>
 				</div>
-				<div class="w3-container w3-cell w3-mobile"><a class="button" href="LP_inschrijving.php">Inschrijving</a>
+				<div class="w3-cell w3-mobile"><a class="button" href="LP_inschrijving.php">Inschrijving</a>
 				</div>
-				<div class="w3-container w3-cell w3-mobile"><a class="button" href="LP_financieel.php">Voorl. bevestiging</a>
+				<div class="w3-cell w3-mobile"><a class="button" href="LP_financieel.php">Voorl. bevestiging</a>
 				</div>
-				<div class="w3-container w3-cell w3-mobile"><a class="button" href="LP_rekening.php">Rekening</a>
+				<div class="w3-cell w3-mobile"><a class="button" href="LP_rekening.php">Rekening</a>
 				</div>
-				<div class="w3-container w3-cell w3-mobile"><a class="button" href="LP_betaling.php">Betaling</a>
+				<div class="w3-cell w3-mobile"><a class="button" href="LP_betaling.php">Betaling</a>
 				</div>
-				<div class="w3-container w3-cell w3-mobile"><a class="button" href="LP_betaling.php">Betaling</a>
+				<div class="w3-cell w3-mobile"><a class="button" href="LP_betaling.php">Betaling</a>
 				</div>
 			</div>
 		</div>
