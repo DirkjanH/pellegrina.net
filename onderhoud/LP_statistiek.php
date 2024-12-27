3da13cbb5cf7935f6a7529f40ee7ac00d92bec8a
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-12-27 17:40:33 +0100

    lp

--- onderhoud/LP_statistiek.php
+++ onderhoud/LP_statistiek.php
@@ -5,7 +5,7 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/includes2025.php');
 
-Kint::$enabled_mode = true;
+Kint::$enabled_mode = false;
 
 d($_GET, $_POST);
 
