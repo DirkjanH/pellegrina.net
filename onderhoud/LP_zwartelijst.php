27eac5b091c420fa6edece51e7c9c989968efe11
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-12-28 15:38:29 +0100

    lp

--- onderhoud/LP_zwartelijst.php
+++ onderhoud/LP_zwartelijst.php
@@ -1,7 +1,7 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/includes2025.php');
 // stel php in dat deze fouten weergeeft
-ini_set('display_errors',1);
+//ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 Kint::$enabled_mode = false;
