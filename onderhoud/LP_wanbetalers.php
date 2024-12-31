65ab6e20202b6290b3b79938547220270e067a93
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-12-31 16:10:59 +0100

    w

--- onderhoud/LP_wanbetalers.php
+++ onderhoud/LP_wanbetalers.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/includes2025.php');
-require_once("/LPmailer.inc.php");
+require_once("/includes/LPmailer.inc.php");
 
 Kint::$enabled_mode = false;
 
