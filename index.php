0f4e3e6cdccbf60ed0a1e721c1c915042640f314
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-01-10 19:50:01 +0100

    i

--- index.php
+++ index.php
@@ -146,6 +146,7 @@ function getisp($ip = '')
 
         img.mySlides {
             box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19) !important;
+            max-width: 640px;
         }
 
         #teller {
