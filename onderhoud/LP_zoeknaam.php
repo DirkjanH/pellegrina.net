f89806dd215079d9a83f8fc057bd406d7cfc6243
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-02-10 14:53:02 +0100

    z

--- onderhoud/LP_zoeknaam.php
+++ onderhoud/LP_zoeknaam.php
@@ -1,86 +1,89 @@
 <?php
-if (isset($_POST['DlnmrId']) and $_POST['DlnmrId'] != '')
-	$_SESSION['DlnmrId'] = $_POST['DlnmrId'];
-
 Kint::$enabled_mode = false;
 
 d($_REQUEST, $_POST, $_SESSION);
 
 $i = $eerstecursus;
 while ($i <= $laatstecursus) {
-	$cur[] = $i;
-	$i++;
+    $cur[] = $i;
+    $i++;
 }
-d($cur);
 
-if (empty($_POST['oude_dlnrs'])) $_POST['oude_dlnrs'] = '';
+if (isset($_POST['DlnmrId']) and $_POST['DlnmrId'] != '') $_SESSION['DlnmrId'] = $_POST['DlnmrId'];
+
+	if (empty($_POST['oude_dlnrs'])) $_POST['oude_dlnrs'] = '';
 if (empty($_POST['cursus'])) {
-	$_POST['cursus'] = '1';
-	$_POST['zoek'] = 'zoek';
+    $_POST['cursus'] = 'nieuw';
+    $_POST['zoek'] = 'zoek';
 }
 
 if ((isset($_POST["zoek"])) and ($_POST["zoek"] == "zoek") and ($_POST["oude_dlnrs"] == 1)) {
 
-	$zoeknaam = '-1';
-	if (isset($_POST['zoeknaam'])) {
-		$zoeknaam = $_POST['zoeknaam'];
-	}
-	$query_Inschr = sprintf("SELECT DISTINCT DlnmrId, naam FROM dlnmr WHERE naam LIKE \"%%%s%%\" 
+    // begin Recordset
+    $zoeknaam = '-1';
+    if (isset($_POST['zoeknaam'])) {
+        $zoeknaam = $_POST['zoeknaam'];
+    } // colname__Inschr
+    $query_Inschr = sprintf("SELECT DISTINCT DlnmrId, naam FROM dlnmr WHERE naam LIKE \"%%%s%%\" 
 		ORDER BY achternaam ASC", $zoeknaam);
-	$Inschr = select_query($query_Inschr);
-	if (is_array($Inschr)) $aantal_ins = count($Inschr);
-	else $aantal_ins = 0;
+    $Inschr = select_query($query_Inschr);
+    if (is_array($Inschr))
+        $aantal_ins = count($Inschr);
+    else
+        $aantal_ins = 0;
 
-	foreach ($Inschr as $ins) {
-		$query_Cursussen = sprintf(
-			"SELECT CursusId_FK FROM dlnmr, inschrijving WHERE naam = \"%s\" 
+    foreach ($Inschr as $ins) {
+        $query_Cursussen = sprintf(
+            "SELECT CursusId_FK FROM dlnmr, inschrijving WHERE naam = \"%s\" 
 			AND DlnmrId = DlnmrId_FK ORDER BY InschrId, CursusId_FK ASC",
-			$ins['naam']
-		);
-		$Cursussen = select_query($query_Cursussen);
-		d($Cursussen);
-		$grijs[$ins['DlnmrId']] = TRUE;
-		foreach ($Cursussen as $cursus) {
-			$grijs[$ins['DlnmrId']] = !(in_array($cursus['CursusId_FK'], $cur));
-		} // foreach Cursussen
-	} // foreach Inschr
+            $ins['naam']
+        );
+        $Cursussen = select_query($query_Cursussen);
+        $grijs[$ins['DlnmrId']] = TRUE;
+        if (is_array($Cursussen))
+            foreach ($Cursussen as $cursus) {
+                $grijs[$ins['DlnmrId']] = !(in_array($cursus['CursusId_FK'], $cur));
+            } // foreach Cursussen
+        else
+            echo ('Geen namen gevonden</br>');
+    } // foreach Inschr
 } // if zoek
 
 if ((isset($_POST["zoek"])) && ($_POST["zoek"] == "zoek") and ($_POST["oude_dlnrs"] != 1)) {
 
-	// begin Recordset
-	$zoeknaam = '-1';
-	if (isset($_POST['zoeknaam'])) $zoeknaam = $_POST['zoeknaam'];
-	else $zoeknaam = '';
-	// colname__Inschr
-	if (empty($_POST['cursus']) or $_POST['cursus'] == 'alles') {
-		$query_Inschr = sprintf("SELECT DISTINCT DlnmrId, naam FROM dlnmr, inschrijving WHERE naam LIKE \"%%%s%%\" 
+    // begin Recordset
+    $zoeknaam = '-1';
+    if (isset($_POST['zoeknaam'])) {
+        $zoeknaam = $_POST['zoeknaam'];
+    } // colname__Inschr
+    if (empty($_POST['cursus']) or $_POST['cursus'] == 'alles') {
+        $query_Inschr = sprintf("SELECT DISTINCT DlnmrId, naam, voorl_bev FROM dlnmr, inschrijving WHERE naam LIKE \"%%%s%%\" 
 			AND DlnmrId = DlnmrId_FK AND NOT (afgewezen <=> 1)
 			AND CursusId_FK > {$cursus_offset} AND CursusId_FK <= ({$aantal_cursussen} + {$cursus_offset}) 
 			ORDER BY achternaam ASC", $zoeknaam);
-	} else {
-		$query_Inschr = sprintf(
-			"SELECT DISTINCT DlnmrId, naam FROM dlnmr, inschrijving WHERE naam LIKE \"%%%s%%\" 
+    } else {
+        $query_Inschr = sprintf(
+            "SELECT DISTINCT DlnmrId, naam, voorl_bev FROM dlnmr, inschrijving WHERE naam LIKE \"%%%s%%\" 
 			AND DlnmrId = DlnmrId_FK AND CursusId_FK=%s AND NOT (afgewezen <=> 1) ORDER BY achternaam ASC",
-			$zoeknaam,
-			($_POST['cursus'] + $cursus_offset)
-		);
-	}
-	if ($_POST['cursus'] == 'nieuw') {
-		$query_Inschr = "SELECT DISTINCT DlnmrId, naam FROM dlnmr, inschrijving WHERE DlnmrId = DlnmrId_FK AND NOT (afgewezen <=> 1) AND achternaam NOT LIKE \"%XXX%\" AND achternaam NOT LIKE \"%YYY%\" AND achternaam NOT LIKE \"%ZZZ%\"
-			AND CursusId_FK > {$cursus_offset} AND CursusId_FK <= ({$aantal_cursussen} + {$cursus_offset}) AND (aanbet_bedrag = 0 OR 
-			aanbet_bedrag IS NULL) AND cursusgeld != 0 ORDER BY achternaam ASC";
-	}
-	d($query_Inschr);
-	$Inschr = select_query($query_Inschr);
-	if (isset($Inschr) and is_array($Inschr)) $aantal_ins = count($Inschr);
-	else $aantal_ins = 0;
-	d($Inschr, $aantal_ins);
+            $zoeknaam,
+            ((int) $_POST['cursus'] + $cursus_offset)
+        );
+    }
+    if ($_POST['cursus'] == 'nieuw') {
+        $query_Inschr = "SELECT DISTINCT DlnmrId, naam FROM dlnmr, inschrijving WHERE DlnmrId = DlnmrId_FK AND NOT (afgewezen <=> 1)
+			AND CursusId_FK > {$cursus_offset} AND CursusId_FK <= ({$aantal_cursussen} + {$cursus_offset}) AND NOT(aangenomen <=> 1) ORDER BY achternaam ASC";
+    }
+    $Inschr = select_query($query_Inschr);
+    if (isset($Inschr) and is_array($Inschr))
+        $aantal_ins = count($Inschr);
+    else
+        $aantal_ins = 0;
 } // if zoek
 
 function check($input)
 {
-	if (isset($_POST['cursus']) and $_POST['cursus'] == $input) echo 'checked';
+    if (isset($_POST['cursus']) and $_POST['cursus'] == $input)
+        echo 'checked';
 }
 
 ?>
