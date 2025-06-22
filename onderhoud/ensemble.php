8a79da8613f6b437df88d339d5b3264ed8873fa5
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-06-22 22:27:53 +0200

    m

--- onderhoud/ensemble.php
+++ onderhoud/ensemble.php
@@ -9,7 +9,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/includes2025.php');
 session_start();
 ob_start();
 
-Kint::$enabled_mode = false;
+Kint::$enabled_mode = true;
 
 d($_GET, $_POST);
 
