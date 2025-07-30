bf32fb9b76591d0a7c7a941da68517176fb252f7
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-07-30 19:51:54 +0200

    cz

--- algemeen/CZ_prijzen.php
+++ algemeen/CZ_prijzen.php
@@ -36,13 +36,13 @@ $i = $eerstecursus; ?>
       <th align="RIGHT"><b>Studentsk&aacute; cena:</b></th>
     </tr>
     <tr valign="TOP">
-    	<td>1. REICHOVO REQUIEM<span class="titel"> pro orchestr, sbor a sólisty</span></td>
+    	<td>1. Dvořákův Zlatý kolovrat a Haydnova <i>Missa in tempore belli<span class="titel"> pro orchestr, sbor a sólisty</span></td>
     	<td><?php echo czk($cursussen[$i]['prijs_cr']); ?> / <?php echo czk($cursussen[$i]['prijs_cr'] - $cursussen[$i]['korting_vroeg_cr']); ?> <span class="nadruk">*)</span></td>
     	<td><?php echo czk($cursussen[$i]['prijs_cr_student']); ?> / <?php echo czk($cursussen[$i]['prijs_cr_student'] - $cursussen[$i]['korting_vroeg_cr']); ?> <span class="nadruk">*)</span></td>
     	<?php $i = $eerstecursus + 0; ?>
     	</tr>
 	  <tr valign="TOP">
-    	<td>2. <span class="titel">BAROKO v 415 Hz: Vivaldiho Benátské nešpory</span></td>
+    	<td>2. <span class="titel">BAROKO v 415 Hz: Purcell & Handel</span></td>
     	<td><?php echo czk($cursussen[$i+1]['prijs_cr']); ?> / <?php echo czk($cursussen[$i+1]['prijs_cr'] - $cursussen[$i+1]['korting_vroeg_cr']); ?> <span class="nadruk">*)</span></td>
     	<td><?php echo czk($cursussen[$i+1]['prijs_cr_student']); ?> / <?php echo czk($cursussen[$i+1]['prijs_cr_student'] - $cursussen[$i+1]['korting_vroeg_cr']); ?> <span class="nadruk">*)</span></td>
     	<?php $i = $eerstecursus + 0; ?>
