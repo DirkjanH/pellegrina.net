8f14529885e3e060be83d69553d3d7a4a6a1c053
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-11-10 11:58:05 +0100

    m

--- includes/akkoord_voorwaarden_EN.php
+++ includes/akkoord_voorwaarden_EN.php
@@ -5,6 +5,6 @@
           <td valign="bottom" class="linkerkolom"><input type="checkbox" name="voorwaarden" value="akkoord"
               <?php if (isset($_POST['voorwaarden'])) echo 'checked'; ?>></td>
           <td>I agree to the terms and conditions as stated under <a href="praktisch.php">practical matters</a> and elsewhere on the site <span class="nadruk">(mandatory!)</span> <br>
-            I understand that my personal data will be stored for practical reasons related to the organization of the summer course. My data will be made available to other participants and teachers. They will never be shared with other parties without my express consent.
+            I understand that my personal data will be stored for practical reasons related to the organization of the summer course. My data will be made available to other participants and tutors. They will never be shared with other parties without my express consent.
         </tr>
       </table>
