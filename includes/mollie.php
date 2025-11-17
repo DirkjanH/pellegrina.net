833dcfe24c3c38dcca155067a81a8138ef6acdeb
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-11-17 17:38:34 +0100

    m

--- includes/mollie.php
+++ includes/mollie.php
@@ -2,7 +2,7 @@
 require_once($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php');
 
 $mollie = new \Mollie\Api\MollieApiClient();
-$mollie->setApiKey("test_PqGy4z4RKMVpgQHzyaxSPSKsjQd5Gq"); // La Pellegrina test
-//$mollie->setApiKey("live_pSRgw8xxa67nTg834W7NyKb6eW3WVU"); // La Pellegrina live
+//$mollie->setApiKey("test_PqGy4z4RKMVpgQHzyaxSPSKsjQd5Gq"); // La Pellegrina test
+$mollie->setApiKey("live_pSRgw8xxa67nTg834W7NyKb6eW3WVU"); // La Pellegrina live
 d($mollie);
 ?>
