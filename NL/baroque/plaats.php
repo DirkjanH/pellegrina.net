9118fce686167936863ad7b2db411c3fff16d4ef
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-11-20 17:45:05 +0100

    m

--- NL/baroque/plaats.php
+++ NL/baroque/plaats.php
@@ -5,12 +5,12 @@ $filenaam = explode('/', str_replace('/var', '', dirname(__FILE__)));
 $filenaam[7] = basename(__FILE__, ".php");
 //	print_r($filenaam);	
 switch ($filenaam[6]) {
-	case 'romantic':
-		$cursus = 1;
-		break;
-	case 'baroque':
-		$cursus = 2;
-		break;
+    case 'romantic':
+        $cursus = 1;
+        break;
+    case 'baroque':
+        $cursus = 2;
+        break;
 }
 //	echo 'Cursus is: '.$cursus.'<br>';
 $taal = $filenaam[5];
@@ -20,6 +20,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/cursusdata.php'; ?>
 <!DOCTYPE HTML>
 <html>
 <!-- InstanceBegin template="/Templates/LP_NL.dwt.php" codeOutsideHTMLIsLocked="false" -->
+
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
@@ -35,47 +36,56 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/cursusdata.php'; ?>
     <!-- InstanceEndEditable -->
     <!-- Facebook Pixel Code -->
     <script>
-    ! function(f, b, e, v, n, t, s) {
-        if (f.fbq) return;
-        n = f.fbq = function() {
-            n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(
-                arguments)
-        };
-        if (!f._fbq) f._fbq = n;
-        n.push = n;
-        n.loaded = !0;
-        n.version = '2.0';
-        n.queue = [];
-        t = b.createElement(e);
-        t.async = !0;
-        t.src = v;
-        s = b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t, s)
-    }(window, document, 'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '537749209897328');
-    fbq('track', 'PageView');
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(
+                    arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '537749209897328');
+        fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
             src="https://www.facebook.com/tr?id=537749209897328&ev=PageView&noscript=1" /></noscript>
     <!-- End Facebook Pixel Code -->
 </head>
+
 <body>
     <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/GA_tagmanager.php'; ?>
     <div id="inhoud" class="w3-main"> <?php
-										echo $navigatie;
-										echo '<span class="w3-opennav w3-xxlarge w3-hide-large" onclick="w3_open()">☰</span>';
-										require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.NL.php';
-										?> <div id="main">
+                                        echo $navigatie;
+                                        echo '<span class="w3-opennav w3-xxlarge w3-hide-large" onclick="w3_open()">☰</span>';
+                                        require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.NL.php';
+                                        ?> <div id="main">
             <!-- InstanceBeginEditable name="mainpage" --> <?php //require_once $_SERVER['DOCUMENT_ROOT'].'/NL/budejovice.php'; 
-															?> <div class="fotocenter"><img
+                                                            ?> <div class="fotocenter"><img
                     src="/Images/Locaties/Nieuw Sion/Nieuw Sion.jpg"
                     class="w3-image" alt="Klooster Nieuw Sion" /></div>
             <p>Klooster Nieuw Sion bestaat sinds 1890 en was tot 2015 een
                 trappistenklooster. De laatste monniken trokken toen de deur
                 achter zich dicht en vonden een nieuwe plek op Schiermonnikoog.
                 Sindsdien vindt er een breed scala van spirituele, maar ook
-                culturele en muzikale activiteiten plaats. Meer informatie vind
+                culturele en muzikale activiteiten plaats. </p>
+            <p> Een groeiend aantal mensen streeft naar een kloosterleven waarin stilte en spiritualiteit
+                centraal staan. Driemaal daags zijn er stiltegebeden in de
+                kloosterkerk. Daarnaast verzorgen ze de grote groentetuin,
+                houden de brouwerij en het koffiehuis draaiende en organiseren
+                een groot aantal activiteiten, zoals retraites, jeugdweekenden
+                weekenden en dergelijke. </p>
+            <p>De inkomsten van al deze activiteiten
+                jaar betalen kleine reconstructies van het klooster. Er zijn nu bijvoorbeeld nieuwe een- en tweepersoonskamers met eigen badkamer beschikbaar in het gerenoveerde Poortgebouw.Meer informatie vind
                 je hier:</p>
             <ul>
                 <li><a href="https://indebuurt.nl/deventer/woning-van-de-week/binnenkijken-bij-klooster-nieuw-sion-de-monniken-zijn-verhuisd-maar-er-wonen-nog-wel-mensen~108570/"
@@ -116,4 +126,5 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/cursusdata.php'; ?>
 </body>
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
 <!-- InstanceEnd -->
+
 </html>
