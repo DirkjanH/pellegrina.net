a22c9cfc00d401c51cb366e0801dd057b7a0af42
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-11-26 12:19:50 +0100

    m

--- EN/baroque/aanmelding.php
+++ EN/baroque/aanmelding.php
@@ -324,15 +324,15 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/ToggleText.php');
                     <td class="linkerkolom"><input type="checkbox" name="hotel_2pp" id="hotel_2pp" value="1"
                             <?php if (isset($_POST["hotel_2pp"]) and $_POST["hotel_2pp"] == 1) echo "checked"; ?>>
                     </td>
-                    <td>I would prefer a place in a double room with private bathroom facilities in the Gatehouse (Poortgebouw) <span class="nadruk">(additional charge <?php echo euro($cursusdata["hotel_2pp"]) ?>; limited availability)</span></td>
+                    <td>I would prefer a place in a double room with private bathroom facilities in the Gatehouse (Poortgebouw) <span class="nadruk">(additional charge <?php echo euro($cursusdata["hotel_2pp"]); ?>; limited availability)</span></td>
                 </tr>
                 <tr>
                     <td class="linkerkolom"><input type="checkbox"
                             name="eenpersoons" id="eenpersoons" value="1"
                             <?php if (isset($_POST["eenpersoons"]) and $_POST["eenpersoons"] == 1) echo "checked"; ?>>
                     </td>
-                    <td>I would prefer a single room in the Guest House (Gastenverblijf) <span class="nadruk">(additional charge <?php echo euro($cursusdata["eenpersoons"]) ?>; limited
-                            availability)</span></td>
+                    <td>I would prefer a single room in the Guest House (Gastenverblijf) <span class="nadruk">(additional charge 
+                        <?php echo euro($cursusdata["eenpersoons"]); ?>; limited availability)</span></td>
                 </tr>
                 <tr>
                     <td class="linkerkolom"><input type="checkbox" name="hotel_1pp" id="hotel_1pp" value="1"
