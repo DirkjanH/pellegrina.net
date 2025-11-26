2872e5388dc2197cec29067df8bca0c066caa60a
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-11-26 13:32:03 +0100

    m

--- NL/baroque/praktisch.php
+++ NL/baroque/praktisch.php
@@ -71,11 +71,158 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/cursusdata.php'; ?>
     require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.NL.php';
     ?>
     <div id="main">
-      <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/NL/praktisch_NS.php'; ?>
+      <h2 class="begin">Praktische gegevens & prijzen
+        "<?php echo $cursusdata['cursusnaam_nl']; ?>"</h2>
+      <h3>Toelating</h3>
+      <p> Omdat bij het samenstellen van de groep wordt gestreefd naar een
+        evenwichtig geheel, wordt bij toelating in de eerste plaats gekeken naar
+        niveau, in het belang van alle deelnemers. Vaak moeten we kiezen uit een
+        te groot aanbod van belangstellenden voor een bepaald instrument, met
+        name bij blazers en bepaalde stemtypen. We letten dan in de eerste
+        plaats op muzikale opleiding, ervaring en inzicht. Het is daarom
+        belangrijk om je muzikale niveau goed te omschrijven. Bij kandidaten
+        voor een bepaald instrument of stemsoort met een gelijkwaardig niveau
+        heeft uiteraard degene die zich het eerst heeft aangemeld voorrang.
+        Wanneer je twijfelt over je niveau, neem dan <a
+          href="contact.php">contact op</a> met <i>La Pellegrina</i>. </p>
+      <p>Om bovenstaande reden worden de aanmeldingen in eerste instantie
+        verzameld. Op <?php echo $cursusdata['beslisdatum']; ?> wordt bekend
+        gemaakt wie er geplaatst kan worden en wie niet. In verband met het
+        beperkte aantal plaatsen is het dus aan te raden zo snel mogelijk in te
+        schrijven. Inschrijving na <?php echo $cursusdata['beslisdatum']; ?> is
+        nog mogelijk, maar uitsluitend voor de plaatsen die dan nog vacant zijn
+        en vermeld op deze website.</p>
+      <p class="citaat">Robert Klotz: &ldquo;Ik vloog een oceaan over om La
+        Pellegrina bij te wonen. Zou zwemmen om er volgend jaar weer heen te
+        kunnen.&rdquo;</p>
+      <h3>Aanmelding en bevestiging</h3>
+      <p>Je kunt je inschrijven met het aanmeldingsformulier op deze site (zie het
+        betreffende project), dat <strong>volledig</strong> ingevuld moet
+        worden. De aanbetaling van
+        <strong><?php echo euro($cursusdata['inschrijfgeld']); ?></strong> kun
+        je gelijk met de aanmelding betalen, of apart overmaken op rekening
+        <strong>NL33 ASNB 0707 2500 72 </strong>t.n.v. <em>La Pellegrina</em> te
+        Utrecht. Je ontvangt dan een voorlopige bevestiging van inschrijving.
+        Als je niet geplaatst kunt worden, wordt de aanbetaling uiteraard
+        teruggestort. Het volledige cursusgeld moet uiterlijk op
+        <?php echo $cursusdata['betaaldatum']; ?> betaald zijn. Alle eventuele
+        bank en creditcardkosten zijn voor rekening van de deelnemer.
+      </p>
+      <p class="citaat">Marrie Kardol: &ldquo;De prachtige inspirerende omgeving,
+        het ordelijke verloop van het programma, de goede voorbereiding en de
+        vriendelijke en prettige manier waarop de tutors omgaan met de
+        deelnemers, dit alles maakt Pellegrina tot een aanrader voor elke
+        muziekliefhebber!&rdquo;</p>
+      <h3>Prijzen</h3>
+      <p>De prijs van het project is <strong><?php echo euro($cursusdata['prijs_volledig']); ?></strong>&nbsp;per
+        persoon, inclusief accommodatie in een tweepersoons kamer en alle maaltijden. Studenten en iedereen tot 36 jaar betalen
+        <strong><?php echo euro($cursusdata['prijs_student']); ?></strong>. Als je je tijdig inschrijft krijg je <strong><?php echo euro($cursusdata['korting_vroeg']); ?>
+          korting</strong>. Als je kampeert of zelf je accommodatie regelt krijg je korting; eenpersoons kamers zijn mogelijk tegen een meerprijs.
+      </p>
+      <h4>Aanzienlijke korting voor studenten en jongeren </h4>
+      <p><em>Voltijds</em> studenten aan het wetenschappelijk en hoger beroepsonderwijs en deelnemers t/m 35 jaar (bij aanvang van de cursus
+        waarvoor ze zich hebben aangemeld), krijgen een aanzienlijke korting en betalen strong><?php echo euro($cursusdata['prijs_student']); ?></strong> cursusgeld per persoon, op basis van een tweepersoons kamer. Stuur bij inschrijving een kopie of een scan van het inschrijvingsbewijs (of van je paspoort als bewijs van je leeftijd) mee.
+      </p>
+      <h3>Accommodatie</h3>
+      <h4>Een- en tweepersoons kamers in Klooster Nieuw Sion</h4>
+      <p>De meeste deelnemers verblijven in klooster Nieuw Sion. Er zijn twee plekken waar mensen kunnen verblijven, het Gastenverblijf en het Poortgebouw. </p>
+      <p>Het Gastenverblijf heeft één tweepersoons en dertien eenpersoons kamers. De voorzieningen zijn er simpel maar schoon, met een gemeenschappelijke sanitaire ruimte.
+      </p>
+      <p>Daarnaast is er sinds kort het geheel gerenoveerde en comfortabele Poortgebouw, met drie eenpersoonskamers met gedeeld sanitair en vijf tweepersoonskamers met eigen sanitair.</p>
+      <h4>Kamperen in de kloostertuin</h4>
+      <p>Er zijn een paar kampeerplekken in de notenboomgaard van het klooster beschikbaar. Tenten, campers en caravans zijn welkom; elektriciteit is NIET beschikbaar. Het aantal plaatsen is beperkt, er zijn eenvoudige gedeelde sanitaire voorzieningen in het klooster.</p>
+      <h4>Eigen accommodatie</h4>
+      <p>Het is mogelijk aan de cursus deel te nemen op basis van eigen accommodatie, bijvoorbeeld voor diegenen die in de buurt wonen of zelf
+        accommodatie kunnen regelen. In dat geval betaal je naast het cursusgeld alleen een bijdrage voor koffie en thee en de gezamenlijke lunch. Er
+        zijn diverse hotels en Bed & Breakfast gelegenheden in de buurt. Het mogelijk om tegen een meerprijs ook het avondeten in het Klooster te krijgen.</p>
+      <h3>Maaltijden</h3>
+      <p>Tijdens de cursus beschikken we over een aparte eetzaal, waar koffie en thee voor ons klaarstaan. Er is een uitgebreid ontbijtbuffet. De lunch
+        en de avondmaaltijd worden als het weer het toestaat op het terras buiten geserveerd.</p>
+      <h3>Alle prijzen</h3>
+      <table class="w3-table-all">
+        <tr>
+          <th>Overzicht van alle prijzen en kortingen</th>
+          <th class="w3-right-align">Bij inschrijving <br />vòòr <?php echo $cursusdata['beslisdatum']; ?>:</th>
+          <th class="w3-right-align">Bij inschrijving <br />op of na <?php echo $cursusdata['beslisdatum']; ?>:</th>
+        </tr>
+        <tr>
+          <td>Deelname incl. tweepersoons kamer in het Gastenverblijf, met gedeeld sanitair</td>
+          <td class="geld"><?php echo euro($cursusdata['prijs_volledig'] - $cursusdata['korting_vroeg']); ?></td>
+          <td class="geld"><?php echo euro($cursusdata['prijs_volledig']); ?></td>
+        </tr>
+        <tr>
+          <td>Deelname student c.q. deelnemer t/m 35 jaar incl. tweepersoons kamer in het Gastenverblijf, met gedeeld sanitair</td>
+          <td class="geld"><?php echo euro($cursusdata['prijs_student'] - $cursusdata['korting_vroeg']); ?></td>
+          <td class="geld"><?php echo euro($cursusdata['prijs_student']); ?></td>
+        </tr>
+        <tr class="onzichtbaar">
+          <td>Deelname toehoorder incl. tweepersoons kamer in het Gastenverblijf, met gedeeld sanitair</td>
+          <td class="geld"><?php echo euro($cursusdata['toehoorder']); ?></td>
+          <td class="geld"><?php echo euro($cursusdata['toehoorder']); ?></td>
+        </tr>
+        <tr>
+          <td>Meerprijs eenpersoons kamer in het Gastenverblijf, met gedeeld sanitair</td>
+          <td class="geld"><?php echo euro($cursusdata['eenpers']); ?></td>
+          <td class="geld"><?php echo euro($cursusdata['eenpers']); ?></td>
+        </tr>
+        <tr>
+          <td>Meerprijs eenpersoons kamer in het Poortgebouw, met gedeeld sanitair</td>
+          <td class="geld"><?php echo euro($cursusdata['hotel_1pp']); ?></td>
+          <td class="geld"><?php echo euro($cursusdata['hotel_1pp']); ?></td>
+        </tr>
+        <tr>
+          <td>Meerprijs plaats in tweepersoons kamer in het Poortgebouw, met eigen sanitair</td>
+          <td class="geld"><?php echo euro($cursusdata['hotel_2pp']); ?></td>
+          <td class="geld"><?php echo euro($cursusdata['hotel_2pp']); ?></td>
+        </tr>
+        <tr>
+          <td>Korting voor kamperen in de kloostertuin <span class="nadruk">(per persoon, incl. alle maaltijden)</span></td>
+          <td class="geld"><?php echo euro($cursusdata['kamperen']); ?></td>
+          <td class="geld"><?php echo euro($cursusdata['kamperen']); ?></td>
+        </tr>
+        <tr>
+          <td>Korting voor eigen accommodatie <span class="nadruk">(de lunch en koffie/thee is wel inbegrepen, ontbijt en avondeten niet)</span></td>
+          <td class="geld"><?php echo euro($cursusdata['korting_eigen_acc']); ?></td>
+          <td class="geld"><?php echo euro($cursusdata['korting_eigen_acc']); ?></td>
+        </tr>
+        <tr>
+          <td>Toeslag voor het gezamenlijke avondeten <span class="nadruk">(alleen nodig bij eigen accommodatie;
+              voor andere deelnemers is het avondeten standaard inbegrepen)</span></td>
+          <td class="geld"><?php echo euro($cursusdata['diner']); ?></td>
+          <td class="geld"><?php echo euro($cursusdata['diner']); ?></td>
+        </tr>
+        <tr>
+          <td>Korting voor deelname aan meer dan één cursus van <em>La Pellegrina</em> <span class="nadruk">(binnen één zomer)</span></td>
+          <td class="geld"><?php echo euro($cursusdata['korting_meer'] * 2); ?></td>
+          <td class="geld"><?php echo euro($cursusdata['korting_meer'] * 2); ?></td>
+        </tr>
+      </table>
+      <h3> Voertaal</h3>
+      <p>De projecten van <i>La Pellegrina</i> worden internationaal aangeboden.
+        Er wordt Nederlands, Duits en Engels gesproken. De coaching zal
+        gedeeltelijk in het Engels zijn.</p>
+      <p class="citaat">Jean Walker: Ik denk elk jaar dat het bij terugkomst niet
+        zo goed kan zijn als het jaar ervoor... en dan is het dat toch!!!</p>
+      <p class="onzichtbaar citaat">Katy en Jan de Jongh: 'V&oacute;&oacute;r het
+        project gaan we een paar dagen naar Praag, en erna uitrusten in de
+        omgeving. Zuid-Bohemen is mooi en goedkoop, dan kun je nog eens wat
+        langer blijven.'</p>
+      <h3><a name="annul"></a>Annuleringsregeling</h3>
+      <p>Als je onverhoopt niet geplaatst kunt worden voor een cursus, of als
+        <i>La Pellegrina</i> het project zou moeten annuleren, ontvang je
+        uiteraard het gehele betaalde bedrag terug. Annuleer je zelf
+        v&oacute;&oacute;r of op <?php echo $cursusdata['betaaldatum']; ?>, dan
+        is het niet mogelijk je aanbetaling te retourneren. Annuleer je na
+        <?php echo $cursusdata['betaaldatum']; ?>, dan ben je het totale
+        cursusgeld verschuldigd. Het is aan te raden tijdig een
+        annuleringsverzekering af te sluiten.
+      </p>
+      <p><i>La Pellegrina</i> behoudt zich het recht voor om, indien noodzakelijk,
+        docenten en programma te wijzigen.</p>
       <h2><a href="javascript: history.go(-1)">Terug</a></h2>
     </div>
   </div>
+  <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
 </body>
-<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
 
 </html>
