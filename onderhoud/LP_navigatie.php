a342c90b652781804770e7c5f66dc5d1b02cf4ea
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-12-03 14:33:47 +0100

    m

--- onderhoud/LP_navigatie.php
+++ onderhoud/LP_navigatie.php
@@ -16,7 +16,7 @@ $editFormhref = $_SERVER['PHP_SELF'] . (isset($_SERVER['QUERY_STRING']) ? "?" . 
 
     .grid_item {
         width: auto;
-        background-color: rgba(255, 255, 255, 0.8);
+        background-color: rgba(172, 227, 63, 0.8);
         padding: 4px;
         font-size: 30px;
         text-align: center;
@@ -35,38 +35,25 @@ $editFormhref = $_SERVER['PHP_SELF'] . (isset($_SERVER['QUERY_STRING']) ? "?" . 
     <form name="form1" method="post" href="<?php echo $editFormhref; ?>">
         <div class="container">
             <div class="grid_item"><a class="button"
-                    href="LP_statistiek.php">Statistieken</a>
-            </div>
+                    href="LP_statistiek.php">Statistieken</a></div>
             <div class="grid_item"><a class="button"
-                    href="LP_reminder.php">Aanmaning inschr.geld</a>
-            </div>
+                    href="LP_persoonlijk.php">Persoonlijk</a></div>
             <div class="grid_item"><a class="button"
-                    href="LP_bezettingen.php">Bezettingen</a>
-            </div>
+                    href="LP_inschrijving.php">Inschrijving</a></div>
             <div class="grid_item"><a class="button"
-                    href="LP_herhaling.php">Herhaling</a>
-            </div>
+                    href="LP_reminder.php">Aanmaning inschr.geld</a></div>
             <div class="grid_item"><a class="button"
-                    href="LP_wanbetalers.php">Aanm. cursusgeld</a>
-            </div>
+                    href="LP_financieel.php">Voorl. bevestiging</a></div>
             <div class="grid_item"><a class="button"
-                    href="LP_zwartelijst.php">Zwarte lijst</a>
-            </div>
+                    href="LP_rekening.php">Rekening</a></div>
             <div class="grid_item"><a class="button"
-                    href="LP_persoonlijk.php">Persoonlijk</a>
-            </div>
+                    href="LP_betaling.php">Betaling</a></div>
             <div class="grid_item"><a class="button"
-                    href="LP_inschrijving.php">Inschrijving</a>
-            </div>
+                    href="LP_bezettingen.php">Bezettingen</a></div>
             <div class="grid_item"><a class="button"
-                    href="LP_financieel.php">Voorl. bevestiging</a>
-            </div>
+                    href="LP_herhaling.php">Herhaling</a></div>
             <div class="grid_item"><a class="button"
-                    href="LP_rekening.php">Rekening</a>
-            </div>
-            <div class="grid_item"><a class="button"
-                    href="LP_betaling.php">Betaling</a>
-            </div>
+                    href="LP_zwartelijst.php">Zwarte lijst</a></div>
         </div>
     </form>
 </body>
