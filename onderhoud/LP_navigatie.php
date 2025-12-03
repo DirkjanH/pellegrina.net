e5537d1202d33535f58e91178fdb498d5e7cbdf9
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-12-03 14:22:48 +0100

    m

--- onderhoud/LP_navigatie.php
+++ onderhoud/LP_navigatie.php
@@ -14,6 +14,14 @@ $editFormhref = $_SERVER['PHP_SELF'] . (isset($_SERVER['QUERY_STRING']) ? "?" . 
         background-color: beige;
     }
 
+    .grid_item {
+        background-color: rgba(255, 255, 255, 0.8);
+        border: 1px solid rgba(0, 0, 0, 0.8);
+        padding: 20px;
+        font-size: 30px;
+        text-align: center;
+    }
+
     /* Make a one column-layout instead of three-column layout */
     @media (max-width: 600px) {
         .container {
@@ -29,34 +37,34 @@ $editFormhref = $_SERVER['PHP_SELF'] . (isset($_SERVER['QUERY_STRING']) ? "?" . 
             <div class="w3-button w3-mobile><a class=" button"
                 href="LP_statistiek.php">Statistieken</a>
             </div>
-            <div class="w3-button w3-mobile w3-hover-yellow"><a class="button"
+            <div class="grid_item"><a class="button"
                     href="LP_reminder.php">Aanmaning inschr.geld</a>
             </div>
-            <div class="w3-button w3-mobile w3-hover-yellow"><a class="button"
+            <div class="grid_item"><a class="button"
                     href="LP_bezettingen.php">Bezettingen</a>
             </div>
-            <div class="w3-button w3-mobile w3-hover-yellow"><a class="button"
+            <div class="grid_item"><a class="button"
                     href="LP_herhaling.php">Herhaling</a>
             </div>
-            <div class="w3-button w3-mobile w3-hover-yellow"><a class="button"
+            <div class="grid_item"><a class="button"
                     href="LP_wanbetalers.php">Aanm. cursusgeld</a>
             </div>
-            <div class="w3-button w3-mobile w3-hover-yellow"><a class="button"
+            <div class="grid_item"><a class="button"
                     href="LP_zwartelijst.php">Zwarte lijst</a>
             </div>
-            <div class="w3-button w3-mobile w3-hover-yellow"><a class="button"
+            <div class="grid_item"><a class="button"
                     href="LP_persoonlijk.php">Persoonlijk</a>
             </div>
-            <div class="w3-button w3-mobile w3-hover-yellow"><a class="button"
+            <div class="grid_item"><a class="button"
                     href="LP_inschrijving.php">Inschrijving</a>
             </div>
-            <div class="w3-button w3-mobile w3-hover-yellow"><a class="button"
+            <div class="grid_item"><a class="button"
                     href="LP_financieel.php">Voorl. bevestiging</a>
             </div>
-            <div class="w3-button w3-mobile w3-hover-yellow"><a class="button"
+            <div class="grid_item"><a class="button"
                     href="LP_rekening.php">Rekening</a>
             </div>
-            <div class="w3-button w3-mobile w3-hover-yellow"><a class="button"
+            <div class="grid_item"><a class="button"
                     href="LP_betaling.php">Betaling</a>
             </div>
         </div>
