d7fb67430145ab8d3d4cf73e3147cef223e1157a
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-12-04 12:47:35 +0100

    m

--- onderhoud/LP_navigatie.php
+++ onderhoud/LP_navigatie.php
@@ -17,7 +17,7 @@ $editFormhref = $_SERVER['PHP_SELF'] . (isset($_SERVER['QUERY_STRING']) ? "?" . 
 
     .grid_item {
         width: 200px;
-        background-color: rgba(172, 227, 63, 0.8);
+        background-color: palegoldenrod;
         text-align: center;
         border: 1px solid grey;
     }
