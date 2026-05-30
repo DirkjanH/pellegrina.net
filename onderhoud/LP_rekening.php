25415222923f285ec810baa92aa4f2f548322b52
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-05-30 14:02:28 +0200

    m

--- onderhoud/LP_rekening.php
+++ onderhoud/LP_rekening.php
@@ -19,8 +19,8 @@ else $id = $_GET['DlnmrId'];
 if (isset($_GET['Reset']) and $_GET['Reset'] == 'Wis') {
     unset($_SESSION, $_GET, $_GET['DlnmrId'], $_POST, $_POST['DlnmrId'], $_REQUEST, $_SESSION['DlnmrId']);
     d($_SESSION, $_GET, $_GET['DlnmrId'], $_POST, $_POST['DlnmrId'], $_REQUEST, $_SESSION['DlnmrId']);
-    d(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
-    header("Location: " . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
+    d(strtok($_SERVER["REQUEST_URI"], '?'));
+    header("Location: " . strtok($_SERVER["REQUEST_URI"], '?'));
     $id = -1;
     exit();
 }
@@ -391,9 +391,8 @@ if ((isset($_POST["verzend"])) && ($_POST["verzend"] == "Maak rekeningen")) {
                     <td colspan="2">
                         <form id="zoek" name="zoek" method="get"
                             action="<?php echo $editFormAction; ?>"> Id: <input
-                                name="DlnmrId" type="text"
-                                value="<?php if (isset($_GET['DlnmrId']))
-                                            echo $_GET['DlnmrId']; ?>"
+                                name="DlnmrId" type="text" value="<?php if (isset($_GET['DlnmrId']))
+                                                                        echo $_GET['DlnmrId']; ?>"
                                 size="5" />
                             <input type="submit" name="Submit" value="Zoek">
                             <input type="submit" name="Reset" value="Wis">
