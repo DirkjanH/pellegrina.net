f772ee4e9cc28031b8974466208053d0566b889b
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-05-30 14:19:11 +0200

    m

--- onderhoud/LP_rekening.php
+++ onderhoud/LP_rekening.php
@@ -22,7 +22,7 @@ if (empty($_GET['DlnmrId']) or $_GET['DlnmrId'] == "") $id = -1;
 else $id = $_GET['DlnmrId'];
 if (isset($_GET['Reset']) and $_GET['Reset'] == 'Wis') {
     unset($_SESSION, $_GET, $_GET['DlnmrId'], $_POST, $_POST['DlnmrId'], $_REQUEST, $_SESSION['DlnmrId']);
-    $_SESSION['DlnmrId'] = $_POST['DlnmrId'] = $_GET['DlnmrId'] = '';
+    $_SESSION['DlnmrId'] = $_POST['DlnmrId'] = $_GET['DlnmrId'] = '-1';
     d($_SESSION, $_GET, $_GET['DlnmrId'], $_POST, $_POST['DlnmrId'], $_REQUEST, $_SESSION['DlnmrId']);
     d($_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']
         . explode('?', $_SERVER['REQUEST_URI'], 2)[0]);
@@ -398,9 +398,8 @@ if ((isset($_POST["verzend"])) && ($_POST["verzend"] == "Maak rekeningen")) {
                     <td colspan="2">
                         <form id="zoek" name="zoek" method="get"
                             action="<?php echo $editFormAction; ?>"> Id: <input
-                                name="DlnmrId" type="text"
-                                value="<?php if (isset($_GET['DlnmrId']) and $_GET['Reset'] != 'Wis')
-                                            echo $_GET['DlnmrId']; ?>"
+                                name="DlnmrId" type="text" value="<?php if (isset($_GET['DlnmrId']) and $_GET['Reset'] != 'Wis')
+                                                                        echo $_GET['DlnmrId']; ?>"
                                 size="5" />
                             <input type="submit" name="Submit" value="Zoek">
                             <input type="submit" name="Reset" value="Wis">
