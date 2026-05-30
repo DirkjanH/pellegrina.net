91986026b6066849e88b5184227217f857f728b9
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-05-30 15:38:28 +0200

    m

--- onderhoud/LP_betaling.php
+++ onderhoud/LP_betaling.php
@@ -339,32 +339,32 @@ $openstaand_giraal = euro2($openstaand_bedrag['totaal'] - $openstaand_cashbedrag
 						<td colspan="2">
 							<p class="groot">Nog openstaande rekeningen:</p>
 							<div id="openstaand">
-								<ul id="navlist"> <?php
-													foreach ($openstaand as $open) {
-														$grijs = TRUE;
-														if (stripos($open['rekening_opmerking'], 'Betaling ter plekke in cash') === FALSE)
-															$grijs =  FALSE;
-														$bedrag = $open['cursusgeld'] + $open['donatie']
-															- $open['aanbet_bedrag'];
-														$opens = '<li id="active"><a href="javascript:SetInschId(';
-														$opens .= $open['InschId'] . ')">';
-														if ($grijs) $opens .= '<span class="grijs">';
-														$opens .= $open['naam'] . ' (';
-														$opens .= euro2($bedrag);
-														$opens .= ")";
-														if ($grijs) $opens .= '</span>';
-														$opens .= "</a></li>\n";
-														echo $opens;
-													}
-													echo '<p>Aantal nog openstaande rekeningen: ' . $totalRows_openstaand . "; Totaal nog openstaand bedrag: cash {$openstaand_cashbedrag['Etotaal']} + giraal {$openstaand_giraal} = {$openstaand_bedrag['Etotaal']}<br>";
-													foreach ($openstaand_bedrag as $key => $value) {
-														if (strpos($key, 'totaal') === false) {
-															echo "Cursus {$key}: ";
-															echo $value . " | ";
-														}
-													}
-													echo '</p>';
-													?> </ul>
+								<ul> <?php
+										foreach ($openstaand as $open) {
+											$grijs = TRUE;
+											if (stripos($open['rekening_opmerking'], 'Betaling ter plekke in cash') === FALSE)
+												$grijs =  FALSE;
+											$bedrag = $open['cursusgeld'] + $open['donatie']
+												- $open['aanbet_bedrag'];
+											$opens = '<li id="active"><a href="javascript:SetInschId(';
+											$opens .= $open['InschId'] . ')">';
+											if ($grijs) $opens .= '<span class="grijs">';
+											$opens .= $open['naam'] . ' (';
+											$opens .= euro2($bedrag);
+											$opens .= ")";
+											if ($grijs) $opens .= '</span>';
+											$opens .= "</a></li>\n";
+											echo $opens;
+										}
+										echo '<p>Aantal nog openstaande rekeningen: ' . $totalRows_openstaand . "; Totaal nog openstaand bedrag: cash {$openstaand_cashbedrag['Etotaal']} + giraal {$openstaand_giraal} = {$openstaand_bedrag['Etotaal']}<br>";
+										foreach ($openstaand_bedrag as $key => $value) {
+											if (strpos($key, 'totaal') === false) {
+												echo "Cursus {$key}: ";
+												echo $value . " | ";
+											}
+										}
+										echo '</p>';
+										?> </ul>
 							</div> 
 						</td>
 					</tr>
