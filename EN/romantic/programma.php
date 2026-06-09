c77839f9d1bbe1fe2b01ebf3ef4bf3c4ebbcb9a4
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-06-09 15:41:55 +0200

    m

--- EN/romantic/programma.php
+++ EN/romantic/programma.php
@@ -59,92 +59,108 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/cursusdata.php'; ?>
                                         echo $navigatie;
                                         echo '<span class="w3-opennav w3-xxlarge w3-hide-large" onclick="w3_open()">☰</span>';
                                         require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.NL.php';
-                                        ?>
-        <div id="main">
+                                        ?> <div id="main">
             <div class="cols2">
                 <h2>Programme details</h2>
                 <h3>Dvořák - The Spectre's Bride</h3>
-
                 <p>
-                <div class="fotolinks w3-image"><img src="/Images/Dvorak.jpg" alt="Antonín Dvořák" width="250"><br>Antonín Dvořák</div>Antonín Dvořák's dramatic ballad <i>The Spectre's Bride</i> composed in 1883 is a work with a powerful musical impact.
-
-                The enthusiasm of English audiences for large-scale vocal works led to
-                Dvořák being commissioned in November 1883 to write a cantata for
-                soloists, chorus and orchestra for the 1885 Birmingham Musical Festival. He
-                himself conducted the first two performances on 28 and 29 March 1885 in
-                Plze&#328;. The English premiere took place on 27 August 1885, again under
-                Dvořák's direction. The new work was enthusiastically received. In the
-                many reviews the assessment of <i>The Spectre's Bride</i> was extremely
-                positive.</p>
-
-                <p>For his work, Dvořák chose the ballad <i>Svatební
-                        košile</i> (which literally translates as The Bride's Nightgown) by the Czech
-                    poet Karel Jaromír Erben (1811 - 1870). Erben's <i>Kytice</i> (Bouquet)
-                    collection of fairy tales inspired Dvořák more often, for example for his
-                    cycle of symphonic poems, <i>The Water Goblin</i>, <i>The Noon Witch</i>, <i>The
-                        Golden Spinning Wheel</i> and <i>The Wild Dove</i>. In these four poems, as in <i>The
-                        Spectre's Bride</i>, Dvořák assigns specific musical themes and even
-                    instruments for important characters and events in the drama. For example the
-                    bass clarinet, a new invention at the time, is always reserved for spooky,
-                    ghastly moments in the story.</p>
-
-                <div class="fotocenter w3-image"><img src="/Images/svatebni_kosile.png" alt="The Spectre's Bride"><br>Illustration for The Spectre's Bride</div>
-                <p>The ghostly ballad <i>Svatební košile</i> is
-                    a turmoil of emotions encompassing longing, horror, hope, doubt and, at the
-                    end, relief. The tale told is one of a wild and furious nocturnal hunt, through
-                    sludgy swamps and over bare rocks, passing by gloomy, glimmering will o' the
-                    wisps and dogs howling threateningly.
+                <div class="fotolinks w3-image"><img src="/Images/Dvorak.jpg"
+                        alt="Antonín Dvořák" width="250"><br>Antonín Dvořák
+                </div>Antonín Dvořák's dramatic ballad <i>The Spectre's
+                    Bride</i> composed in 1883 is a work with a powerful musical
+                impact. The enthusiasm of English audiences for large-scale
+                vocal works led to Dvořák being commissioned in November 1883 to
+                write a cantata for soloists, chorus and orchestra for the 1885
+                Birmingham Musical Festival. He himself conducted the first two
+                performances on 28 and 29 March 1885 in Plze&#328;. The English
+                premiere took place on 27 August 1885, again under Dvořák's
+                direction. The new work was enthusiastically received. In the
+                many reviews the assessment of <i>The Spectre's Bride</i> was
+                extremely positive.</p>
+                <p>For his work, Dvořák chose the ballad <i>Svatební košile</i>
+                    (which literally translates as The Bride's Nightgown) by the
+                    Czech poet Karel Jaromír Erben (1811 - 1870). Erben's
+                    <i>Kytice</i> (Bouquet) collection of fairy tales inspired
+                    Dvořák more often, for example for his cycle of symphonic
+                    poems, <i>The Water Goblin</i>, <i>The Noon Witch</i>,
+                    <i>The Golden Spinning Wheel</i> and <i>The Wild Dove</i>.
+                    In these four poems, as in <i>The Spectre's Bride</i>,
+                    Dvořák assigns specific musical themes and even instruments
+                    for important characters and events in the drama. For
+                    example the bass clarinet, a new invention at the time, is
+                    always reserved for spooky, ghastly moments in the story.
                 </p>
+                <div class="fotocenter w3-image"><img
+                        src="/Images/svatebni_kosile.png"
+                        alt="The Spectre's Bride"><br>Illustration for The
+                    Spectre's Bride</div>
+                <p>The ghostly ballad <i>Svatební košile</i> is a turmoil of
+                    emotions encompassing longing, horror, hope, doubt and, at
+                    the end, relief. The tale told is one of a wild and furious
+                    nocturnal hunt, through sludgy swamps and over bare rocks,
+                    passing by gloomy, glimmering will o' the wisps and dogs
+                    howling threateningly. </p>
                 <h4>Synopsis of the 'plot'</h4>
-                <p>In the opening soprano monologue the girl
-                    thinks of her beloved who left to travel abroad. She prays to the Virgin Mary
-                    that he return safely and, at the end of the scene, expresses the blasphemous
-                    thought that, if he did not return, she would rather die. Then a living corpse
-                    appears at the parlour window, pretending to be the girls beloved, and he
-                    invites her to his home. She then sets out with him on a terrible journey
-                    on a flying horse through the night landscape, during which the dead man gradually throws away
-                    the objects the girl has taken with her: a little cross, a rosary and a prayer
-                    book. The couple finally arrive at the place the corpse calls his home - a
-                    cemetery. It is only now that the girl realises her mistake and she is saved by
-                    uttering a prayer of apology to the Virgin Mary and by the appearance of the
-                    first rays of the sun announcing the new day.</p>
-
+                <p>In the opening soprano monologue the girl thinks of her
+                    beloved who left to travel abroad. She prays to the Virgin
+                    Mary that he return safely and, at the end of the scene,
+                    expresses the blasphemous thought that, if he did not
+                    return, she would rather die. Then a living corpse appears
+                    at the parlour window, pretending to be the girls beloved,
+                    and he invites her to his home. She then sets out with him
+                    on a terrible journey on a flying horse through the night
+                    landscape, during which the dead man gradually throws away
+                    the objects the girl has taken with her: a little cross, a
+                    rosary and a prayer book. The couple finally arrive at the
+                    place the corpse calls his home - a cemetery. It is only now
+                    that the girl realises her mistake and she is saved by
+                    uttering a prayer of apology to the Virgin Mary and by the
+                    appearance of the first rays of the sun announcing the new
+                    day.</p>
+                <p><a href="Text_SB.php" target="_blank">The full Czech text
+                        with English translation can be found here.</a></p>
                 <h4>Structure of the composition</h4>
-                <p>The form of the cantata was determined by
-                    the nature of the text: the dialogue between the girl and the corpse is sung by
-                    solo soprano and tenor; the role of the narrator is entrusted to the solo
-                    baritone and mixed choir. The extensive work precisely follows the sequence of
-                    scenes as they appear in the ballad.</p>
-
-                <p>The ballad is divided into 18 numbers
-                    (mainly linked in the composition through attacca transitions) which are
-                    grouped according to the various scenarios in the story into three parts
-                    (Introduction up to no. 4, nos. 5-12 and nos. 13-18). Diverging from the
-                    literary model, which envisaged a single narrator to recite the whole text,
-                    Dvořák assigned most of the text passages in which the protagonists
-                    'speak' to the solo roles of the girl (soprano) and the dead bridegroom
-                    (tenor). In addition, the function of the narrator is taken jointly by a solo
-                    part (bass or baritone) and the chorus. The orchestra also has a very important
-                    role. It conveys the epic-dramatic expression of the musical progress,
-                    complementing the vocal parts, and is the bearer of the musical action. Only in
-                    two numbers does a lyrical style come to the fore, both times in the two arias
-                    of the girl (no. 2 and no. 17). These are conceived as prayer scenes and mark
-                    inner turning points which slow down the forward movement, at the same time
-                    eliciting decisive transformations.</p>
-
-                <p>Erben's poem has a powerful plot with
-                    numerous dramatic situations which offer all kinds of opportunities for a
-                    highly contrastive musical depiction of the individual scenes and characters. Dvořák
-                    makes use of this, employing his inexhaustible source of imagery without losing
-                    sight of the requirement for overall structural unity and conciseness. He found
-                    great inspiration in the stirring rhythms of Erben's verse, whose impact he was
-                    able to reinforce through his music. The work retains its homogeneity throughout
-                    thanks to the motif of descending fifths which winds its way through the
-                    individual sections. Dvořák's musical setting demonstrates a clear
-                    understanding of the text and a flawless synthesis of music and verse. The
-                    cantata is typical for its dramatic expression, inventive melodies and unusual
-                    rhythms, all told, a supreme example of the composer's creative genius.</p>
-
+                <p>The form of the cantata was determined by the nature of the
+                    text: the dialogue between the girl and the corpse is sung
+                    by solo soprano and tenor; the role of the narrator is
+                    entrusted to the solo baritone and mixed choir. The
+                    extensive work precisely follows the sequence of scenes as
+                    they appear in the ballad.</p>
+                <p>The ballad is divided into 18 numbers (mainly linked in the
+                    composition through attacca transitions) which are grouped
+                    according to the various scenarios in the story into three
+                    parts (Introduction up to no. 4, nos. 5-12 and nos. 13-18).
+                    Diverging from the literary model, which envisaged a single
+                    narrator to recite the whole text, Dvořák assigned most of
+                    the text passages in which the protagonists 'speak' to the
+                    solo roles of the girl (soprano) and the dead bridegroom
+                    (tenor). In addition, the function of the narrator is taken
+                    jointly by a solo part (bass or baritone) and the chorus.
+                    The orchestra also has a very important role. It conveys the
+                    epic-dramatic expression of the musical progress,
+                    complementing the vocal parts, and is the bearer of the
+                    musical action. Only in two numbers does a lyrical style
+                    come to the fore, both times in the two arias of the girl
+                    (no. 2 and no. 17). These are conceived as prayer scenes and
+                    mark inner turning points which slow down the forward
+                    movement, at the same time eliciting decisive
+                    transformations.</p>
+                <p>Erben's poem has a powerful plot with numerous dramatic
+                    situations which offer all kinds of opportunities for a
+                    highly contrastive musical depiction of the individual
+                    scenes and characters. Dvořák makes use of this, employing
+                    his inexhaustible source of imagery without losing sight of
+                    the requirement for overall structural unity and
+                    conciseness. He found great inspiration in the stirring
+                    rhythms of Erben's verse, whose impact he was able to
+                    reinforce through his music. The work retains its
+                    homogeneity throughout thanks to the motif of descending
+                    fifths which winds its way through the individual sections.
+                    Dvořák's musical setting demonstrates a clear understanding
+                    of the text and a flawless synthesis of music and verse. The
+                    cantata is typical for its dramatic expression, inventive
+                    melodies and unusual rhythms, all told, a supreme example of
+                    the composer's creative genius.</p>
                 <h2><a href="javascript: history.go(-1)">Back</a></h2>
             </div>
         </div>
