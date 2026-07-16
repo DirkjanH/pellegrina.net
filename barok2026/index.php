7abf5bcfaa9a77ec9100dd92b4dbff35d367e99c
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-07-16 16:45:16 +0200

    Update index.php: enhance HTML structure and improve meta tags for better SEO

--- barok2026/index.php
+++ barok2026/index.php
@@ -4,73 +4,82 @@
 ?>
 <!DOCTYPE HTML>
 <html lang=NL>
-
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf8mb4_ubicode_ci">
-  <title>La Pellegrina - Concert Purcell & Handel</title>
-  <link rel="apple-touch-icon" sizes="180x180" href="https://pellegrina.net/Images/Logos/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="https://pellegrina.net/Images/Logos/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="https://pellegrina.net/Images/Logos/favicon-16x16.png">
-  <link rel="shortcut icon" href="https://pellegrina.net/Images/Logos/favicon.ico" type="image/png">
-  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-  <meta name='keywords' content='Kaartreservering concert Purcell & Handel,La Pellegrina,barokcursus'>
-  <meta name='description' content='Kaartreserveringspagian voor concert Purcell & Handel'>
-  <meta property="og:type" content='website'>
-  <meta property="og:title" content='La Pellegrina - Concert Purcell & Handel'>
-  <meta property="og:description" content='Kaartreserveringspagian voor concert Purcell & Handel'>
-  <meta property="og:image" content='https://pellegrina.net/Images/Logos/favicon-32x32.png'>
-  <!--[if lt IE 9]>
+    <meta http-equiv="Content-Type"
+        content="text/html; charset=utf8mb4_ubicode_ci">
+    <title>La Pellegrina - Concert Barokmuziek uit Centraal Europa - woensdag 19
+        augustus</title>
+    <link rel="apple-touch-icon" sizes="180x180"
+        href="https://pellegrina.net/Images/Logos/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32"
+        href="https://pellegrina.net/Images/Logos/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16"
+        href="https://pellegrina.net/Images/Logos/favicon-16x16.png">
+    <link rel="shortcut icon"
+        href="https://pellegrina.net/Images/Logos/favicon.ico" type="image/png">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta name='keywords'
+        content='Kaartreservering concert Purcell & Handel,La Pellegrina,barokcursus'>
+    <meta name='description'
+        content='Kaartreserveringspagian voor concert Purcell & Handel'>
+    <meta property="og:type" content='website'>
+    <meta property="og:title"
+        content='La Pellegrina - Concert Purcell & Handel'>
+    <meta property="og:description"
+        content='Kaartreserveringspagian voor concert Purcell & Handel'>
+    <meta property="og:image"
+        content='https://pellegrina.net/Images/Logos/favicon-32x32.png'>
+    <!--[if lt IE 9]>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.js"></script>
 <![endif]-->
-  <!--[if lt IE 9]>
+    <!--[if lt IE 9]>
 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js">
 </script>
 <![endif]-->
-  <!-- Facebook Pixel Code -->
-  <script>
+    <!-- Facebook Pixel Code -->
+    <script>
     ! function(f, b, e, v, n, t, s) {
-      if (f.fbq) return;
-      n = f.fbq = function() {
-        n.callMethod ?
-          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-      };
-      if (!f._fbq) f._fbq = n;
-      n.push = n;
-      n.loaded = !0;
-      n.version = '2.0';
-      n.queue = [];
-      t = b.createElement(e);
-      t.async = !0;
-      t.src = v;
-      s = b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t, s)
+        if (f.fbq) return;
+        n = f.fbq = function() {
+            n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(
+                arguments)
+        };
+        if (!f._fbq) f._fbq = n;
+        n.push = n;
+        n.loaded = !0;
+        n.version = '2.0';
+        n.queue = [];
+        t = b.createElement(e);
+        t.async = !0;
+        t.src = v;
+        s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s)
     }(window, document, 'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
+        'https://connect.facebook.net/en_US/fbevents.js');
     fbq('init', '2292426324112331');
     fbq('track', 'PageView');
-  </script>
-  <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2292426324112331&ev=PageView&noscript=1" /></noscript>
-  <!-- End Facebook Pixel Code -->
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=2292426324112331&ev=PageView&noscript=1" /></noscript>
+    <!-- End Facebook Pixel Code -->
 </head>
-
 <body>
-  <style>
+    <style>
     body {
-      margin: 0;
-      /* Reset default margin */
+        margin: 0;
+        /* Reset default margin */
     }
 
     iframe {
-      display: block;
-      /* iframes are inline by default */
-      border: none;
-      /* Reset default border */
-      height: 100vh;
-      /* Viewport-relative units */
-      width: 100vw;
+        display: block;
+        /* iframes are inline by default */
+        border: none;
+        /* Reset default border */
+        height: 100vh;
+        /* Viewport-relative units */
+        width: 100vw;
     }
-  </style>
-  <iframe src="https://barok.ingoedehanden.management"></iframe>
+    </style>
+    <iframe src="https://barok.ingoedehanden.management"></iframe>
 </body>
-
 </html>
