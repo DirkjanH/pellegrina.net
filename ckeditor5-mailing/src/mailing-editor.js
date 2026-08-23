dac04a8dda12565db39bfde2b7f82bd3523c9739
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-08-23 11:34:56 +0200

    veel veranderingen

---
+++ ckeditor5-mailing/src/mailing-editor.js
@@ -0,0 +1,161 @@
+import { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic"
+import { Alignment } from "@ckeditor/ckeditor5-alignment"
+import { Autoformat } from "@ckeditor/ckeditor5-autoformat"
+import {
+	Bold,
+	Italic,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	Underline,
+} from "@ckeditor/ckeditor5-basic-styles"
+import { BlockQuote } from "@ckeditor/ckeditor5-block-quote"
+import { Code, CodeBlock } from "@ckeditor/ckeditor5-code-block"
+import { Essentials } from "@ckeditor/ckeditor5-essentials"
+import { FindAndReplace } from "@ckeditor/ckeditor5-find-and-replace"
+import {
+	Font,
+	FontBackgroundColor,
+	FontColor,
+	FontFamily,
+	FontSize,
+} from "@ckeditor/ckeditor5-font"
+import { Heading } from "@ckeditor/ckeditor5-heading"
+import { Highlight } from "@ckeditor/ckeditor5-highlight"
+import { HorizontalLine } from "@ckeditor/ckeditor5-horizontal-line"
+import { HtmlEmbed } from "@ckeditor/ckeditor5-html-embed"
+import {
+	Image,
+	ImageCaption,
+	ImageInsert,
+	ImageStyle,
+	ImageToolbar,
+} from "@ckeditor/ckeditor5-image"
+import { Indent } from "@ckeditor/ckeditor5-indent"
+import { Link } from "@ckeditor/ckeditor5-link"
+import { List } from "@ckeditor/ckeditor5-list"
+import { MediaEmbed } from "@ckeditor/ckeditor5-media-embed"
+import { PageBreak } from "@ckeditor/ckeditor5-page-break"
+import { Paragraph } from "@ckeditor/ckeditor5-paragraph"
+import { PasteFromOffice } from "@ckeditor/ckeditor5-paste-from-office"
+import { RemoveFormat } from "@ckeditor/ckeditor5-remove-format"
+import { SelectAll } from "@ckeditor/ckeditor5-select-all"
+import { SourceEditing } from "@ckeditor/ckeditor5-source-editing"
+import {
+	SpecialCharacters,
+	SpecialCharactersEssentials,
+} from "@ckeditor/ckeditor5-special-characters"
+import { Style } from "@ckeditor/ckeditor5-style"
+import { Table, TableToolbar } from "@ckeditor/ckeditor5-table"
+import { Undo } from "@ckeditor/ckeditor5-undo"
+import "@ckeditor/ckeditor5-theme-lark/theme/theme.css"
+
+class MailingEditor extends ClassicEditor {}
+
+MailingEditor.builtinPlugins = [
+	Alignment,
+	Autoformat,
+	Bold,
+	BlockQuote,
+	Code,
+	CodeBlock,
+	Essentials,
+	FindAndReplace,
+	Font,
+	FontBackgroundColor,
+	FontColor,
+	FontFamily,
+	FontSize,
+	Heading,
+	Highlight,
+	HorizontalLine,
+	HtmlEmbed,
+	Image,
+	ImageCaption,
+	ImageInsert,
+	ImageStyle,
+	ImageToolbar,
+	Indent,
+	Italic,
+	Link,
+	List,
+	MediaEmbed,
+	PageBreak,
+	Paragraph,
+	PasteFromOffice,
+	RemoveFormat,
+	SelectAll,
+	SourceEditing,
+	SpecialCharacters,
+	SpecialCharactersEssentials,
+	Strikethrough,
+	Style,
+	Subscript,
+	Superscript,
+	Table,
+	TableToolbar,
+	Underline,
+	Undo,
+]
+
+MailingEditor.defaultConfig = {
+	language: "nl",
+	toolbar: {
+		items: [
+			"undo",
+			"redo",
+			"|",
+			"sourceEditing",
+			"findAndReplace",
+			"selectAll",
+			"|",
+			"heading",
+			"style",
+			"fontSize",
+			"fontFamily",
+			"fontColor",
+			"fontBackgroundColor",
+			"highlight",
+			"|",
+			"bold",
+			"italic",
+			"underline",
+			"strikethrough",
+			"subscript",
+			"superscript",
+			"removeFormat",
+			"|",
+			"alignment",
+			"bulletedList",
+			"numberedList",
+			"outdent",
+			"indent",
+			"link",
+			"blockQuote",
+			"insertTable",
+			"insertImage",
+			"mediaEmbed",
+			"code",
+			"codeBlock",
+			"htmlEmbed",
+			"horizontalLine",
+			"pageBreak",
+			"specialCharacters",
+		],
+		shouldNotGroupWhenFull: true,
+	},
+	table: {
+		contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+	},
+	image: {
+		toolbar: ["imageTextAlternative", "imageStyle:inline", "imageStyle:block"],
+	},
+	style: {
+		definitions: [
+			{ name: "Info box", element: "p", classes: ["info-box"] },
+			{ name: "Small text", element: "p", classes: ["small-text"] },
+		],
+	},
+}
+
+export default MailingEditor
---
+++ ckeditor5-mailing/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path")
+const { styles } = require("@ckeditor/ckeditor5-dev-utils")
+
+module.exports = {
+	mode: "production",
+	entry: "./src/mailing-editor.js",
+	output: {
+		path: path.resolve(__dirname, "../mailing"),
+		filename: "mailing-editor.js",
+		library: "MailingEditor",
+		libraryExport: "default",
+		libraryTarget: "window",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					"style-loader",
+					"css-loader",
+					{
+						loader: "postcss-loader",
+						options: {
+							postcssOptions: styles.getPostCssConfig({
+								themeImporter: {
+									themePath:
+										require.resolve("@ckeditor/ckeditor5-theme-lark/theme/theme.css"),
+								},
+								minify: true,
+							}),
+						},
+					},
+				],
+			},
+		],
+	},
+	optimization: {
+		minimizer: [],
+	},
+	resolve: {
+		extensions: [".js"],
+	},
+}
