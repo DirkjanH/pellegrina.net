99c715cb182b9e652922597ea2b208368264a1e8
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-09-02 12:19:11 +0200

    Refactor error reporting and PDO initialization for improved readability and consistency

--- connections/connect_PDO.php
+++ connections/connect_PDO.php
@@ -1,25 +1,24 @@
 <?php
-error_reporting( E_ALL ); 
+error_reporting(E_ALL);
 
-	$HOSTNAME = 'localhost';
-	$DATABASE = 'pellegrina_db';
-	$USERNAME = 'pellegrina_lp';
-	$PASSWORD = '12dirig.';
+$HOSTNAME = 'localhost';
+$DATABASE = 'pellegrina_db';
+$USERNAME = 'pellegrina_lp';
+$PASSWORD = '12dirig.';
 
 // echo "mysql:dbname=$DATABASE";
 
 try {
-    $db = new PDO("mysql:host=localhost;dbname=$DATABASE;charset=utf8", $USERNAME, $PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    $initCommandAttribute = defined('Pdo\\Mysql::ATTR_INIT_COMMAND')
+        ? constant('Pdo\\Mysql::ATTR_INIT_COMMAND')
+        : PDO::MYSQL_ATTR_INIT_COMMAND;
+    $db = new PDO("mysql:host=localhost;dbname=$DATABASE;charset=utf8", $USERNAME, $PASSWORD, array($initCommandAttribute => "SET NAMES 'utf8'"));
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} 
-catch(PDOException $e) 
-{ 
+} catch (PDOException $e) {
     $sMsg = '<p> 
-            Regelnummer: '.$e->getLine().'<br /> 
-            Bestand: '.$e->getFile().'<br /> 
-            Foutmelding: '.$e->getMessage().'</p>'; 
-     
-    trigger_error($sMsg); 
-} 
+            Regelnummer: ' . $e->getLine() . '<br /> 
+            Bestand: ' . $e->getFile() . '<br /> 
+            Foutmelding: ' . $e->getMessage() . '</p>';
 
-?>
+    trigger_error($sMsg);
+}
