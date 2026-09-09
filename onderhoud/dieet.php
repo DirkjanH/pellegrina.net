f82d8815626022515134f173ee526cac8e4cb6ae
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-09-09 13:23:02 +0200

    Refactor error handling and improve HTML structure in dieet.php for better readability and consistency; sanitize output for security

--- onderhoud/dieet.php
+++ onderhoud/dieet.php
@@ -1,10 +1,9 @@
 <?php
-// stel php in dat deze fouten weergeeft
-ini_set('display_errors', 1);
-
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
 error_reporting(E_ALL);
 
-require_once $_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 Kint::$enabled_mode = false;
 
@@ -20,7 +19,8 @@ Kint::$enabled_mode = false;
 	<head>
 		<title>Dieet/Dieta/Régime</title>
 		<meta charset="utf-8">
-		<link rel="stylesheet" href="../css/pellegrina_stijlen.css" type="text/css">
+		<link rel="stylesheet" href="../css/pellegrina_stijlen.css"
+			type="text/css">
 		<style type="text/css">
 			body,
 			td,
@@ -31,52 +31,52 @@ Kint::$enabled_mode = false;
 	</head>
 
 <body>
-	<div class="inhoud">
-
-		<?php
-		//Connection statement
-		require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/includes2027.php');
-		/* echo '<pre>';
+	<div class="inhoud"> <?php
+							//Connection statement
+							require_once dirname(__DIR__) . '/includes/includes2027.php';
+							/* echo '<pre>';
 print_r($_GET);
 echo '</pre>';
  */
-		$CursusId = $eerstecursus;
+							$CursusId = (int) $eerstecursus;
 
-		while ($CursusId <= $laatstecursus) {
+							while ($CursusId <= $laatstecursus) {
 
-			// begin Recordset
-			$query_passagiers = "SELECT d.naam, d.dieet FROM dlnmr d, inschrijving i 
+								// begin Recordset
+								$query_passagiers = "SELECT d.naam, d.dieet FROM dlnmr d, inschrijving i 
 	WHERE NOT (d.naam LIKE \"%XXX%\" OR d.naam LIKE \"%YYY%\" OR d.naam LIKE \"%ZZZ%\") AND i.CursusId_FK = {$CursusId} 
 	AND d.dlnmrid = i.dlnmrid_fk AND NOT (afgewezen <=> 1) AND NOT(dieet IS NULL OR dieet LIKE \"%[geen]%\" 
 	OR dieet LIKE \"%none%\") ORDER BY d.achternaam ASC";
 
-			$passagiers = select_query($query_passagiers);
-			$totalRows_passagiers = count($passagiers);
+								$passagiers = select_query($query_passagiers) ?: [];
+								$totalRows_passagiers = count($passagiers);
 
-			$query_Cursusnaam = "SELECT cursusnaam_en, YEAR(datum_begin) as jaar FROM cursus WHERE CursusId = {$CursusId}";
-			$Cursusnaam = select_query($query_Cursusnaam, 1);
-		?>
-			<h2>Course "<?php echo $Cursusnaam['cursusnaam_en']; ?>"</h2>
-			<p>&nbsp;</p>
-			<table width="600" border="1" cellpadding="5">
-				<tr>
-					<td width="40%"><strong><em>Naam/Jmeno/Nom:</em></strong></td>
-					<td width="40%"><strong><em>Dieet/Dieta/Régime:</em></strong></td>
-				</tr>
-				<?php
-				foreach ($passagiers as $passagier) {
-				?>
+								$query_Cursusnaam = "SELECT cursusnaam_en, YEAR(datum_begin) as jaar FROM cursus WHERE CursusId = {$CursusId}";
+								$Cursusnaam = select_query($query_Cursusnaam, 1);
+							?> <h2>Course
+				"<?php echo htmlspecialchars($Cursusnaam['cursusnaam_en'] ?? '', ENT_QUOTES, 'UTF-8'); ?>"
+			</h2>
+			<p>&nbsp;</p> <?php if ($totalRows_passagiers === 0) { ?> <p>No
+					participants with dietary requirements were found for this course.
+				</p> <?php } else { ?> <table width="600" border="1" cellpadding="5">
 					<tr>
-						<td width="40%"><?php echo $passagier['naam']; ?>&nbsp;</td>
-						<td width="40%"><?php echo $passagier['dieet']; ?>&nbsp;</td>
-					</tr>
-				<?php	  } ?>
-			</table>
-			<p>&nbsp;</p>
-		<?php
-			$CursusId++;
-		}
-		?>
+						<td width="40%"><strong><em>Naam/Jmeno/Nom:</em></strong></td>
+						<td width="40%"><strong><em>Dieet/Dieta/Régime:</em></strong>
+						</td>
+					</tr> <?php
+									foreach ($passagiers as $passagier) {
+							?> <tr>
+							<td width="40%">
+								<?php echo htmlspecialchars($passagier['naam'] ?? '', ENT_QUOTES, 'UTF-8'); ?>&nbsp;
+							</td>
+							<td width="40%">
+								<?php echo htmlspecialchars($passagier['dieet'] ?? '', ENT_QUOTES, 'UTF-8'); ?>&nbsp;
+							</td>
+						</tr> <?php	  } ?>
+				</table> <?php } ?> <p>&nbsp;</p> <?php
+													$CursusId++;
+												}
+													?>
 	</div>
 </body>
 
