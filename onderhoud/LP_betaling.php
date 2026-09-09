0ac7da270bfd4f38c6eac159fddc5793d653f0b4
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-09-09 15:17:26 +0200

    Refactor LP_betaling.php for improved HTML structure and readability; streamline input field formatting

--- onderhoud/LP_betaling.php
+++ onderhoud/LP_betaling.php
@@ -280,15 +280,13 @@ $openstaand_giraal = euro2($openstaand_bedrag['totaal'] - $openstaand_cashbedrag
                                 <?php if ($ins['CursusId_FK'] != "") echo "<p>Inschrijving nr. 
 			<input name=\"Id\" type=\"text\" DISABLED value=\"{$ins['InschId']}\"
 			size=\"2\">&nbsp;voor cursus:&nbsp;<b>{$cursusnaam[$ins['CursusId_FK']]['NL']}</b></p>"; ?> <input name="aanbet_bedrag"
-                                    type="hidden"
-                                    value="<?php
-                                            echo $ins['aanbet_bedrag']; ?>">
+                                    type="hidden" value="<?php
+                                                            echo $ins['aanbet_bedrag']; ?>">
                                 <input name="InschId" id="InschId" type="hidden"
                                     value="<?php
                                             echo $ins['InschId']; ?>">
-                                <input name="CursusId_FK" type="hidden"
-                                    value="<?php
-                                            echo $ins['CursusId_FK']; ?>">
+                                <input name="CursusId_FK" type="hidden" value="<?php
+                                                                                echo $ins['CursusId_FK']; ?>">
                             </td>
                         </tr>
                         <tr valign="baseline">
