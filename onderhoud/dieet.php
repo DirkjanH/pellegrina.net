2e450e5f95d1455cb61ab1ab3ed40b6077568df5
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-09-09 14:40:47 +0200

    Refactor dieet.php for improved HTML structure and styling; enhance table presentation for dietary requirements

--- onderhoud/dieet.php
+++ onderhoud/dieet.php
@@ -27,17 +27,18 @@ function dieet_select_query($query, $single = false)
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="utf-8">
 	<title>Dieet/Dieta/Régime</title>
+	<link rel="stylesheet" href="../css/w3.css" type="text/css">
 	<link rel="stylesheet" href="../css/pellegrina_stijlen.css" type="text/css">
 </head>
 
 <body>
-	<div class="inhoud"> <?php
-							$CursusId = (int) $eerstecursus;
+	<div id="inhoud" class="w3-auto w3-main"> <?php
+												$CursusId = (int) $eerstecursus;
 
-							while ($CursusId <= $laatstecursus) {
+												while ($CursusId <= $laatstecursus) {
 
-								// begin Recordset
-								$query_passagiers = "SELECT d.naam, d.dieet
+													// begin Recordset
+													$query_passagiers = "SELECT d.naam, d.dieet
 	FROM dlnmr AS d
 	INNER JOIN inschrijving AS i ON d.dlnmrid = i.dlnmrid_fk
 	WHERE NOT (d.naam LIKE \"%XXX%\" OR d.naam LIKE \"%YYY%\" OR d.naam LIKE \"%ZZZ%\")
@@ -46,35 +47,37 @@ function dieet_select_query($query, $single = false)
 	AND NOT (d.dieet IS NULL OR d.dieet LIKE \"%[geen]%\" OR d.dieet LIKE \"%none%\")
 	ORDER BY d.achternaam ASC";
 
-								$passagiers = dieet_select_query($query_passagiers);
-								$totalRows_passagiers = count($passagiers);
+													$passagiers = dieet_select_query($query_passagiers);
+													$totalRows_passagiers = count($passagiers);
 
-								$query_Cursusnaam = "SELECT cursusnaam_en, YEAR(datum_begin) as jaar FROM cursus WHERE CursusId = {$CursusId}";
-								$Cursusnaam = dieet_select_query($query_Cursusnaam, true);
-							?> <h2>Course
+													$query_Cursusnaam = "SELECT cursusnaam_en, YEAR(datum_begin) as jaar FROM cursus WHERE CursusId = {$CursusId}";
+													$Cursusnaam = dieet_select_query($query_Cursusnaam, true);
+												?> <h2>Course
 				"<?php echo htmlspecialchars($Cursusnaam['cursusnaam_en'] ?? '', ENT_QUOTES, 'UTF-8'); ?>"
 			</h2>
 			<p>&nbsp;</p> <?php if ($totalRows_passagiers === 0) { ?> <p>No
 					participants with dietary requirements were found for this course.
-				</p> <?php } else { ?> <table width="600" border="1" cellpadding="5">
-					<tr>
-						<td width="40%"><strong><em>Naam/Jmeno/Nom:</em></strong></td>
-						<td width="40%"><strong><em>Dieet/Dieta/Régime:</em></strong>
-						</td>
-					</tr> <?php
-									foreach ($passagiers as $passagier) {
-							?> <tr>
-							<td width="40%">
-								<?php echo htmlspecialchars($passagier['naam'] ?? '', ENT_QUOTES, 'UTF-8'); ?>&nbsp;
-							</td>
-							<td width="40%">
-								<?php echo htmlspecialchars($passagier['dieet'] ?? '', ENT_QUOTES, 'UTF-8'); ?>&nbsp;
-							</td>
-						</tr> <?php	  } ?>
-				</table> <?php } ?> <p>&nbsp;</p> <?php
+				</p> <?php } else { ?> <div class="w3-responsive">
+					<table class="w3-table-all w3-striped">
+						<thead>
+							<tr>
+								<th scope="col">Naam/Jmeno/Nom:</th>
+								<th scope="col">Dieet/Dieta/Régime:</th>
+							</tr>
+						</thead>
+						<tbody> <?php
+														foreach ($passagiers as $passagier) {
+								?> <tr>
+									<td> <?php echo htmlspecialchars($passagier['naam'] ?? '', ENT_QUOTES, 'UTF-8'); ?>&nbsp;
+									</td>
+									<td> <?php echo htmlspecialchars($passagier['dieet'] ?? '', ENT_QUOTES, 'UTF-8'); ?>&nbsp;
+									</td>
+								</tr> <?php	  } ?> </tbody>
+					</table>
+				</div> <?php } ?> <p>&nbsp;</p> <?php
 													$CursusId++;
 												}
-													?>
+												?>
 	</div>
 </body>
 
