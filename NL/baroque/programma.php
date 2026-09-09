8fd259aaf1eb39fe43e3fb6c9fafea2d68775e24
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-09-09 11:47:51 +0200

    Update program details in programma.php to highlight Baroque music from Naples

--- NL/baroque/programma.php
+++ NL/baroque/programma.php
@@ -63,28 +63,42 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/cursusdata.php'; ?>
                                     echo '<span class="w3-opennav w3-xxlarge w3-hide-large" onclick="w3_open()">☰</span>';
                                     require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.NL.php';
                                     ?> <div id="main">
-      <div class="cols2">
-        <h2>Programmadetails</h2>
-        <p>Centraal in de zomercursus staat barokmuziek in Centraal-Europa (ca. 1600 – 1750). Het gebied van Oost-Duitsland, Oostenrijk, Tsjechië en Polen speelde een centrale rol in de ontwikkeling van westerse klassieke muziek, waarbij expressie, virtuositeit en nieuwe vormen centraal stonden.</p>
-        <h4>Historische en sociale context</h4>
-        <p>De barokperiode viel samen met een tijd van politieke en religieuze spanningen, zoals de Dertigjarige Oorlog en de opkomst van absolute vorsten als de Habsburgers in Wenen en de Hohenzollerns in Brandenburg. Veel muziek werd in dienst van hof en kerk geschreven, waar rijke vorsten en bisschoppen componisten en musici aan zich verbonden. Tegelijkertijd groeide het openbare concertleven door de toenemende welvaart van de burgerij, waardoor muziek ook buiten kerken en hoven tot bloei kwam.</p>
-        <h4>Belangrijke componisten</h4>
-        <p>Centraal-Europese componisten als Johann Sebastian Bach (Duitsland), Heinrich Schütz, Georg Philipp Telemann, en voor Oostenrijk Johann Fux en Jan Dismas Zelenka (uit het toenmalige Bohemen, nu Tsjechië) drukten een stempel op de barokmuziek. Zij ontwikkelden diverse instrumentale en vocale stijlen die de verdere geschiedenis van de westerse muziek sterk bepaalden. Barokmuziek uit Centraal-Europa weerspiegelt zo de zoektocht naar expressie, pracht en innovatie die kenmerkend was voor deze bloeiperiode in de Europese muziekgeschiedenis.</p>
-        <p><div class="fotolinks w3-center"><img src="/Images/Biber.png" alt="Heinrich Ignaz Franz Biber" class="w3-image" width="250"><br>Heinrich Ignaz Franz Biber</div>Een typisch voorbeeld van een Centraal-Europese componist is Heinrich Ignaz Franz Biber (1644 - 1704). Biber werd geboren in Stráž pod Ralskem (toen Wartenberg), Bohemen. Hij genoot zijn basisopleiding aan het jezuïetencollege in Opava (Duits: Troppau), Moravië, waar hij onder andere kennis maakte met Pavel Josef Vejvanovský en Philipp Jacob Rittler. Hij studeerde waarschijnlijk in Praag, Dresden en Wenen bij onder andere de vioolvirtuoos Johann Heinrich Schmelzer. In het begin van de jaren 1660 was hij als musicus in dienst bij prins Johann Seyfried Eggenberg in Graz in de Oostenrijkse provincie Stiermarken (Steiermark). Later, tot 1670, was hij kapelmeester van de beroemde kapel van de prins-aartsbisschop van Olomouc Karl von Lichtenstein-Kastelkorn, die optrad in de zomerresidentie van de bisschop, het kasteel Kroměříž in Moravië. Daarna was hij violist en componist, dirigent en rentmeester aan het hof van de aartsbisschop van Salzburg, Maximilian Gandolph von Khuenburg. We zullen zijn Requiem in F mineur uitvoeren.</p>
-        <div style="display:inline-block;">
-          <h4 style="margin-top: 0;">Muzikale kenmerken</h4>
-          <p>De overgang van renaissance naar barok bracht een verschuiving van polyfonie naar een stijl waarin een duidelijke melodielijn boven een begeleidende bas (basso continuo) centraal stond. Barokmuziek kenmerkt zich door uitbundige versieringen, affectenleer (het uitdrukken van emoties), harmonisch contrapunt en de ontwikkeling van vormen als het concerto grosso en de suite. Instrumenten als het clavecimbel en de viool groeiden uit tot belangrijke instrumenten, en harmonie werd essentieel voor de opbouw van stukken. Kerkmuziek bleef belangrijk, maar kreeg concurrentie van seculiere genres als kamermuziek, dansvormen en de eerste openbare concerten. De opkomst van de opera als genre, met name in Wenen en later ook in Praag en Dresden, getuigt van de theatrale kant van Centraal-Europese barokmuziek.</p>
-        </div>
-        <div style="display:inline-block;">
-          <h4 style="margin-top: 0;">De rol van het archief in Kroměříž</h4>
-          <div class="fotocenter w3-center"><img src="/Images/kromeriz.jpg" alt="Kroměříž" class="w3-image"><br>Impressie van het barokke Kroměříž</div>
-          <p>Het muziekarchief van het kasteel van Kroměříž in Moravië (Tsjechië) is een van de rijkste collecties barokke kerkmuziek van Centraal-Europa. Dit archief, opgebouwd door bisschoppen als Karl von Liechtenstein-Castelcorn in de 17e eeuw, bevat duizenden manuscripten en drukken, waaronder werken van internationale en lokale componisten, zowel religieus als instrumentaal repertoire. Kroměříž fungeerde als een belangrijk cultureel centrum waar componisten uit heel Europa, waaronder Italiaanse en Duitse meesters, werkzaam waren of hun werken lieten uitvoeren. Het archief biedt inzicht in de hoge kwaliteit, de internationale stijlvermenging en de maatschappelijke rol van kerkmuziek in de barok, zowel tijdens de liturgie als voor concertante uitvoering aan het hof.​</p>
-          <h4>Invloed en Erfgoed</h4>
-          <p>Dankzij het archief van Kroměříž is veel kerkmuzikaal erfgoed uit Centraal-Europa bewaard gebleven, dat anders wellicht verloren was gegaan. Dit maakt het mogelijk repertoire van componisten als Johann Heinrich Schmelzer, Pavel Josef Vejvanovský, Heinrich Biber en Giovanni Valentini weer tot leven te wekken in hedendaagse uitvoeringen. Het archief staat symbool voor de uitwisseling van stijlen en invloeden binnen Centraal-Europa en toont hoe kerkmuziek bijdroeg aan de culturele identiteit van de regio.</p>
-        </div>
+      <div class="cols2"> Hier is de Nederlandse vertaling met behoud van
+        de HTML-structuur: ```html <h2>Barokmuziek uit Napels
+          (1650–1750)</h2>
+        <p>Het programma voor alle deelnemers aan de zomerschool staat
+          in het teken van barokmuziek uit Napels. Gedurende de
+          zeventiende en achttiende eeuw was Napels het kloppende hart
+          van het Europese muzikale leven. Aangedreven door de
+          prestigieuze conservatoria en bruisende operahuizen trok de
+          "Napolitaanse School" musici uit het hele continent aan.
+          Deze zomer duiken we in het rijke, expressieve en virtuoze
+          repertoire dat ontstond aan de Golf van Napels, waarbij we
+          zowel meeslepende religieuze orkestwerken als intieme
+          kamermuziek verkennen. </p>
+        <p>Tijdens de dagelijkse middagsessies bundelen zangers en
+          instrumentalisten hun krachten voor het uitvoeren van
+          meeslepende religieuze meesterwerken en dramatische
+          vocaal-instrumentale stukken uit de Napolitaanse laatbarok.
+          Ons centrale repertoire omvat onder meer:</p>
+        <ul>
+          <li><strong>Giovanni Battista Pergolesi:</strong>
+            geselecteerde orkestdelen — een illustratie van de
+            doorleefde, lyrische en intens dramatische stijl die
+            Pergolesi door heel Europa legendarisch maakte.</li>
+          <li><strong>Alessandro Scarlatti:</strong> representatieve
+            geestelijke werken en koren — met de nadruk op het
+            ingenieuze contrapunt en de vocale meesterschap van de
+            aartsvader van de Napolitaanse School.</li>
+          <li><strong>Francesco Durante & Leonardo Leo:</strong>
+            geselecteerde geestelijke concerten en dubbelkorige
+            psalmzettingen — met een rijke strijkerspartij,
+            expressieve solo's en bruisende koorfuga's.</li>
+        </ul>
       </div>
-      <h2><a href="javascript: history.go(-1)">Terug</a></h2>
     </div>
+    <h2><a href="javascript: history.go(-1)">Terug</a></h2>
+  </div>
   </div>
 </body>
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
