18600be8750bdb4e2d0f3646d9c21c017e552dcf
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-09-09 12:05:15 +0200

    Refactor HTML structure and improve formatting in index.php for Baroque section

--- EN/baroque/index.php
+++ EN/baroque/index.php
@@ -17,17 +17,15 @@ $taal = $filenaam[5];
 // echo 'taal is: '.$taal.'<br>';
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/cursusdata.php'; ?>
-
 <!DOCTYPE HTML>
-<html><!-- InstanceBegin template="/Templates/LP_EN.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<html>
+<!-- InstanceBegin template="/Templates/LP_EN.dwt.php" codeOutsideHTMLIsLocked="false" -->
 
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta charset="utf-8">
-
   <!-- CSS: -->
   <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
-
   <!-- InstanceBeginEditable name="doctitle" -->
   <title><?php echo $cursusdata['cursusnaam_en']; ?></title>
   <!-- InstanceEndEditable -->
@@ -41,8 +39,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/cursusdata.php'; ?>
     ! function(f, b, e, v, n, t, s) {
       if (f.fbq) return;
       n = f.fbq = function() {
-        n.callMethod ?
-          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(
+          arguments)
       };
       if (!f._fbq) f._fbq = n;
       n.push = n;
@@ -66,30 +64,48 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/cursusdata.php'; ?>
 
 <body>
   <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/GA_tagmanager.php'; ?>
-  <div id="inhoud" class="w3-main">
-    <?php
-    echo $navigatie;
-    echo '<span class="w3-opennav w3-xxlarge w3-hide-large" onclick="w3_open()">☰</span>';
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.EN.php';
-    ?>
-    <div id="main">
+  <div id="inhoud" class="w3-main"> <?php
+                                    echo $navigatie;
+                                    echo '<span class="w3-opennav w3-xxlarge w3-hide-large" onclick="w3_open()">☰</span>';
+                                    require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/header.EN.php';
+                                    ?> <div id="main">
       <!-- InstanceBeginEditable name="mainpage" -->
       <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/cursustitel.php'); ?>
       <ul>
-        <li>For: singers (soloists and very experienced choral singers), instrumentalists (baroque strings, viola da gamba, recorder/traverso, baroque oboe, baroque bassoon, baroque trumpet & trombone, harpsichord/organ, theorbo; period instruments only, tuning pitch 415 Hz)</li>
-        <li>With Dirkjan Horringa (singers and artistic direction), Femke Huizinga (orchestra, violin), Hanna Lindeijer (baroque winds), Ricardo Rodríguez Miranda (viola da gamba, cello, double bass, baroque dance), Mitchell Sandler (singers). and Edoardo Valorz (basso continuo)<br>
+        <li>For: singers (soloists and very experienced choral singers),
+          instrumentalists (baroque strings, viola da gamba,
+          recorder/traverso, baroque oboe, baroque bassoon, baroque
+          trumpet & trombone, harpsichord/organ, theorbo; period
+          instruments only, tuning pitch 415 Hz)</li>
+        <li>With Dirkjan Horringa (singers and artistic direction),
+          Femke Huizinga (orchestra, violin), Hanna Lindeijer (baroque
+          winds), Ricardo Rodríguez Miranda (viola da gamba, cello,
+          double bass, baroque dance), Mitchell Sandler (singers). and
+          Edoardo Valorz (basso continuo)<br>
         </li>
-        <li>Central works for everyone: music from Vienna/Prague/Dresden/Kroměříž by Fux, Caldara, Biber, Zelenka, Michna, Vejvanovský, Schmelzer, and others. The program includes Biber's Requiem in F minor and Zelenka's Miserere in C minor ZWV 57. These works will be presented in a public concerten in the monastery church.
-        <li>Baroque chamber music every morning for instrumental, vocal or combined vocal-instrumental ensembles</li>
-        <li>In monastery Nieuw Sion, Diepenveen near Deventer (Netherlands), with accommodation in single and double rooms or camping</li>
-        <li>With <em>Concerto & Aria Event</em>: participants can play a concerto or sing an aria; the other participants and tutors form an ad hoc orchestra which sightreads the accompaniment</li>
+        <li>During the daily afternoon sessions, all singers and
+          instrumentalists join forces to work on a compelling
+          Neapolitan Baroque programme (1650 – 1750), featuring
+          masterpieces for orchestra, soloists, and choir by, for
+          example, Pergolesi, Scarlatti, Durante, and Leo, which will
+          be performed in the Nieuw Sion monastery church as a festive
+          conclusion to the summer school.</li>
+        <li>Baroque chamber music every morning for instrumental, vocal
+          or combined vocal-instrumental ensembles</li>
+        <li>In monastery Nieuw Sion, Diepenveen near Deventer
+          (Netherlands), with accommodation in single and double rooms
+          or camping</li>
+        <li>With <em>Concerto & Aria Event</em>: participants can play a
+          concerto or sing an aria; the other participants and tutors
+          form an ad hoc orchestra which sightreads the accompaniment
+        </li>
         <li>With baroque dance workshop for all</li>
       </ul>
-      <p class="onzichtbaar plaatsvoor"> Place available for:  one more baroque oboe</p>
-
+      <p class="onzichtbaar plaatsvoor"> Place available for:  one more
+        baroque oboe</p>
       <p class="onzichtbaar">Full for: all instruments and singers </p>
-
-      <p><a href="cursus.php" class="onzichtbaar">Read more about this course</a></p>
+      <p><a href="cursus.php" class="onzichtbaar">Read more about this
+          course</a></p>
       <!-- InstanceEndEditable -->
       <h2><a href="javascript: history.go(-1)">Back</a></h2>
     </div>
