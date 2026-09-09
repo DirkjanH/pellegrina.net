93e136296320e439795122381b714bbb59834989
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-09-09 16:31:50 +0200

    Refactor LP_betaling.php to enhance the 'd' function parameters; improve HTML structure and readability of input fields

--- onderhoud/LP_betaling.php
+++ onderhoud/LP_betaling.php
@@ -11,7 +11,7 @@ if (class_exists('Kint')) {
     Kint::$enabled_mode = false;
 }
 if (!function_exists('d')) {
-    function d(...$values) {}
+    function d($_GET, $_POST, $_SESSION) {}
 }
 
 // Initialiseer sessie datum (standaard vandaag)
@@ -278,13 +278,15 @@ $openstaand_giraal = euro2($openstaand_bedrag['totaal'] - $openstaand_cashbedrag
                             <?php if ($ins['CursusId_FK'] != "") echo "<p>Inschrijving nr. 
 			<input name=\"Id\" type=\"text\" DISABLED value=\"{$ins['InschId']}\"
 			size=\"2\">&nbsp;voor cursus:&nbsp;<b>{$cursusnaam[$ins['CursusId_FK']]['NL']}</b></p>"; ?> <input name="aanbet_bedrag"
-                                type="hidden" value="<?php
-                                            echo $ins['aanbet_bedrag']; ?>">
+                                type="hidden"
+                                value="<?php
+                                                            echo $ins['aanbet_bedrag']; ?>">
                             <input name="InschId" id="InschId" type="hidden"
                                 value="<?php
                                             echo $ins['InschId']; ?>">
-                            <input name="CursusId_FK" type="hidden" value="<?php
-                                            echo $ins['CursusId_FK']; ?>">
+                            <input name="CursusId_FK" type="hidden"
+                                value="<?php
+                                                                                echo $ins['CursusId_FK']; ?>">
                         </td>
                     </tr>
                     <tr valign="baseline">
